8caa71749219cb5f3607bc107fcd739b02d4c227
John Mazanec <jmazane@amazon.com>
2021-05-31 16:52:04 -0700

    Use gmock for mocking jnienv

--- jni/tests/jni_test.cpp
+++ jni/tests/jni_test.cpp
@@ -15,8 +15,122 @@
 #include "mock_jni.h"
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <vector>
 #include <org_opensearch_knn_index_JNIService.h>
+
+using ::testing::Return;
+
+// Mock test
+// For simple cases, we can implement the behavior automatically. For other cases, we will need to mock
+class MockJNIUtil : public knn_jni::JNIUtilInterface {
+public:
+
+    MockJNIUtil() {
+        // At the moment, these are the defaults. Either make them oncall, or static
+        EXPECT_CALL(*this, ConvertJavaObjectToCppString).WillRepeatedly([this](JNIEnv * env, jobject objectJ) {
+            return *((std::string *) objectJ);
+        });
+
+        EXPECT_CALL(*this, ConvertJavaStringToCppString).WillRepeatedly([this](JNIEnv * env, jstring stringJ) {
+            return *((std::string *) stringJ);
+        });
+
+        EXPECT_CALL(*this, GetJavaIntArrayLength).WillRepeatedly([this](JNIEnv *env, jintArray arrayJ) {
+            return reinterpret_cast<std::vector<int64_t> *>(arrayJ)->size();
+        });
+
+        EXPECT_CALL(*this, GetInnerDimensionOf2dJavaFloatArray).WillRepeatedly([this](JNIEnv *env, jobjectArray array2dJ) {
+            return (*reinterpret_cast<std::vector<std::vector<float>> *>(array2dJ))[0].size();
+        });
+
+        EXPECT_CALL(*this, Convert2dJavaObjectArrayToCppFloatVector).WillRepeatedly([this](JNIEnv *env, jobjectArray array2dJ, int dim) {
+            std::vector<float> data;
+            for (auto v : (*reinterpret_cast<std::vector<std::vector<float>> *>(array2dJ)))
+                for (auto item : v)
+                    data.push_back(item);
+            return data;
+        });
+
+        EXPECT_CALL(*this, ConvertJavaIntArrayToCppIntVector).WillRepeatedly([this](JNIEnv *env, jintArray arrayJ) {
+            return *reinterpret_cast<std::vector<int64_t> *>(arrayJ);
+        });
+
+        EXPECT_CALL(*this, ConvertJavaMapToCppMap).WillRepeatedly([this](JNIEnv * env, jobject parametersJ) {
+            return *reinterpret_cast<std::unordered_map<std::string, jobject> *>(parametersJ);
+        });
+
+        EXPECT_CALL(*this, DeleteLocalRef).WillRepeatedly([this](JNIEnv *env, jobject obj) {});
+    }
+
+    MOCK_METHOD(void, ThrowJavaException, (JNIEnv* env, const char* type, const char* message));
+    MOCK_METHOD(void, HasExceptionInStack, (JNIEnv* env));
+    MOCK_METHOD(void, HasExceptionInStack, (JNIEnv* env, const std::string& message));
+    MOCK_METHOD(void, CatchCppExceptionAndThrowJava, (JNIEnv* env));
+    MOCK_METHOD(jclass, FindClass, (JNIEnv * env, const std::string& className));
+    MOCK_METHOD(jmethodID, FindMethod, (JNIEnv * env, jclass jClass, const std::string& methodName, const std::string& methodSignature));
+    MOCK_METHOD(std::string, ConvertJavaStringToCppString, (JNIEnv * env, jstring javaString));
+    //TODO: Figure out why this cant use "new" MOCK_METHOD
+    MOCK_METHOD2(ConvertJavaMapToCppMap, std::unordered_map<std::string, jobject>(JNIEnv * env, jobject parametersJ));
+    MOCK_METHOD(std::string, ConvertJavaObjectToCppString, (JNIEnv * env, jobject objectJ));
+    MOCK_METHOD(int, ConvertJavaObjectToCppInteger, (JNIEnv *env, jobject objectJ));
+    MOCK_METHOD(std::vector<float>, Convert2dJavaObjectArrayToCppFloatVector, (JNIEnv *env, jobjectArray array2dJ, int dim));
+    MOCK_METHOD(std::vector<int64_t>, ConvertJavaIntArrayToCppIntVector, (JNIEnv *env, jintArray arrayJ));
+    MOCK_METHOD(int, GetInnerDimensionOf2dJavaFloatArray, (JNIEnv *env, jobjectArray array2dJ));
+    MOCK_METHOD(int, GetJavaObjectArrayLength, (JNIEnv *env, jobjectArray arrayJ));
+    MOCK_METHOD(int, GetJavaIntArrayLength, (JNIEnv *env, jintArray arrayJ));
+    MOCK_METHOD(int, GetJavaBytesArrayLength, (JNIEnv *env, jbyteArray arrayJ));
+    MOCK_METHOD(int, GetJavaFloatArrayLength, (JNIEnv *env, jfloatArray arrayJ));
+    MOCK_METHOD(void, DeleteLocalRef, (JNIEnv *env, jobject obj));
+    MOCK_METHOD(jbyte *, GetByteArrayElements, (JNIEnv *env, jbyteArray array, jboolean * isCopy));
+    MOCK_METHOD(jfloat *, GetFloatArrayElements, (JNIEnv *env, jfloatArray array, jboolean * isCopy));
+    MOCK_METHOD(jint *, GetIntArrayElements, (JNIEnv *env, jintArray array, jboolean * isCopy));
+    MOCK_METHOD(jobject, GetObjectArrayElement, (JNIEnv *env, jobjectArray array, jsize index));
+    MOCK_METHOD(jobject, NewObject, (JNIEnv *env, jclass clazz, jmethodID methodId, int id, float distance));
+    MOCK_METHOD(jobjectArray, NewObjectArray, (JNIEnv *env, jsize len, jclass clazz, jobject init));
+    MOCK_METHOD(jbyteArray, NewByteArray, (JNIEnv *env, jsize len));
+    MOCK_METHOD(void, ReleaseByteArrayElements, (JNIEnv *env, jbyteArray array, jbyte *elems, int mode));
+    MOCK_METHOD(void, ReleaseFloatArrayElements, (JNIEnv *env, jfloatArray array, jfloat *elems, int mode));
+    MOCK_METHOD(void, ReleaseIntArrayElements, (JNIEnv *env, jintArray array, jint *elems, jint mode));
+    MOCK_METHOD(void, SetObjectArrayElement, (JNIEnv *env, jobjectArray array, jsize index, jobject val));
+    MOCK_METHOD(void, SetByteArrayRegion, (JNIEnv *env, jbyteArray array, jsize start, jsize len, const jbyte * buf));
+};
+
+TEST(FaissCreateIndexTest, BasicAssertions) {
+    // Define the data
+    JNIEnv * jniEnv = nullptr;
+
+    std::vector<int64_t> ids = {1, 2, 3, 4};
+
+    std::vector<std::vector<float>> vectors = {
+            {1.0, 2.0},
+            {3.0, 4.0},
+            {5.0, 6.0},
+            {7.0, 8.0}
+    };
+
+    std::string indexPath = "test-create.faiss";
+    std::string spaceType = knn_jni::L2;
+    std::string method = "HNSW32,Flat";
+
+    std::unordered_map<std::string, jobject> parametersMap;
+    parametersMap[knn_jni::SPACE_TYPE] = (jobject) &spaceType;
+    parametersMap[knn_jni::METHOD] = (jobject) &method;
+
+    // Set up the jni mocking
+    MockJNIUtil mockJNIUtil;
+
+    EXPECT_CALL(mockJNIUtil, GetJavaObjectArrayLength(jniEnv, reinterpret_cast<jobjectArray>(&vectors)))
+            .WillRepeatedly(Return(vectors.size()));
+
+    // Create the index
+    knn_jni::faiss_wrapper::CreateIndex(&mockJNIUtil,
+                                        jniEnv,
+                                        reinterpret_cast<jintArray>(&ids),
+                                        reinterpret_cast<jobjectArray>(&vectors),
+                                        (jstring) &indexPath,
+                                        (jobject) &parametersMap);
+}
 
 TEST(InitLibraryTest, BasicAssertions) {
     knn_jni::faiss_wrapper::InitLibrary();
@@ -30,157 +144,4 @@ TEST(FreeVectorsTest, BasicAssertions) {
     vect->push_back(3.0);
 
     Java_org_opensearch_knn_index_JNIService_freeVectors(nullptr, nullptr, reinterpret_cast<jlong>(vect));
-}
-
-TEST(GetJavaIntArrayLengthTest, BasicAssertions) {
-    auto * jnini = mock_jni::GenerateMockJNINativeInterface();
-    JNIEnv_ jniEnv = {jnini};
-
-    std::vector<int> vect = {
-            1, 2, 3, 4
-    };
-
-    auto * javaIntArray = reinterpret_cast<jintArray> (&vect);
-
-    ASSERT_EQ(vect.size(), knn_jni::GetJavaIntArrayLength(&jniEnv, javaIntArray));
-
-    delete jnini;
-}
-
-TEST(ConvertJavaMapToCppMapTest, BasicAssertions) {
-    auto * jnini = mock_jni::GenerateMockJNINativeInterface();
-
-    // Define values of interest for checks
-    const char * key1 = "key1";
-    const char * key2 = "key2";
-    int value1 = 10;
-    const char * value2 = "value2";
-    const char * dummyValue = "dummy";
-
-    // Vector of returns from MockCallObjectMethod
-    static const std::vector<void *> mockObjectCallValues{
-            (void*) dummyValue,
-            (void*) dummyValue,
-            (void*) dummyValue,
-            (void*) key1,
-            (void*) &value1,
-            (void*) dummyValue,
-            (void*) key2,
-            (void*) value2
-    };
-
-    // Initialize static variables that can be used in lambdas
-    static int callObjectMethodOrdinal = 0;
-    auto MockCallObjectMethod = [](JNIEnv_ jniEnv, jobject obj, jmethodID methodID, va_list args) {
-        return (jobject) mockObjectCallValues[callObjectMethodOrdinal++];
-    };
-
-    static int callBooleanMethodOrdinal = 2;
-    auto MockCallBooleanMethod = [](JNIEnv_ jniEnv, jobject obj, jmethodID methodID, va_list args) {
-        // Loop through the vector until there is nothing left and then return false
-        if (callBooleanMethodOrdinal-- <= 0) {
-            return false;
-        }
-        return true;
-    };
-
-    jnini->CallObjectMethodV =  reinterpret_cast<jobject (*)(JNIEnv *, jobject, jmethodID, va_list)>(*MockCallObjectMethod);
-    jnini->CallBooleanMethodV = reinterpret_cast<jboolean (*)(JNIEnv *, jobject, jmethodID, va_list)>(*MockCallBooleanMethod);
-
-    JNIEnv_ jniEnv = {jnini};
-
-    // We dont actually do anything with parametersJ, the mocking controls the return values
-    auto convertedMap = knn_jni::ConvertJavaMapToCppMap(&jniEnv, (jobject) 1);
-
-    ASSERT_EQ(value1, *(int *) convertedMap.find(key1)->second);
-    ASSERT_EQ(value2, (const char *) convertedMap.find(key2)->second);
-
-    delete jnini;
-}
-
-TEST(FaissCreateIndexTest, BasicAssertions) {
-    // Define data
-    const int dim = 2;
-    const std::vector<int> ids = {
-            1, 2, 3, 4
-    };
-
-    static const std::vector<std::vector<float>> vectors = {
-            {1.0, 2.0},
-            {3.0, 4.0},
-            {5.0, 6.0},
-            {7.0, 8.0},
-    };
-
-    // Create a basic jnienv
-    auto * jnini = mock_jni::GenerateMockJNINativeInterface();
-
-    // Mock GetArrayLength
-    static std::vector<int> mockArrayLengthCalls{
-        static_cast<int>(vectors.size()),
-        static_cast<int>(ids.size()),
-        static_cast<int>(vectors.size()),
-        dim,
-        static_cast<int>(vectors.size()),
-    };
-
-    for (auto i : vectors)
-        mockArrayLengthCalls.push_back(i.size());
-
-    mockArrayLengthCalls.push_back(static_cast<int>(ids.size()));
-
-    static int getArrayLengthOrdinal = 0;
-    auto MockGetArrayLength = [](JNIEnv_ jniEnv, jintArray array) {
-        return mockArrayLengthCalls[getArrayLengthOrdinal++];
-    };
-
-    // Mock GetObjectArrayElement
-    static long getObjectArrayElementOrdinal = 0;
-    // can this be simplified to use array instead of static vector?
-    auto MockGetObjectArrayElement = [](JNIEnv_ jniEnv, jobjectArray array, int index) {
-        if (getObjectArrayElementOrdinal == 0) {
-            return (jobject) ++getObjectArrayElementOrdinal;
-        }
-        return (jobject) &vectors[getObjectArrayElementOrdinal++ - 1];
-    };
-
-    // Define values returned by CallObject
-    const char * dummyValue = "dummy";
-    static const std::vector<void *> mockObjectCallValues{
-            (void*) dummyValue,
-            (void*) dummyValue,
-            (void*) dummyValue,
-            (void*) knn_jni::SPACE_TYPE.c_str(),
-            (void*) knn_jni::L2.c_str(),
-            (void*) dummyValue,
-            (void*) knn_jni::METHOD.c_str(),
-            (void*) "HNSW32,Flat"
-    };
-
-    // Mock CallObject
-    static int callObjectMethodOrdinal = 0;
-    auto MockCallObjectMethod = [](JNIEnv_ jniEnv, jobject obj, jmethodID methodID, va_list args) {
-        return (jobject) mockObjectCallValues[callObjectMethodOrdinal++];
-    };
-
-    // Mock CallBooleanMethod
-    static int callBooleanMethodOrdinal = 2;
-    auto MockCallBooleanMethod = [](JNIEnv_ jniEnv, jobject obj, jmethodID methodID, va_list args) {
-        // Loop through the vector until there is nothing left and then return false
-        if (callBooleanMethodOrdinal-- <= 0) {
-            return false;
-        }
-        return true;
-    };
-
-    jnini->GetArrayLength = reinterpret_cast<jsize (*)(JNIEnv *, jarray)>(*MockGetArrayLength);
-    jnini->CallObjectMethodV =  reinterpret_cast<jobject (*)(JNIEnv *, jobject, jmethodID, va_list)>(*MockCallObjectMethod);
-    jnini->CallBooleanMethodV = reinterpret_cast<jboolean (*)(JNIEnv *, jobject, jmethodID, va_list)>(*MockCallBooleanMethod);
-    jnini->GetObjectArrayElement = reinterpret_cast<jobject (*)(JNIEnv *, jobjectArray, int)>(*MockGetObjectArrayElement);
-
-    JNIEnv_ jniEnv = {jnini};
-
-    const char * tempPath = "test22.faiss";
-
-    knn_jni::faiss_wrapper::CreateIndex(&jniEnv, (jintArray) &ids, (jobjectArray) &vectors, (jstring) tempPath, (jobject) 1);
 }
