cc5e35ff7f4eb663f04988cc0a2b9e4b691118f4
Jack Mazanec <jmazane@amazon.com>
2021-09-15 14:26:36 -0700

    Add model metadata to system index metadata (#89)
    Signed-off-by: Jack Mazanec <jmazane@amazon.com>
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

---
+++ src/test/java/org/opensearch/knn/plugin/transport/UpdateModelMetadataRequestTests.java
@@ -0,0 +1,81 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.plugin.transport;
+
+import org.opensearch.common.io.stream.BytesStreamOutput;
+import org.opensearch.knn.KNNTestCase;
+import org.opensearch.knn.index.SpaceType;
+import org.opensearch.knn.index.util.KNNEngine;
+import org.opensearch.knn.indices.ModelMetadata;
+
+import java.io.IOException;
+
+public class UpdateModelMetadataRequestTests extends KNNTestCase {
+
+    public void testStreams() throws IOException {
+        KNNEngine knnEngine = KNNEngine.DEFAULT;
+        SpaceType spaceType = SpaceType.L2;
+        int dimension = 128;
+
+        String modelId = "test-model";
+        boolean isRemoveRequest = false;
+
+        ModelMetadata modelMetadata = new ModelMetadata(knnEngine, spaceType, dimension);
+        UpdateModelMetadataRequest updateModelMetadataRequest = new UpdateModelMetadataRequest(modelId, isRemoveRequest, modelMetadata);
+
+        BytesStreamOutput streamOutput = new BytesStreamOutput();
+        updateModelMetadataRequest.writeTo(streamOutput);
+
+        UpdateModelMetadataRequest updateModelMetadataRequestCopy = new UpdateModelMetadataRequest(streamOutput.bytes().streamInput());
+
+        assertEquals(updateModelMetadataRequest.getModelId(), updateModelMetadataRequestCopy.getModelId());
+        assertEquals(updateModelMetadataRequest.isRemoveRequest(), updateModelMetadataRequestCopy.isRemoveRequest());
+        assertEquals(updateModelMetadataRequest.getModelMetadata(), updateModelMetadataRequestCopy.getModelMetadata());
+    }
+
+    public void testValidate() {
+        ModelMetadata modelMetadata = new ModelMetadata(KNNEngine.DEFAULT, SpaceType.L2, 128);
+
+        UpdateModelMetadataRequest updateModelMetadataRequest1 = new UpdateModelMetadataRequest("test", true, null);
+        assertNull(updateModelMetadataRequest1.validate());
+
+        UpdateModelMetadataRequest updateModelMetadataRequest2 = new UpdateModelMetadataRequest("test", false, null);
+        assertNotNull(updateModelMetadataRequest2.validate());
+
+        UpdateModelMetadataRequest updateModelMetadataRequest3 = new UpdateModelMetadataRequest("test", false, modelMetadata);
+        assertNull(updateModelMetadataRequest3.validate());
+
+        UpdateModelMetadataRequest updateModelMetadataRequest4 = new UpdateModelMetadataRequest("", true, null);
+        assertNotNull(updateModelMetadataRequest4.validate());
+    }
+
+    public void testGetModelId() {
+        String modelId = "test-model";
+        UpdateModelMetadataRequest updateModelMetadataRequest = new UpdateModelMetadataRequest(modelId, true, null);
+
+        assertEquals(modelId, updateModelMetadataRequest.getModelId());
+    }
+
+    public void testIsRemoveRequest() {
+        boolean isRemoveRequest = false;
+        UpdateModelMetadataRequest updateModelMetadataRequest = new UpdateModelMetadataRequest("test", isRemoveRequest, null);
+
+        assertEquals(isRemoveRequest, updateModelMetadataRequest.isRemoveRequest());
+    }
+
+    public void testGetModelMetadata() {
+        ModelMetadata modelMetadata = new ModelMetadata(KNNEngine.DEFAULT, SpaceType.L2, 128);
+        UpdateModelMetadataRequest updateModelMetadataRequest = new UpdateModelMetadataRequest("test", true, modelMetadata);
+
+        assertEquals(modelMetadata, updateModelMetadataRequest.getModelMetadata());
+    }
+}
