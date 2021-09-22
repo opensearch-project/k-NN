6319bb2e3221609e0fe2f8ab2dcaf450bbda8db4
Jack Mazanec <jmazane@amazon.com>
2021-09-22 13:36:20 -0700

    Refactor model management to support apis  (#95)
    Signed-off-by: Jack Mazanec <jmazane@amazon.com>

--- src/main/java/org/opensearch/knn/indices/ModelMetadata.java
+++ src/main/java/org/opensearch/knn/indices/ModelMetadata.java
@@ -16,12 +16,15 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.opensearch.common.io.stream.StreamInput;
 import org.opensearch.common.io.stream.StreamOutput;
 import org.opensearch.common.io.stream.Writeable;
+import org.opensearch.common.unit.TimeValue;
 import org.opensearch.knn.index.SpaceType;
 import org.opensearch.knn.index.util.KNNEngine;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
+import static org.opensearch.knn.common.KNNConstants.MODEL_TIMESTAMP;
 import static org.opensearch.knn.index.KNNVectorFieldMapper.MAX_DIMENSION;
 
 public class ModelMetadata implements Writeable {
@@ -32,6 +35,11 @@ public class ModelMetadata implements Writeable {
     final private SpaceType spaceType;
     final private int dimension;
 
+    private AtomicReference<ModelState> state;
+    final private TimeValue timestamp;
+    final private String description;
+    private String error;
+
     /**
      * Constructor
      *
@@ -41,6 +49,13 @@ public class ModelMetadata implements Writeable {
         this.knnEngine = KNNEngine.getEngine(in.readString());
         this.spaceType = SpaceType.getSpace(in.readString());
         this.dimension = in.readInt();
+        this.state = new AtomicReference<>(ModelState.readFrom(in));
+        this.timestamp = in.readTimeValue();
+
+        // Description and error may be empty. However, reading the string will work as long as they are not null
+        // which is checked in constructor and setters
+        this.description = in.readString();
+        this.error = in.readString();
     }
 
     /**
@@ -49,8 +64,13 @@ public class ModelMetadata implements Writeable {
      * @param knnEngine engine model is built with
      * @param spaceType space type model uses
      * @param dimension dimension of the model
+     * @param modelState state of the model
+     * @param timestamp timevalue when model was created
+     * @param description information about the model
+     * @param error error message associated with model
      */
-    public ModelMetadata(KNNEngine knnEngine, SpaceType spaceType, int dimension) {
+    public ModelMetadata(KNNEngine knnEngine, SpaceType spaceType, int dimension, ModelState modelState,
+                         TimeValue timestamp, String description, String error) {
         this.knnEngine = Objects.requireNonNull(knnEngine, "knnEngine must not be null");
         this.spaceType = Objects.requireNonNull(spaceType, "spaceType must not be null");
         if (dimension <= 0 || dimension >= MAX_DIMENSION) {
@@ -58,6 +78,11 @@ public class ModelMetadata implements Writeable {
                     "than 0 and less than " + MAX_DIMENSION);
         }
         this.dimension = dimension;
+
+        this.state = new AtomicReference<>(Objects.requireNonNull(modelState, "modelState must not be null"));
+        this.timestamp = Objects.requireNonNull(timestamp, "timestamp must not be null");
+        this.description = Objects.requireNonNull(description, "description must not be null");
+        this.error = Objects.requireNonNull(error, "error must not be null");
     }
 
     /**
@@ -87,9 +112,64 @@ public class ModelMetadata implements Writeable {
         return dimension;
     }
 
+    /**
+     * getter for model's state
+     *
+     * @return state
+     */
+    public ModelState getState() {
+        return state.get();
+    }
+
+    /**
+     * getter for model's timestamp
+     *
+     * @return timestamp
+     */
+    public TimeValue getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * getter for model's description
+     *
+     * @return description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * getter for model's error
+     *
+     * @return error
+     */
+    public String getError() {
+        return error;
+    }
+
+    /**
+     * setter for model's state
+     *
+     * @param state of the model
+     */
+    public synchronized void setState(ModelState state) {
+        this.state.set(Objects.requireNonNull(state, "state must not be null"));
+    }
+
+    /**
+     * setter for model's error
+     *
+     * @param error set on failure
+     */
+    public synchronized void setError(String error) {
+        this.error = Objects.requireNonNull(error, "error must not be null");
+    }
+
     @Override
     public String toString() {
-        return knnEngine.getName() + DELIMITER + spaceType.getValue() + DELIMITER + dimension;
+        return String.join(DELIMITER, knnEngine.getName(), spaceType.getValue(), Integer.toString(dimension),
+                getState().toString(), timestamp.toString(), description, error);
     }
 
     @Override
@@ -101,16 +181,21 @@ public class ModelMetadata implements Writeable {
         ModelMetadata other = (ModelMetadata) obj;
 
         EqualsBuilder equalsBuilder = new EqualsBuilder();
-        equalsBuilder.append(knnEngine, other.knnEngine);
-        equalsBuilder.append(spaceType, other.spaceType);
-        equalsBuilder.append(dimension, other.dimension);
+        equalsBuilder.append(getKnnEngine(), other.getKnnEngine());
+        equalsBuilder.append(getSpaceType(), other.getSpaceType());
+        equalsBuilder.append(getDimension(), other.getDimension());
+        equalsBuilder.append(getState(), other.getState());
+        equalsBuilder.append(getTimestamp(), other.getTimestamp());
+        equalsBuilder.append(getDescription(), other.getDescription());
+        equalsBuilder.append(getError(), other.getError());
 
         return equalsBuilder.isEquals();
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder().append(knnEngine).append(spaceType).append(dimension).toHashCode();
+        return new HashCodeBuilder().append(getKnnEngine()).append(getSpaceType()).append(getDimension())
+                .append(getState()).append(getTimestamp()).append(getDescription()).append(getError()).toHashCode();
     }
 
     /**
@@ -120,24 +205,32 @@ public class ModelMetadata implements Writeable {
      * @return modelMetadata from string
      */
     public static ModelMetadata fromString(String modelMetadataString) {
-        String[] modelMetadataArray = modelMetadataString.split(DELIMITER);
+        String[] modelMetadataArray = modelMetadataString.split(DELIMITER, -1);
 
-        if (modelMetadataArray.length != 3) {
+        if (modelMetadataArray.length != 7) {
             throw new IllegalArgumentException("Illegal format for model metadata. Must be of the form " +
-                    "\"<KNNEngine>,<SpaceType>,<Dimension>\"");
+                    "\"<KNNEngine>,<SpaceType>,<Dimension>,<ModelState>,<Timestamp>,<Description>,<Error>\".");
         }
 
         KNNEngine knnEngine = KNNEngine.getEngine(modelMetadataArray[0]);
         SpaceType spaceType = SpaceType.getSpace(modelMetadataArray[1]);
         int dimension = Integer.parseInt(modelMetadataArray[2]);
+        ModelState modelState = ModelState.getModelState(modelMetadataArray[3]);
+        TimeValue timestamp = TimeValue.parseTimeValue(modelMetadataArray[4], MODEL_TIMESTAMP);
+        String description = modelMetadataArray[5];
+        String error = modelMetadataArray[6];
 
-        return new ModelMetadata(knnEngine, spaceType, dimension);
+        return new ModelMetadata(knnEngine, spaceType, dimension, modelState, timestamp, description, error);
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
-        out.writeString(knnEngine.getName());
-        out.writeString(spaceType.getValue());
-        out.writeInt(dimension);
+        out.writeString(getKnnEngine().getName());
+        out.writeString(getSpaceType().getValue());
+        out.writeInt(getDimension());
+        getState().writeTo(out);
+        out.writeTimeValue(getTimestamp());
+        out.writeString(getDescription());
+        out.writeString(getError());
     }
 }
