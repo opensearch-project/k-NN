cd58483a6508cc01d61062b1b2fff33fb32a53d8
Jack Mazanec <jmazane@amazon.com>
2021-10-11 16:34:48 -0700

    Fix bug when clearing models (#127)
    Signed-off-by: Jack Mazanec <jmazane@amazon.com>
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

---
+++ src/test/java/org/opensearch/knn/plugin/transport/RemoveModelFromCacheTransportActionTests.java
@@ -0,0 +1,93 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.plugin.transport;
+
+import com.google.common.collect.ImmutableSet;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.ClusterSettings;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.knn.KNNSingleNodeTestCase;
+import org.opensearch.knn.index.SpaceType;
+import org.opensearch.knn.index.util.KNNEngine;
+import org.opensearch.knn.indices.Model;
+import org.opensearch.knn.indices.ModelCache;
+import org.opensearch.knn.indices.ModelDao;
+import org.opensearch.knn.indices.ModelMetadata;
+import org.opensearch.knn.indices.ModelState;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.opensearch.knn.index.KNNSettings.MODEL_CACHE_SIZE_IN_BYTES_SETTING;
+
+public class RemoveModelFromCacheTransportActionTests extends KNNSingleNodeTestCase {
+
+    public void testNodeOperation_modelNotInCache() {
+        ClusterService clusterService = mock(ClusterService.class);
+        Settings settings = Settings.builder().put(MODEL_CACHE_SIZE_IN_BYTES_SETTING.getKey(), 100).build();
+        ClusterSettings clusterSettings = new ClusterSettings(settings,
+                ImmutableSet.of(MODEL_CACHE_SIZE_IN_BYTES_SETTING));
+        when(clusterService.getClusterSettings()).thenReturn(clusterSettings);
+        when(clusterService.getSettings()).thenReturn(settings);
+
+        ModelDao modelDao = mock(ModelDao.class);
+        ModelCache.initialize(modelDao, clusterService);
+
+        // Check that model cache is initially empty
+        ModelCache modelCache = ModelCache.getInstance();
+        assertEquals(0, modelCache.getTotalWeight());
+
+        // Remove the model from the cache
+        RemoveModelFromCacheTransportAction action = node().injector()
+                .getInstance(RemoveModelFromCacheTransportAction.class);
+
+        RemoveModelFromCacheNodeRequest request = new RemoveModelFromCacheNodeRequest("invalid-model");
+        action.nodeOperation(request);
+
+        assertEquals(0L, modelCache.getTotalWeight());
+    }
+
+    public void testNodeOperation_modelInCache() throws ExecutionException, InterruptedException {
+        ClusterService clusterService = mock(ClusterService.class);
+        Settings settings = Settings.builder().put(MODEL_CACHE_SIZE_IN_BYTES_SETTING.getKey(), 100).build();
+        ClusterSettings clusterSettings = new ClusterSettings(settings,
+                ImmutableSet.of(MODEL_CACHE_SIZE_IN_BYTES_SETTING));
+        when(clusterService.getClusterSettings()).thenReturn(clusterSettings);
+        when(clusterService.getSettings()).thenReturn(settings);
+
+        ModelDao modelDao = mock(ModelDao.class);
+        String modelId = "test-model-id";
+        Model model = new Model(
+                new ModelMetadata(KNNEngine.DEFAULT, SpaceType.L2, 16, ModelState.CREATED,
+                "timestamp", "description", ""),
+                new byte[128]
+        );
+
+        when(modelDao.get(modelId)).thenReturn(model);
+
+        ModelCache.initialize(modelDao, clusterService);
+
+        // Load the model into the cache
+        ModelCache modelCache = ModelCache.getInstance();
+        modelCache.get(modelId);
+
+        // Remove the model from the cache
+        RemoveModelFromCacheTransportAction action = node().injector()
+                .getInstance(RemoveModelFromCacheTransportAction.class);
+
+        RemoveModelFromCacheNodeRequest request = new RemoveModelFromCacheNodeRequest(modelId);
+        action.nodeOperation(request);
+
+        assertEquals(0L, modelCache.getTotalWeight());
+    }
+}
