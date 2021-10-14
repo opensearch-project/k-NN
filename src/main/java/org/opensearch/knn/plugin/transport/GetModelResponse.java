c7c82d3549164c345afe746d0906b150731597a8
Vijayan Balasubramanian <balasvij@amazon.com>
2021-10-14 14:47:52 -0700

    Allow model to implement Writeable, ToXContent (#135)
    Refactor model to XContent from GetModelResponse to
    Model, since it will be shared by SearchModelResponse
    in future commits.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- src/main/java/org/opensearch/knn/plugin/transport/GetModelResponse.java
+++ src/main/java/org/opensearch/knn/plugin/transport/GetModelResponse.java
@@ -16,48 +16,23 @@ import org.opensearch.common.io.stream.StreamOutput;
 import org.opensearch.common.xcontent.ToXContentObject;
 import org.opensearch.common.xcontent.XContentBuilder;
 import org.opensearch.knn.indices.Model;
-import org.opensearch.knn.indices.ModelMetadata;
 
 import java.io.IOException;
-import java.util.Base64;
-import java.util.Objects;
-
-import static org.opensearch.knn.common.KNNConstants.DIMENSION;
-import static org.opensearch.knn.common.KNNConstants.KNN_ENGINE;
-import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_SPACE_TYPE;
-import static org.opensearch.knn.common.KNNConstants.MODEL_BLOB_PARAMETER;
-import static org.opensearch.knn.common.KNNConstants.MODEL_DESCRIPTION;
-import static org.opensearch.knn.common.KNNConstants.MODEL_ERROR;
-import static org.opensearch.knn.common.KNNConstants.MODEL_ID;
-import static org.opensearch.knn.common.KNNConstants.MODEL_STATE;
-import static org.opensearch.knn.common.KNNConstants.MODEL_TIMESTAMP;
 
 /**
  * {@link GetModelResponse} represents Response returned by {@link GetModelRequest}
  */
 public class GetModelResponse extends ActionResponse implements ToXContentObject {
 
-    private final String modelID;
     private final Model model;
 
-    public GetModelResponse(String modelID, Model model) {
-        this.modelID = modelID;
+    public GetModelResponse(Model model) {
         this.model = model;
     }
 
     public GetModelResponse(StreamInput in) throws IOException {
         super(in);
-        this.modelID = in.readString();
-        ModelMetadata metadata = new ModelMetadata(in);
-        this.model = new Model(metadata, readOptionalModelBlob(in));
-    }
-
-    private byte[] readOptionalModelBlob(StreamInput in) throws IOException {
-        return in.readBoolean() ? in.readByteArray(): null;
-    }
-
-    public String getModelID() {
-        return modelID;
+        this.model = new Model(in);
     }
 
     public Model getModel() {
@@ -79,42 +54,12 @@ public class GetModelResponse extends ActionResponse implements ToXContentObject
                 "dimension": 128
         }
          */
-        builder.startObject();
-        builder.field(MODEL_ID, modelID);
-        builder.field(MODEL_STATE, model.getModelMetadata().getState().getName());
-        builder.field(MODEL_TIMESTAMP, model.getModelMetadata().getTimestamp());
-        builder.field(MODEL_DESCRIPTION, model.getModelMetadata().getDescription());
-        builder.field(MODEL_ERROR, model.getModelMetadata().getError());
-
-        String base64Model = "";
-        if(model.getModelBlob() != null){
-            base64Model = Base64.getEncoder().encodeToString(model.getModelBlob());
-        }
-        builder.field(MODEL_BLOB_PARAMETER, base64Model);
-
-        builder.field(METHOD_PARAMETER_SPACE_TYPE, model.getModelMetadata().getSpaceType().getValue());
-        builder.field(DIMENSION, model.getModelMetadata().getDimension());
-        builder.field(KNN_ENGINE, model.getModelMetadata().getKnnEngine().getName());
-
-        builder.endObject();
-
-        return builder;
-    }
-
-    private void writeOptionalModelBlob(StreamOutput output) throws IOException {
-        if(model.getModelBlob() == null){
-            output.writeBoolean(false);
-            return;
-        }
-        output.writeBoolean(true);
-        output.writeByteArray(model.getModelBlob());
+        return model.toXContent(builder, params);
     }
 
 
     @Override
     public void writeTo(StreamOutput output) throws IOException {
-        output.writeString(modelID);
-        model.getModelMetadata().writeTo(output);
-        writeOptionalModelBlob(output);
+        model.writeTo(output);
     }
 }
