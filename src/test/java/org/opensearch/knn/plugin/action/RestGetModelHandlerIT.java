7ae666826c063bb20659f09956b96424ea435379
Vijayan Balasubramanian <balasvij@amazon.com>
2021-10-28 18:31:10 -0700

    Update Model Search Response
    Return model index search response as output for
    model search api.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- src/test/java/org/opensearch/knn/plugin/action/RestGetModelHandlerIT.java
+++ src/test/java/org/opensearch/knn/plugin/action/RestGetModelHandlerIT.java
@@ -13,57 +13,32 @@ package org.opensearch.knn.plugin.action;
 
 import joptsimple.internal.Strings;
 import org.apache.http.util.EntityUtils;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.opensearch.ResourceNotFoundException;
-import org.opensearch.action.ActionFuture;
-import org.opensearch.action.ActionListener;
-import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.client.ResponseException;
-import org.opensearch.common.settings.Settings;
-import org.opensearch.common.unit.TimeValue;
 import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.knn.KNNRestTestCase;
-import org.opensearch.knn.KNNSingleNodeTestCase;
 import org.opensearch.knn.index.SpaceType;
 import org.opensearch.knn.index.util.KNNEngine;
-import org.opensearch.knn.indices.Model;
-import org.opensearch.knn.indices.ModelDao;
 import org.opensearch.knn.indices.ModelMetadata;
 import org.opensearch.knn.indices.ModelState;
 import org.opensearch.knn.plugin.KNNPlugin;
-import org.opensearch.knn.plugin.transport.GetModelAction;
-import org.opensearch.knn.plugin.transport.GetModelRequest;
-import org.opensearch.knn.plugin.transport.GetModelResponse;
 import org.opensearch.rest.RestStatus;
 
 import java.io.IOException;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import static org.opensearch.knn.common.KNNConstants.DIMENSION;
 import static org.opensearch.knn.common.KNNConstants.KNN_ENGINE;
 import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_SPACE_TYPE;
 import static org.opensearch.knn.common.KNNConstants.MODELS;
-import static org.opensearch.knn.common.KNNConstants.MODEL_BLOB_PARAMETER;
 import static org.opensearch.knn.common.KNNConstants.MODEL_DESCRIPTION;
 import static org.opensearch.knn.common.KNNConstants.MODEL_ERROR;
 import static org.opensearch.knn.common.KNNConstants.MODEL_ID;
-import static org.opensearch.knn.common.KNNConstants.MODEL_INDEX_NAME;
 import static org.opensearch.knn.common.KNNConstants.MODEL_STATE;
 import static org.opensearch.knn.common.KNNConstants.MODEL_TIMESTAMP;
-import static org.opensearch.knn.common.KNNConstants.SPACE_TYPE;
 
 /**
  * Integration tests to check the correctness of {@link org.opensearch.knn.plugin.rest.RestGetModelHandler}
@@ -109,7 +84,6 @@ public class RestGetModelHandlerIT extends KNNRestTestCase {
     }
 
 
-
     public void testGetModelExistsWithFilter() throws IOException {
         createModelSystemIndex();
         String testModelID = "test-model-id";
@@ -121,7 +95,7 @@ public class RestGetModelHandlerIT extends KNNRestTestCase {
         String restURI = String.join("/", KNNPlugin.KNN_BASE_URI, MODELS, testModelID);
         Request request = new Request("GET", restURI);
 
-        List<String> filterdPath = Arrays.asList(MODEL_ID,MODEL_DESCRIPTION,MODEL_TIMESTAMP,KNN_ENGINE);
+        List<String> filterdPath = Arrays.asList(MODEL_ID, MODEL_DESCRIPTION, MODEL_TIMESTAMP, KNN_ENGINE);
         request.addParameter("filter_path", Strings.join(filterdPath, ","));
 
         Response response = client().performRequest(request);
@@ -135,7 +109,7 @@ public class RestGetModelHandlerIT extends KNNRestTestCase {
             responseBody
         ).map();
 
-        assertTrue(responseMap.size()==filterdPath.size());
+        assertTrue(responseMap.size() == filterdPath.size());
         assertEquals(testModelID, responseMap.get(MODEL_ID));
         assertEquals(testModelMetadata.getDescription(), responseMap.get(MODEL_DESCRIPTION));
         assertEquals(testModelMetadata.getTimestamp(), responseMap.get(MODEL_TIMESTAMP));
