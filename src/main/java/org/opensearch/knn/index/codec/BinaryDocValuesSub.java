b5a4c0aa635340dde99d9793907e7ad288a77841
Naveen Tatikonda <89161683+naveentatikonda@users.noreply.github.com>
2021-11-09 18:26:35 -0600

    Update license headers (#194)
    * Remove Amazon License Headers
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Update NOTICE.txt and CONTRIBUTING.md
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Replace Apache License Header with short version
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>

--- src/main/java/org/opensearch/knn/index/codec/BinaryDocValuesSub.java
+++ src/main/java/org/opensearch/knn/index/codec/BinaryDocValuesSub.java
@@ -1,26 +1,6 @@
 /*
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- *
- * Modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
- */
-/*
- *   Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- *   Licensed under the Apache License, Version 2.0 (the "License").
- *   You may not use this file except in compliance with the License.
- *   A copy of the License is located at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   or in the "license" file accompanying this file. This file is distributed
- *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- *   express or implied. See the License for the specific language governing
- *   permissions and limitations under the License.
  */
 
 package org.opensearch.knn.index.codec;
