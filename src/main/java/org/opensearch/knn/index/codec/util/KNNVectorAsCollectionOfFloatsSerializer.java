2a78a8243724f5b35828f6eaeb26ae21c88eb96e
Martin Gaievski <gaievski@amazon.com>
2022-01-13 10:34:25 -0800

    Changing serialization for knn vector from single array object to collection of floats (#253)
    * Changing serialization for knn vector from single array object to collection of floats
    
    rev2:
    * Addressing PR comments:
    - added getDefaultSerializer to Factory
    - moved SerializationMode enum to a separate file
    - added javadocs and comments
    - adjust format, added missing endline characters
    rev3:
    * Addressing multiple review comments:
    - replace Vector by KNNVector in class names and variables
    - fixed method names in Serializer interface
    - replace number of bytes in float from number to constant
    rev4:
    * Moving new classes under index.codec.util
    rev5:
    * Addressing multiple review comments:
    - rework factory method getSerializerByStreamContent
    - added test case for stream of unsupported content
    - removed exceptions from Serializer interface method's signatures, changed it to unchecked runtime exception
    - simplify license header in new classes
    
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

---
+++ src/main/java/org/opensearch/knn/index/codec/util/KNNVectorAsCollectionOfFloatsSerializer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.knn.index.codec.util;
+
+import java.io.ByteArrayInputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.stream.IntStream;
+
+/**
+ * Class implements KNNVectorSerializer based on serialization/deserialization of array as collection of individual numbers
+ */
+public class KNNVectorAsCollectionOfFloatsSerializer implements KNNVectorSerializer {
+    private static final int BYTES_IN_FLOAT = 4;
+
+    @Override
+    public byte[] floatToByteArray(float[] input) {
+        final ByteBuffer bb = ByteBuffer.allocate(input.length * BYTES_IN_FLOAT).order(ByteOrder.BIG_ENDIAN);
+        IntStream.range(0, input.length).forEach((index) -> bb.putFloat(input[index]));
+        byte[] bytes = new byte[bb.flip().limit()];
+        bb.get(bytes);
+        return bytes;
+    }
+
+    @Override
+    public float[] byteToFloatArray(ByteArrayInputStream byteStream) {
+        if (byteStream == null || byteStream.available() % BYTES_IN_FLOAT != 0) {
+            throw new IllegalArgumentException("Byte stream cannot be deserialized to array of floats");
+        }
+        final byte[] vectorAsByteArray = new byte[byteStream.available()];
+        byteStream.read(vectorAsByteArray, 0, byteStream.available());
+        final int sizeOfFloatArray = vectorAsByteArray.length / BYTES_IN_FLOAT;
+        final float[] vector = new float[sizeOfFloatArray];
+        ByteBuffer.wrap(vectorAsByteArray).asFloatBuffer().get(vector);
+        return vector;
+    }
+}
