bb42bb635b1bd640e55496cf773c1e5e7ab96c49
Naveen Tatikonda <navtat@amazon.com>
2022-04-07 16:31:42 -0500

    Add size validation for Search Model API (#358)
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>

--- src/main/java/org/opensearch/knn/plugin/rest/RestSearchModelHandler.java
+++ src/main/java/org/opensearch/knn/plugin/rest/RestSearchModelHandler.java
@@ -12,27 +12,15 @@
 package org.opensearch.knn.plugin.rest;
 
 import com.google.common.collect.ImmutableList;
-import org.opensearch.action.search.SearchAction;
 import org.opensearch.action.search.SearchRequest;
-import org.opensearch.action.search.SearchResponse;
 import org.opensearch.client.node.NodeClient;
-import org.opensearch.common.bytes.BytesReference;
-import org.opensearch.common.xcontent.ToXContentObject;
-import org.opensearch.common.xcontent.XContentBuilder;
-import org.opensearch.knn.indices.Model;
 import org.opensearch.knn.plugin.KNNPlugin;
 import org.opensearch.knn.plugin.transport.SearchModelAction;
 import org.opensearch.rest.BaseRestHandler;
-import org.opensearch.rest.BytesRestResponse;
-import org.opensearch.rest.RestChannel;
 import org.opensearch.rest.RestRequest;
-import org.opensearch.rest.RestResponse;
-import org.opensearch.rest.RestStatus;
 import org.opensearch.rest.action.RestCancellableNodeClient;
-import org.opensearch.rest.action.RestResponseListener;
 import org.opensearch.rest.action.RestToXContentListener;
 import org.opensearch.rest.action.search.RestSearchAction;
-import org.opensearch.search.SearchHit;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -41,10 +29,10 @@ import java.util.List;
 import java.util.Locale;
 import java.util.function.IntConsumer;
 
-import static org.opensearch.common.xcontent.ToXContent.EMPTY_PARAMS;
-import static org.opensearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.opensearch.knn.common.KNNConstants.MODELS;
-import static org.opensearch.knn.common.KNNConstants.MODEL_INDEX_NAME;
+import static org.opensearch.knn.common.KNNConstants.PARAM_SIZE;
+import static org.opensearch.knn.common.KNNConstants.SEARCH_MODEL_MAX_SIZE;
+import static org.opensearch.knn.common.KNNConstants.SEARCH_MODEL_MIN_SIZE;
 
 /**
  * Rest Handler for search model api endpoint.
@@ -53,9 +41,9 @@ public class RestSearchModelHandler extends BaseRestHandler {
 
     private final static String NAME = "knn_search_model_action";
     private static final String SEARCH = "_search";
-    //Add params that are not fit to be part of model search
+    // Add params that are not fit to be part of model search
     public List<String> UNSUPPORTED_PARAM_LIST = Arrays.asList(
-        "index" //we don't want to search across all indices
+        "index" // we don't want to search across all indices
     );
 
     @Override
@@ -65,17 +53,10 @@ public class RestSearchModelHandler extends BaseRestHandler {
 
     @Override
     public List<Route> routes() {
-        return ImmutableList
-            .of(
-                new Route(
-                    RestRequest.Method.GET,
-                    String.format(Locale.ROOT, "%s/%s/%s", KNNPlugin.KNN_BASE_URI, MODELS, SEARCH)
-                ),
-                new Route(
-                    RestRequest.Method.POST,
-                    String.format(Locale.ROOT, "%s/%s/%s", KNNPlugin.KNN_BASE_URI, MODELS, SEARCH)
-                )
-            );
+        return ImmutableList.of(
+            new Route(RestRequest.Method.GET, String.format(Locale.ROOT, "%s/%s/%s", KNNPlugin.KNN_BASE_URI, MODELS, SEARCH)),
+            new Route(RestRequest.Method.POST, String.format(Locale.ROOT, "%s/%s/%s", KNNPlugin.KNN_BASE_URI, MODELS, SEARCH))
+        );
     }
 
     private void checkUnSupportedParamsExists(RestRequest request) {
@@ -86,19 +67,40 @@ public class RestSearchModelHandler extends BaseRestHandler {
                 invalidParam.add(param);
             }
         });
-        if (invalidParam.isEmpty())
-            return;
+        if (invalidParam.isEmpty()) return;
         String errorMessage = "request contains an unrecognized parameter: [ " + String.join(",", invalidParam) + " ]";
         throw new IllegalArgumentException(errorMessage);
     }
 
+    private void validateSizeParameter(RestRequest request) {
+        if (!request.hasParam(PARAM_SIZE)) {
+            return;
+        }
+        if (isSearchSizeValueValid(request.paramAsInt(PARAM_SIZE, 1))) {
+            return;
+        }
+        throw new IllegalArgumentException(
+            String.format("%s must be between %d and %d inclusive", PARAM_SIZE, SEARCH_MODEL_MIN_SIZE, SEARCH_MODEL_MAX_SIZE)
+        );
+    }
+
+    private boolean isSearchSizeValueValid(int searchSize) {
+        return (searchSize >= SEARCH_MODEL_MIN_SIZE) && (searchSize <= SEARCH_MODEL_MAX_SIZE);
+    }
+
+    private void validateRequest(RestRequest request) {
+        checkUnSupportedParamsExists(request);
+        validateSizeParameter(request);
+    }
+
     @Override
     protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
-        checkUnSupportedParamsExists(request);
+        validateRequest(request);
         SearchRequest searchRequest = new SearchRequest();
         IntConsumer setSize = size -> searchRequest.source().size(size);
-        request.withContentOrSourceParamParserOrNull(parser ->
-            RestSearchAction.parseSearchRequest(searchRequest, request, parser, client.getNamedWriteableRegistry(), setSize));
+        request.withContentOrSourceParamParserOrNull(
+            parser -> RestSearchAction.parseSearchRequest(searchRequest, request, parser, client.getNamedWriteableRegistry(), setSize)
+        );
 
         return channel -> {
             RestCancellableNodeClient cancelClient = new RestCancellableNodeClient(client, request.getHttpChannel());
