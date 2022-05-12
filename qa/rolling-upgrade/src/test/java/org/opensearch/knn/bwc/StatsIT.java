0bf490e7857893d00f74450b74cf833e787ce863
Naveen Tatikonda <navtat@amazon.com>
2022-05-12 17:46:38 -0500

    Address review comments
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>

--- qa/rolling-upgrade/src/test/java/org/opensearch/knn/bwc/StatsIT.java
+++ qa/rolling-upgrade/src/test/java/org/opensearch/knn/bwc/StatsIT.java
@@ -7,58 +7,32 @@ package org.opensearch.knn.bwc;
 
 import org.apache.http.util.EntityUtils;
 import org.opensearch.client.Response;
-import org.opensearch.cluster.health.ClusterHealthStatus;
-import org.opensearch.common.xcontent.XContentType;
+import org.opensearch.client.ResponseException;
+import org.opensearch.knn.plugin.stats.KNNStats;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
-import static org.opensearch.knn.TestUtils.NODES_BWC_CLUSTER;
-import static org.opensearch.knn.common.KNNConstants.MODEL_INDEX_NAME;
-import static org.opensearch.knn.plugin.stats.StatNames.MODEL_INDEX_STATUS;
+import static org.opensearch.knn.plugin.stats.KNNStatsConfig.KNN_STATS;
 
 public class StatsIT extends AbstractRollingUpgradeTestCase {
-    private static final String MODEL_INDEX_STATUS_NAME = MODEL_INDEX_STATUS.getName();
+    private KNNStats knnStats = new KNNStats(KNN_STATS);
 
-    // KNN Stats : model_index_status
-    public void testModelIndexHealthMetrics() throws IOException {
-        waitForClusterHealthGreen(NODES_BWC_CLUSTER);
-        switch (getClusterType()) {
-            case OLD:
-                Map<String, Object> statsMap = getModelIndexHealthMetric(MODEL_INDEX_STATUS_NAME);
-                assertNull(statsMap.get(MODEL_INDEX_STATUS_NAME));
-
-                createModelSystemIndex();
-                break;
-            case MIXED:
-                if (isFirstMixedRound()) {
-                    Map<String, Object> statsMapFirstMixedRound = getModelIndexHealthMetric(MODEL_INDEX_STATUS_NAME);
-                    assertNotNull(statsMapFirstMixedRound.get(MODEL_INDEX_STATUS_NAME));
-                    assertNotNull(ClusterHealthStatus.fromString((String) statsMapFirstMixedRound.get(MODEL_INDEX_STATUS_NAME)));
-                } else {
-                    deleteKNNIndex(MODEL_INDEX_NAME);
-
-                    Map<String, Object> statsMapSecondMixedRound = getModelIndexHealthMetric(MODEL_INDEX_STATUS_NAME);
-                    assertNull(statsMapSecondMixedRound.get(MODEL_INDEX_STATUS_NAME));
-
-                    createModelSystemIndex();
-                }
-                break;
-            case UPGRADED:
-                Map<String, Object> statsMapUpgraded = getModelIndexHealthMetric(MODEL_INDEX_STATUS_NAME);
-                assertNotNull(statsMapUpgraded.get(MODEL_INDEX_STATUS_NAME));
-                assertNotNull(ClusterHealthStatus.fromString((String) statsMapUpgraded.get(MODEL_INDEX_STATUS_NAME)));
-
-                deleteKNNIndex(MODEL_INDEX_NAME);
-        }
+    // Validate if all the KNN Stats metrics are returned
+    public void testAllMetricStatsReturned() throws IOException {
+        Response response = getKnnStats(Collections.emptyList(), Collections.emptyList());
+        String responseBody = EntityUtils.toString(response.getEntity());
+        Map<String, Object> clusterStats = parseClusterStatsResponse(responseBody);
+        assertEquals(knnStats.getClusterStats().keySet(), clusterStats.keySet());
+        List<Map<String, Object>> nodeStats = parseNodeStatsResponse(responseBody);
+        assertEquals(knnStats.getNodeStats().keySet(), nodeStats.get(0).keySet());
     }
 
-    public Map<String, Object> getModelIndexHealthMetric(String modelIndexStatusName) throws IOException {
-        Response response = getKnnStats(Collections.emptyList(), Arrays.asList(modelIndexStatusName));
-        String responseBody = EntityUtils.toString(response.getEntity());
-        return createParser(XContentType.JSON.xContent(), responseBody).map();
+    // Verify if it returns failure for invalid metric
+    public void testInvalidMetricsStats() {
+        expectThrows(ResponseException.class, () -> getKnnStats(Collections.emptyList(), Collections.singletonList("invalid_metric")));
     }
 
 }
