4d9a0e6484d774b5d50658b8b4a4c44f93ace05a
Naveen Tatikonda <navtat@amazon.com>
2022-05-12 20:44:16 -0500

    Add Rolling-Upgrade BWC Tests (#386)
    * Add Rolling-Upgrade BWC Tests
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Address review comments
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    (cherry picked from commit 14380428b5b6f82d5425304b2e5641d57deca51f)

--- qa/rolling-upgrade/src/test/java/org/opensearch/knn/bwc/WarmupIT.java
+++ qa/rolling-upgrade/src/test/java/org/opensearch/knn/bwc/WarmupIT.java
@@ -6,16 +6,17 @@
 package org.opensearch.knn.bwc;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import static org.opensearch.knn.TestUtils.NODES_BWC_CLUSTER;
 
-public class IndexingIT extends AbstractRollingUpgradeTestCase {
+public class WarmupIT extends AbstractRollingUpgradeTestCase {
     private static final String TEST_FIELD = "test-field";
     private static final int DIMENSIONS = 5;
     private static final int K = 5;
     private static final int NUM_DOCS = 10;
 
-    public void testKNNDefaultIndexSettings() throws Exception {
+    public void testKNNWarmup() throws Exception {
         waitForClusterHealthGreen(NODES_BWC_CLUSTER);
         switch (getClusterType()) {
             case OLD:
@@ -27,33 +28,37 @@ public class IndexingIT extends AbstractRollingUpgradeTestCase {
                 int totalDocsCountMixed;
                 int docIdMixed;
                 if (isFirstMixedRound()) {
-                    totalDocsCountMixed = NUM_DOCS;
                     docIdMixed = NUM_DOCS;
-                } else {
                     totalDocsCountMixed = 2 * NUM_DOCS;
+                } else {
                     docIdMixed = 2 * NUM_DOCS;
+                    totalDocsCountMixed = 3 * NUM_DOCS;
                 }
-                validateKNNIndexingOnUpgrade(totalDocsCountMixed, docIdMixed);
+                validateKNNWarmupOnUpgrade(totalDocsCountMixed, docIdMixed);
                 break;
             case UPGRADED:
-                int totalDocsCountUpgraded = 3 * NUM_DOCS;
                 int docIdUpgraded = 3 * NUM_DOCS;
-                validateKNNIndexingOnUpgrade(totalDocsCountUpgraded, docIdUpgraded);
-
-                int updatedDocIdUpgraded = docIdUpgraded + NUM_DOCS;
-                forceMergeKnnIndex(testIndex);
-                validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, updatedDocIdUpgraded, K);
+                int totalDocsCountUpgraded = 4 * NUM_DOCS;
+                validateKNNWarmupOnUpgrade(totalDocsCountUpgraded, docIdUpgraded);
 
                 deleteKNNIndex(testIndex);
         }
+
     }
 
-    // validation steps for indexing after upgrading each node from old version to new version
-    public void validateKNNIndexingOnUpgrade(int totalDocsCount, int docId) throws IOException {
-        validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, totalDocsCount, K);
+    // validation steps for KNN Warmup after upgrading each node from old version to new version
+    public void validateKNNWarmupOnUpgrade(int totalDocsCount, int docId) throws IOException {
+        int graphCount = getTotalGraphsInCache();
+        knnWarmup(Collections.singletonList(testIndex));
+        assertTrue(getTotalGraphsInCache() > graphCount);
+
         addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, docId, NUM_DOCS);
 
-        totalDocsCount = totalDocsCount + NUM_DOCS;
+        int updatedGraphCount = getTotalGraphsInCache();
+        knnWarmup(Collections.singletonList(testIndex));
+        assertTrue(getTotalGraphsInCache() > updatedGraphCount);
+
         validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, totalDocsCount, K);
     }
+
 }
