jmazanec15/k-NN-1

Refactor library and engine structure
Refactors library and engine structure to be less coupled. Pulls classes
and interfaces out into individual files. Additionally, renames package
from util to engine.

Signed-off-by: John Mazanec <jmazane@amazon.com>

src/main/java/org/opensearch/knn/index/engine/LibVersion.java
@@ -1,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.engine;
+
+interface LibVersion {
+
+    String indexLibraryVersion();
+
+    String getBuildVersion();
+}

src/main/java/org/opensearch/knn/index/engine/NativeLibrary.java
@@ -1,0 +1,136 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.engine;
+
+import org.opensearch.common.ValidationException;
+import org.opensearch.knn.common.KNNConstants;
+import org.opensearch.knn.index.KNNMethod;
+import org.opensearch.knn.index.KNNMethodContext;
+import org.opensearch.knn.index.SpaceType;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+/**
+ * Abstract implementation of KNNLibrary. It contains several default methods and fields that
+ * are common across different underlying libraries.
+ */
+abstract class NativeLibrary implements KNNLibrary {
+    protected Map<String, KNNMethod> methods;
+    private final Map<SpaceType, Function<Float, Float>> scoreTranslation;
+    private final String latestLibraryBuildVersion;
+    private final String latestLibraryVersion;
+    private final String extension;
+    private final AtomicBoolean initialized;
+
+    /**
+     * Constructor for NativeLibrary
+     *
+     * @param methods map of methods the native library supports
+     * @param scoreTranslation Map of translation of space type to scores returned by the library
+     * @param latestLibraryBuildVersion String representation of latest build version of the library
+     * @param latestLibraryVersion String representation of latest version of the library
+     * @param extension String representing the extension that library files should use
+     */
+    public NativeLibrary(
+        Map<String, KNNMethod> methods,
+        Map<SpaceType, Function<Float, Float>> scoreTranslation,
+        String latestLibraryBuildVersion,
+        String latestLibraryVersion,
+        String extension
+    ) {
+        this.methods = methods;
+        this.scoreTranslation = scoreTranslation;
+        this.latestLibraryBuildVersion = latestLibraryBuildVersion;
+        this.latestLibraryVersion = latestLibraryVersion;
+        this.extension = extension;
+        this.initialized = new AtomicBoolean(false);
+    }
+
+    @Override
+    public String getLatestBuildVersion() {
+        return this.latestLibraryBuildVersion;
+    }
+
+    @Override
+    public String getLatestLibVersion() {
+        return this.latestLibraryVersion;
+    }
+
+    @Override
+    public String getExtension() {
+        return this.extension;
+    }
+
+    @Override
+    public String getCompoundExtension() {
+        return getExtension() + KNNConstants.COMPOUND_EXTENSION;
+    }
+
+    @Override
+    public KNNMethod getMethod(String methodName) {
+        KNNMethod method = methods.get(methodName);
+        if (method != null) {
+            return method;
+        }
+        throw new IllegalArgumentException("Invalid method name: " + methodName);
+    }
+
+    @Override
+    public float score(float rawScore, SpaceType spaceType) {
+        if (this.scoreTranslation.containsKey(spaceType)) {
+            return this.scoreTranslation.get(spaceType).apply(rawScore);
+        }
+
+        return spaceType.scoreTranslation(rawScore);
+    }
+
+    @Override
+    public ValidationException validateMethod(KNNMethodContext knnMethodContext) {
+        String methodName = knnMethodContext.getMethodComponent().getName();
+        return getMethod(methodName).validate(knnMethodContext);
+    }
+
+    @Override
+    public boolean isTrainingRequired(KNNMethodContext knnMethodContext) {
+        String methodName = knnMethodContext.getMethodComponent().getName();
+        return getMethod(methodName).isTrainingRequired(knnMethodContext);
+    }
+
+    @Override
+    public int estimateOverheadInKB(KNNMethodContext knnMethodContext, int dimension) {
+        String methodName = knnMethodContext.getMethodComponent().getName();
+        return getMethod(methodName).estimateOverheadInKB(knnMethodContext, dimension);
+    }
+
+    @Override
+    public Map<String, Object> getMethodAsMap(KNNMethodContext knnMethodContext) {
+        KNNMethod knnMethod = methods.get(knnMethodContext.getMethodComponent().getName());
+
+        if (knnMethod == null) {
+            throw new IllegalArgumentException("Invalid method name: " + knnMethodContext.getMethodComponent().getName());
+        }
+
+        return knnMethod.getAsMap(knnMethodContext);
+    }
+
+    @Override
+    public Boolean isInitialized() {
+        return initialized.get();
+    }
+
+    @Override
+    public void setInitialized(Boolean isInitialized) {
+        initialized.set(isInitialized);
+    }
+}

src/main/java/org/opensearch/knn/index/engine/Nmslib.java
@@ -1,0 +1,101 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.engine;
+
+import com.google.common.collect.ImmutableMap;
+import org.opensearch.knn.common.KNNConstants;
+import org.opensearch.knn.index.*;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+
+import static org.opensearch.knn.common.KNNConstants.*;
+import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_EF_CONSTRUCTION;
+
+/**
+ * Implements NativeLibrary for the nmslib native library
+ */
+class Nmslib extends NativeLibrary {
+    // ======================================
+    // Constants pertaining to nmslib library
+    // ======================================
+    public final static String HNSW_LIB_NAME = "hnsw";
+    public final static String EXTENSION = ".hnsw";
+
+    public final static Map<String, KNNMethod> METHODS = ImmutableMap.of(
+        METHOD_HNSW,
+        KNNMethod.Builder.builder(
+            MethodComponent.Builder.builder(HNSW_LIB_NAME)
+                .addParameter(
+                    METHOD_PARAMETER_M,
+                    new Parameter.IntegerParameter(METHOD_PARAMETER_M, KNNSettings.INDEX_KNN_DEFAULT_ALGO_PARAM_M, v -> v > 0)
+                )
+                .addParameter(
+                    METHOD_PARAMETER_EF_CONSTRUCTION,
+                    new Parameter.IntegerParameter(
+                        METHOD_PARAMETER_EF_CONSTRUCTION,
+                        KNNSettings.INDEX_KNN_DEFAULT_ALGO_PARAM_EF_CONSTRUCTION,
+                        v -> v > 0
+                    )
+                )
+                .build()
+        ).addSpaces(SpaceType.L2, SpaceType.L1, SpaceType.LINF, SpaceType.COSINESIMIL, SpaceType.INNER_PRODUCT).build()
+    );
+
+    public final static Nmslib INSTANCE = new Nmslib(
+        METHODS,
+        Collections.emptyMap(),
+        Version.LATEST.getBuildVersion(),
+        Version.LATEST.indexLibraryVersion(),
+        EXTENSION
+    );
+
+    /**
+     * Constructor for Nmslib
+     *
+     * @param methods Set of methods the native library supports
+     * @param scoreTranslation Map of translation of space type to scores returned by the library
+     * @param latestLibraryBuildVersion String representation of latest build version of the library
+     * @param latestLibraryVersion String representation of latest version of the library
+     * @param extension String representing the extension that library files should use
+     */
+    private Nmslib(
+        Map<String, KNNMethod> methods,
+        Map<SpaceType, Function<Float, Float>> scoreTranslation,
+        String latestLibraryBuildVersion,
+        String latestLibraryVersion,
+        String extension
+    ) {
+        super(methods, scoreTranslation, latestLibraryBuildVersion, latestLibraryVersion, extension);
+    }
+
+    public enum Version implements LibVersion {
+        V2011("2011");
+
+        public static final Version LATEST = V2011;
+
+        private final String buildVersion;
+
+        Version(String buildVersion) {
+            this.buildVersion = buildVersion;
+        }
+
+        public String indexLibraryVersion() {
+            return KNNConstants.NMSLIB_JNI_LIBRARY_NAME;
+        }
+
+        public String getBuildVersion() {
+            return buildVersion;
+        }
+    }
+}

src/test/java/org/opensearch/knn/index/engine/FaissTests.java
@@ -1,0 +1,74 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.engine;
+
+import org.opensearch.common.xcontent.XContentBuilder;
+import org.opensearch.common.xcontent.XContentFactory;
+import org.opensearch.knn.KNNTestCase;
+import org.opensearch.knn.index.MethodComponent;
+import org.opensearch.knn.index.MethodComponentContext;
+import org.opensearch.knn.index.Parameter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.opensearch.knn.common.KNNConstants.INDEX_DESCRIPTION_PARAMETER;
+import static org.opensearch.knn.common.KNNConstants.NAME;
+import static org.opensearch.knn.common.KNNConstants.PARAMETERS;
+
+public class FaissTests extends KNNTestCase {
+
+    public void testMethodAsMapBuilder() throws IOException {
+        String methodName = "test-method";
+        String methodDescription = "test-description";
+        String parameter1 = "test-parameter-1";
+        Integer value1 = 10;
+        Integer defaultValue1 = 1;
+        String parameter2 = "test-parameter-2";
+        Integer value2 = 15;
+        Integer defaultValue2 = 2;
+        String parameter3 = "test-parameter-3";
+        Integer defaultValue3 = 3;
+        MethodComponent methodComponent = MethodComponent.Builder.builder(methodName)
+            .addParameter(parameter1, new Parameter.IntegerParameter(parameter1, defaultValue1, value -> value > 0))
+            .addParameter(parameter2, new Parameter.IntegerParameter(parameter2, defaultValue2, value -> value > 0))
+            .addParameter(parameter3, new Parameter.IntegerParameter(parameter3, defaultValue3, value -> value > 0))
+            .build();
+
+        XContentBuilder xContentBuilder = XContentFactory.jsonBuilder()
+            .startObject()
+            .field(NAME, methodName)
+            .startObject(PARAMETERS)
+            .field(parameter1, value1)
+            .field(parameter2, value2)
+            .endObject()
+            .endObject();
+        Map<String, Object> in = xContentBuilderToMap(xContentBuilder);
+        MethodComponentContext methodComponentContext = MethodComponentContext.parse(in);
+
+        Map<String, Object> expectedParametersMap = new HashMap<>(methodComponentContext.getParameters());
+        expectedParametersMap.put(parameter3, defaultValue3);
+        expectedParametersMap.remove(parameter1);
+        Map<String, Object> expectedMap = new HashMap<>();
+        expectedMap.put(PARAMETERS, expectedParametersMap);
+        expectedMap.put(NAME, methodName);
+        expectedMap.put(INDEX_DESCRIPTION_PARAMETER, methodDescription + value1);
+
+        Map<String, Object> methodAsMap = Faiss.MethodAsMapBuilder.builder(methodDescription, methodComponent, methodComponentContext)
+            .addParameter(parameter1, "", "")
+            .build();
+
+        assertEquals(expectedMap, methodAsMap);
+    }
+
+}

opensearch-project/k-NN

src/main/java/org/opensearch/knn/index/util/LibVersion.java
@@ -1,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.util;
+
+interface LibVersion {
+
+    String indexLibraryVersion();
+
+    String getBuildVersion();
+}

src/main/java/org/opensearch/knn/index/util/NativeLibrary.java
@@ -1,0 +1,136 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.util;
+
+import org.opensearch.common.ValidationException;
+import org.opensearch.knn.common.KNNConstants;
+import org.opensearch.knn.index.KNNMethod;
+import org.opensearch.knn.index.KNNMethodContext;
+import org.opensearch.knn.index.SpaceType;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+/**
+ * Abstract implementation of KNNLibrary. It contains several default methods and fields that
+ * are common across different underlying libraries.
+ */
+abstract class NativeLibrary implements KNNLibrary {
+    protected Map<String, KNNMethod> methods;
+    private final Map<SpaceType, Function<Float, Float>> scoreTranslation;
+    private final String latestLibraryBuildVersion;
+    private final String latestLibraryVersion;
+    private final String extension;
+    private final AtomicBoolean initialized;
+
+    /**
+     * Constructor for NativeLibrary
+     *
+     * @param methods map of methods the native library supports
+     * @param scoreTranslation Map of translation of space type to scores returned by the library
+     * @param latestLibraryBuildVersion String representation of latest build version of the library
+     * @param latestLibraryVersion String representation of latest version of the library
+     * @param extension String representing the extension that library files should use
+     */
+    public NativeLibrary(
+        Map<String, KNNMethod> methods,
+        Map<SpaceType, Function<Float, Float>> scoreTranslation,
+        String latestLibraryBuildVersion,
+        String latestLibraryVersion,
+        String extension
+    ) {
+        this.methods = methods;
+        this.scoreTranslation = scoreTranslation;
+        this.latestLibraryBuildVersion = latestLibraryBuildVersion;
+        this.latestLibraryVersion = latestLibraryVersion;
+        this.extension = extension;
+        this.initialized = new AtomicBoolean(false);
+    }
+
+    @Override
+    public String getLatestBuildVersion() {
+        return this.latestLibraryBuildVersion;
+    }
+
+    @Override
+    public String getLatestLibVersion() {
+        return this.latestLibraryVersion;
+    }
+
+    @Override
+    public String getExtension() {
+        return this.extension;
+    }
+
+    @Override
+    public String getCompoundExtension() {
+        return getExtension() + KNNConstants.COMPOUND_EXTENSION;
+    }
+
+    @Override
+    public KNNMethod getMethod(String methodName) {
+        KNNMethod method = methods.get(methodName);
+        if (method != null) {
+            return method;
+        }
+        throw new IllegalArgumentException("Invalid method name: " + methodName);
+    }
+
+    @Override
+    public float score(float rawScore, SpaceType spaceType) {
+        if (this.scoreTranslation.containsKey(spaceType)) {
+            return this.scoreTranslation.get(spaceType).apply(rawScore);
+        }
+
+        return spaceType.scoreTranslation(rawScore);
+    }
+
+    @Override
+    public ValidationException validateMethod(KNNMethodContext knnMethodContext) {
+        String methodName = knnMethodContext.getMethodComponent().getName();
+        return getMethod(methodName).validate(knnMethodContext);
+    }
+
+    @Override
+    public boolean isTrainingRequired(KNNMethodContext knnMethodContext) {
+        String methodName = knnMethodContext.getMethodComponent().getName();
+        return getMethod(methodName).isTrainingRequired(knnMethodContext);
+    }
+
+    @Override
+    public int estimateOverheadInKB(KNNMethodContext knnMethodContext, int dimension) {
+        String methodName = knnMethodContext.getMethodComponent().getName();
+        return getMethod(methodName).estimateOverheadInKB(knnMethodContext, dimension);
+    }
+
+    @Override
+    public Map<String, Object> getMethodAsMap(KNNMethodContext knnMethodContext) {
+        KNNMethod knnMethod = methods.get(knnMethodContext.getMethodComponent().getName());
+
+        if (knnMethod == null) {
+            throw new IllegalArgumentException("Invalid method name: " + knnMethodContext.getMethodComponent().getName());
+        }
+
+        return knnMethod.getAsMap(knnMethodContext);
+    }
+
+    @Override
+    public Boolean isInitialized() {
+        return initialized.get();
+    }
+
+    @Override
+    public void setInitialized(Boolean isInitialized) {
+        initialized.set(isInitialized);
+    }
+}

src/main/java/org/opensearch/knn/index/util/Nmslib.java
@@ -1,0 +1,101 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.util;
+
+import com.google.common.collect.ImmutableMap;
+import org.opensearch.knn.common.KNNConstants;
+import org.opensearch.knn.index.*;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+
+import static org.opensearch.knn.common.KNNConstants.*;
+import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_EF_CONSTRUCTION;
+
+/**
+ * Implements NativeLibrary for the nmslib native library
+ */
+class Nmslib extends NativeLibrary {
+    // ======================================
+    // Constants pertaining to nmslib library
+    // ======================================
+    public final static String HNSW_LIB_NAME = "hnsw";
+    public final static String EXTENSION = ".hnsw";
+
+    public final static Map<String, KNNMethod> METHODS = ImmutableMap.of(
+        METHOD_HNSW,
+        KNNMethod.Builder.builder(
+            MethodComponent.Builder.builder(HNSW_LIB_NAME)
+                .addParameter(
+                    METHOD_PARAMETER_M,
+                    new Parameter.IntegerParameter(METHOD_PARAMETER_M, KNNSettings.INDEX_KNN_DEFAULT_ALGO_PARAM_M, v -> v > 0)
+                )
+                .addParameter(
+                    METHOD_PARAMETER_EF_CONSTRUCTION,
+                    new Parameter.IntegerParameter(
+                        METHOD_PARAMETER_EF_CONSTRUCTION,
+                        KNNSettings.INDEX_KNN_DEFAULT_ALGO_PARAM_EF_CONSTRUCTION,
+                        v -> v > 0
+                    )
+                )
+                .build()
+        ).addSpaces(SpaceType.L2, SpaceType.L1, SpaceType.LINF, SpaceType.COSINESIMIL, SpaceType.INNER_PRODUCT).build()
+    );
+
+    public final static Nmslib INSTANCE = new Nmslib(
+        METHODS,
+        Collections.emptyMap(),
+        Version.LATEST.getBuildVersion(),
+        Version.LATEST.indexLibraryVersion(),
+        EXTENSION
+    );
+
+    /**
+     * Constructor for Nmslib
+     *
+     * @param methods Set of methods the native library supports
+     * @param scoreTranslation Map of translation of space type to scores returned by the library
+     * @param latestLibraryBuildVersion String representation of latest build version of the library
+     * @param latestLibraryVersion String representation of latest version of the library
+     * @param extension String representing the extension that library files should use
+     */
+    private Nmslib(
+        Map<String, KNNMethod> methods,
+        Map<SpaceType, Function<Float, Float>> scoreTranslation,
+        String latestLibraryBuildVersion,
+        String latestLibraryVersion,
+        String extension
+    ) {
+        super(methods, scoreTranslation, latestLibraryBuildVersion, latestLibraryVersion, extension);
+    }
+
+    public enum Version implements LibVersion {
+        V2011("2011");
+
+        public static final Version LATEST = V2011;
+
+        private final String buildVersion;
+
+        Version(String buildVersion) {
+            this.buildVersion = buildVersion;
+        }
+
+        public String indexLibraryVersion() {
+            return KNNConstants.NMSLIB_JNI_LIBRARY_NAME;
+        }
+
+        public String getBuildVersion() {
+            return buildVersion;
+        }
+    }
+}

src/test/java/org/opensearch/knn/index/util/FaissTests.java
@@ -1,0 +1,74 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.util;
+
+import org.opensearch.common.xcontent.XContentBuilder;
+import org.opensearch.common.xcontent.XContentFactory;
+import org.opensearch.knn.KNNTestCase;
+import org.opensearch.knn.index.MethodComponent;
+import org.opensearch.knn.index.MethodComponentContext;
+import org.opensearch.knn.index.Parameter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.opensearch.knn.common.KNNConstants.INDEX_DESCRIPTION_PARAMETER;
+import static org.opensearch.knn.common.KNNConstants.NAME;
+import static org.opensearch.knn.common.KNNConstants.PARAMETERS;
+
+public class FaissTests extends KNNTestCase {
+
+    public void testMethodAsMapBuilder() throws IOException {
+        String methodName = "test-method";
+        String methodDescription = "test-description";
+        String parameter1 = "test-parameter-1";
+        Integer value1 = 10;
+        Integer defaultValue1 = 1;
+        String parameter2 = "test-parameter-2";
+        Integer value2 = 15;
+        Integer defaultValue2 = 2;
+        String parameter3 = "test-parameter-3";
+        Integer defaultValue3 = 3;
+        MethodComponent methodComponent = MethodComponent.Builder.builder(methodName)
+            .addParameter(parameter1, new Parameter.IntegerParameter(parameter1, defaultValue1, value -> value > 0))
+            .addParameter(parameter2, new Parameter.IntegerParameter(parameter2, defaultValue2, value -> value > 0))
+            .addParameter(parameter3, new Parameter.IntegerParameter(parameter3, defaultValue3, value -> value > 0))
+            .build();
+
+        XContentBuilder xContentBuilder = XContentFactory.jsonBuilder()
+            .startObject()
+            .field(NAME, methodName)
+            .startObject(PARAMETERS)
+            .field(parameter1, value1)
+            .field(parameter2, value2)
+            .endObject()
+            .endObject();
+        Map<String, Object> in = xContentBuilderToMap(xContentBuilder);
+        MethodComponentContext methodComponentContext = MethodComponentContext.parse(in);
+
+        Map<String, Object> expectedParametersMap = new HashMap<>(methodComponentContext.getParameters());
+        expectedParametersMap.put(parameter3, defaultValue3);
+        expectedParametersMap.remove(parameter1);
+        Map<String, Object> expectedMap = new HashMap<>();
+        expectedMap.put(PARAMETERS, expectedParametersMap);
+        expectedMap.put(NAME, methodName);
+        expectedMap.put(INDEX_DESCRIPTION_PARAMETER, methodDescription + value1);
+
+        Map<String, Object> methodAsMap = Faiss.MethodAsMapBuilder.builder(methodDescription, methodComponent, methodComponentContext)
+            .addParameter(parameter1, "", "")
+            .build();
+
+        assertEquals(expectedMap, methodAsMap);
+    }
+
+}