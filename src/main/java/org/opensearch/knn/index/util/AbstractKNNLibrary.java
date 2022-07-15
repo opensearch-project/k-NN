3b8d14900530f3937b7c2ffb6e495480c1d1b6e6
John Mazanec <jmazane@amazon.com>
2022-07-15 15:58:32 -0700

    Add Lucene as a KNNEngine (#452)
    Adds lucene as another k-NN engine that can be used. Adds one supported
    method to the engine, "hnsw", with parameters "ef_construction" and "m".
    In addition, adds some layers of abstraction to reduce code
    depuplication.
    
    Signed-off-by: John Mazanec <jmazane@amazon.com>
    (cherry picked from commit 7a76d11cd049ebe546989c78fc86e57da8d208f7)

---
+++ src/main/java/org/opensearch/knn/index/util/AbstractKNNLibrary.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.knn.index.util;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.opensearch.common.ValidationException;
+import org.opensearch.knn.index.KNNMethod;
+import org.opensearch.knn.index.KNNMethodContext;
+
+import java.util.Map;
+
+/**
+ * AbstractKNNLibrary implements common functionality shared between libraries
+ */
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+public abstract class AbstractKNNLibrary implements KNNLibrary {
+
+    protected final Map<String, KNNMethod> methods;
+    @Getter
+    protected final String version;
+
+    @Override
+    public KNNMethod getMethod(String methodName) {
+        KNNMethod method = methods.get(methodName);
+        if (method == null) {
+            throw new IllegalArgumentException(String.format("Invalid method name: %s", methodName));
+        }
+        return method;
+    }
+
+    @Override
+    public ValidationException validateMethod(KNNMethodContext knnMethodContext) {
+        String methodName = knnMethodContext.getMethodComponent().getName();
+        return getMethod(methodName).validate(knnMethodContext);
+    }
+
+    @Override
+    public boolean isTrainingRequired(KNNMethodContext knnMethodContext) {
+        String methodName = knnMethodContext.getMethodComponent().getName();
+        return getMethod(methodName).isTrainingRequired(knnMethodContext);
+    }
+
+    @Override
+    public Map<String, Object> getMethodAsMap(KNNMethodContext knnMethodContext) {
+        KNNMethod knnMethod = methods.get(knnMethodContext.getMethodComponent().getName());
+
+        if (knnMethod == null) {
+            throw new IllegalArgumentException(String.format("Invalid method name: %s", knnMethodContext.getMethodComponent().getName()));
+        }
+
+        return knnMethod.getAsMap(knnMethodContext);
+    }
+}
---
+++ src/test/java/org/opensearch/knn/index/util/LuceneTests.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.knn.index.util;
+
+import org.opensearch.common.xcontent.XContentBuilder;
+import org.opensearch.common.xcontent.XContentFactory;
+import org.opensearch.knn.KNNTestCase;
+import org.opensearch.knn.index.KNNMethod;
+import org.opensearch.knn.index.KNNMethodContext;
+import org.opensearch.knn.index.SpaceType;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.opensearch.knn.common.KNNConstants.METHOD_HNSW;
+import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_EF_CONSTRUCTION;
+import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_M;
+import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_SPACE_TYPE;
+import static org.opensearch.knn.common.KNNConstants.NAME;
+import static org.opensearch.knn.common.KNNConstants.PARAMETERS;
+
+public class LuceneTests extends KNNTestCase {
+
+    public void testLucenHNSWMethod() throws IOException {
+        KNNMethod luceneHNSW = Lucene.INSTANCE.getMethod(METHOD_HNSW);
+        assertNotNull(luceneHNSW);
+
+        int efConstruction = 100;
+        int m = 17;
+        XContentBuilder xContentBuilder = XContentFactory.jsonBuilder()
+            .startObject()
+            .field(NAME, METHOD_HNSW)
+            .field(METHOD_PARAMETER_SPACE_TYPE, SpaceType.L2.getValue())
+            .startObject(PARAMETERS)
+            .field(METHOD_PARAMETER_EF_CONSTRUCTION, efConstruction)
+            .field(METHOD_PARAMETER_M, m)
+            .endObject()
+            .endObject();
+        Map<String, Object> in = xContentBuilderToMap(xContentBuilder);
+        KNNMethodContext knnMethodContext1 = KNNMethodContext.parse(in);
+        assertNull(luceneHNSW.validate(knnMethodContext1));
+
+        // Invalid parameter
+        String invalidParameter = "invalid";
+        xContentBuilder = XContentFactory.jsonBuilder()
+            .startObject()
+            .field(NAME, METHOD_HNSW)
+            .startObject(PARAMETERS)
+            .field(invalidParameter, 10)
+            .endObject()
+            .endObject();
+        in = xContentBuilderToMap(xContentBuilder);
+        KNNMethodContext knnMethodContext2 = KNNMethodContext.parse(in);
+        assertNotNull(luceneHNSW.validate(knnMethodContext2));
+
+        // Valid parameter, invalid value
+        int invalidEfConstruction = -1;
+        xContentBuilder = XContentFactory.jsonBuilder()
+            .startObject()
+            .field(NAME, METHOD_HNSW)
+            .startObject(PARAMETERS)
+            .field(METHOD_PARAMETER_EF_CONSTRUCTION, invalidEfConstruction)
+            .endObject()
+            .endObject();
+        in = xContentBuilderToMap(xContentBuilder);
+        KNNMethodContext knnMethodContext3 = KNNMethodContext.parse(in);
+        assertNotNull(luceneHNSW.validate(knnMethodContext3));
+
+        // Unsupported space type
+        SpaceType invalidSpaceType = SpaceType.LINF; // Not currently supported
+        xContentBuilder = XContentFactory.jsonBuilder()
+            .startObject()
+            .field(NAME, METHOD_HNSW)
+            .field(METHOD_PARAMETER_SPACE_TYPE, invalidSpaceType.getValue())
+            .endObject();
+        in = xContentBuilderToMap(xContentBuilder);
+        KNNMethodContext knnMethodContext4 = KNNMethodContext.parse(in);
+        assertNotNull(luceneHNSW.validate(knnMethodContext4));
+    }
+
+    public void testGetExtension() {
+        Lucene luceneLibrary = new Lucene(Collections.emptyMap(), "");
+        expectThrows(UnsupportedOperationException.class, luceneLibrary::getExtension);
+    }
+
+    public void testGetCompundExtension() {
+        Lucene luceneLibrary = new Lucene(Collections.emptyMap(), "");
+        expectThrows(UnsupportedOperationException.class, luceneLibrary::getCompoundExtension);
+    }
+
+    public void testScore() {
+        Lucene luceneLibrary = new Lucene(Collections.emptyMap(), "");
+        float rawScore = 10.0f;
+        assertEquals(rawScore, luceneLibrary.score(rawScore, SpaceType.DEFAULT), 0.001);
+    }
+
+    public void testIsInitialized() {
+        Lucene luceneLibrary = new Lucene(Collections.emptyMap(), "");
+        assertTrue(luceneLibrary.isInitialized());
+    }
+
+    public void testSetInitialized() {
+        Lucene luceneLibrary = new Lucene(Collections.emptyMap(), "");
+        expectThrows(UnsupportedOperationException.class, () -> luceneLibrary.setInitialized(true));
+    }
+}
