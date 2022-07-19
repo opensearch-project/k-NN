a4c074d51f8186810ace286583c77345e6bb6f12
John Mazanec <jmazane@amazon.com>
2022-07-19 10:56:07 -0700

    Fix jni path
    Signed-off-by: John Mazanec <jmazane@amazon.com>

--- jni/include/org_opensearch_knn_jni_NmslibService.h
+++ jni/include/org_opensearch_knn_jni_NmslibService.h
@@ -37,7 +37,7 @@ JNIEXPORT jlong JNICALL Java_org_opensearch_knn_jni_NmslibService_loadIndex
 /*
  * Class:     org_opensearch_knn_jni_NmslibService
  * Method:    queryIndex
- * Signature: (J[FI)[Lorg/opensearch/knn/index/KNNQueryResult;
+ * Signature: (J[FI)[Lorg/opensearch/knn/index/query/KNNQueryResult;
  */
 JNIEXPORT jobjectArray JNICALL Java_org_opensearch_knn_jni_NmslibService_queryIndex
   (JNIEnv *, jclass, jlong, jfloatArray, jint);
