0270fcbc094d694380aee6dd9e8380ca66bfbbce
Daniel Widdis <widdis@gmail.com>
2022-09-22 12:17:26 -0700

    Test that at least one supported context exists
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/test/java/org/opensearch/knn/plugin/script/KNNScoringScriptEngineTests.java
+++ src/test/java/org/opensearch/knn/plugin/script/KNNScoringScriptEngineTests.java
@@ -6,15 +6,19 @@
 package org.opensearch.knn.plugin.script;
 
 import java.io.IOException;
+import java.util.Set;
 
 import org.opensearch.knn.KNNTestCase;
+import org.opensearch.script.ScriptContext;
 import org.opensearch.script.ScriptEngine;
 
 public class KNNScoringScriptEngineTests extends KNNTestCase {
 
     public void testGetSupportedContexts() throws IOException {
         try (ScriptEngine engine = new KNNScoringScriptEngine()) {
-            assertNotNull(engine.getSupportedContexts());
+            Set<ScriptContext<?>> supportedContexts = engine.getSupportedContexts();
+            assertNotNull(supportedContexts);
+            assertFalse(supportedContexts.isEmpty());
         }
     }
 
