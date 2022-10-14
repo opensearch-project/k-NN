9b32e17f6b673e3435978f1e3f1289ce1beaa947
Martin Gaievski <gaievski@amazon.com>
2022-10-14 14:10:02 -0700

    Read min cluster version directly from DiscoveryNodes (#581)
    * Simplify min cluster version lookup
    
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

--- src/test/java/org/opensearch/knn/index/KNNClusterTestUtils.java
+++ src/test/java/org/opensearch/knn/index/KNNClusterTestUtils.java
@@ -7,16 +7,11 @@ package org.opensearch.knn.index;
 
 import org.opensearch.Version;
 import org.opensearch.cluster.ClusterState;
-import org.opensearch.cluster.node.DiscoveryNode;
 import org.opensearch.cluster.node.DiscoveryNodes;
 import org.opensearch.cluster.service.ClusterService;
-import org.opensearch.common.collect.ImmutableOpenMap;
-
-import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.opensearch.test.OpenSearchTestCase.randomAlphaOfLength;
 
 /**
  * Collection of util methods required for testing and related to OpenSearch cluster setup and functionality
@@ -25,24 +20,16 @@ public class KNNClusterTestUtils {
 
     /**
      * Create new mock for ClusterService
-     * @param versions list of versions for cluster nodes
+     * @param version min version for cluster nodes
      * @return
      */
-    public static ClusterService mockClusterService(final List<Version> versions) {
+    public static ClusterService mockClusterService(final Version version) {
         ClusterService clusterService = mock(ClusterService.class);
         ClusterState clusterState = mock(ClusterState.class);
         when(clusterService.state()).thenReturn(clusterState);
         DiscoveryNodes discoveryNodes = mock(DiscoveryNodes.class);
         when(clusterState.getNodes()).thenReturn(discoveryNodes);
-        ImmutableOpenMap.Builder<String, DiscoveryNode> builder = ImmutableOpenMap.builder();
-        for (Version version : versions) {
-            DiscoveryNode clusterNode = mock(DiscoveryNode.class);
-            when(clusterNode.getVersion()).thenReturn(version);
-            builder.put(randomAlphaOfLength(10), clusterNode);
-        }
-        ImmutableOpenMap<String, DiscoveryNode> mapOfNodes = builder.build();
-        when(discoveryNodes.getNodes()).thenReturn(mapOfNodes);
-
+        when(discoveryNodes.getMinNodeVersion()).thenReturn(version);
         return clusterService;
     }
 }
