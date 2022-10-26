4a98708a2059f2f035264146db67d95a8128b182
Naveen <navtat@amazon.com>
2022-10-26 04:26:11 +0000

    Remove Git Patch and add branching logic for CMakeLists.txt
    Signed-off-by: Naveen <navtat@amazon.com>

--- scripts/windowsScript.ps1
+++ scripts/windowsScript.ps1
@@ -6,16 +6,12 @@
 git submodule update --init -- jni/external/nmslib
 git submodule update --init -- jni/external/faiss
 
-git apply patches/windows/CMakeLists.patch --verbose
-
-# Validating if the CMakeLists patch is applied
-echo "Validating if the CMakeLists patch is applied"
-type jni/CMakeLists.txt | Select-String "Windows"
-
-# Replace '_MSC_VER' with '__MINGW32__'
+# _MSC_VER is a predefined macro which defines the version of Visual Studio Compiler
+# As we are using x86_64-w64-mingw32-gcc compiler we need to replace this macro with __MINGW32__
 (Get-Content jni/external/faiss/faiss/impl/index_read.cpp).replace('_MSC_VER', '__MINGW32__') | Set-Content jni/external/faiss/faiss/impl/index_read.cpp
 (Get-Content jni/external/faiss/faiss/impl/index_write.cpp).replace('_MSC_VER', '__MINGW32__') | Set-Content jni/external/faiss/faiss/impl/index_write.cpp
 
+# <sys/mman.h> is a Unix header and is not available on Windows. So, adding condition to include it if not running on Windows
 # Replace '#include <sys/mman.h>' with
 #  #ifndef __MINGW32__
 #    #include <sys/mman.h>
