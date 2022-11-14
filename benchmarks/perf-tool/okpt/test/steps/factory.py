79ae6c24b989dd6c41fc17878ae85255080163fc
Martin Gaievski <gaievski@amazon.com>
2022-11-14 14:48:15 -0800

    Adding benchmark workflow for queries with filters (#598)
    * Adding workflow for benchmarking queries with filters
    
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

--- benchmarks/perf-tool/okpt/test/steps/factory.py
+++ benchmarks/perf-tool/okpt/test/steps/factory.py
@@ -9,7 +9,7 @@ from okpt.io.config.parsers.base import ConfigurationError
 from okpt.test.steps.base import Step, StepConfig
 
 from okpt.test.steps.steps import CreateIndexStep, DisableRefreshStep, RefreshIndexStep, DeleteIndexStep, \
-    TrainModelStep, DeleteModelStep, ForceMergeStep, ClearCacheStep, IngestStep, QueryStep
+    TrainModelStep, DeleteModelStep, ForceMergeStep, ClearCacheStep, IngestStep, IngestMultiFieldStep, QueryStep, QueryWithFilterStep
 
 
 def create_step(step_config: StepConfig) -> Step:
@@ -27,8 +27,12 @@ def create_step(step_config: StepConfig) -> Step:
         return DeleteIndexStep(step_config)
     elif step_config.step_name == IngestStep.label:
         return IngestStep(step_config)
+    elif step_config.step_name == IngestMultiFieldStep.label:
+        return IngestMultiFieldStep(step_config)
     elif step_config.step_name == QueryStep.label:
         return QueryStep(step_config)
+    elif step_config.step_name == QueryWithFilterStep.label:
+        return QueryWithFilterStep(step_config)
     elif step_config.step_name == ForceMergeStep.label:
         return ForceMergeStep(step_config)
     elif step_config.step_name == ClearCacheStep.label:
