165f4000001abc452ee796d12109f0b07740749d
John Mazanec <jmazane@amazon.com>
2023-04-04 11:52:06 -0700

    Update exception comments
    Signed-off-by: John Mazanec <jmazane@amazon.com>

--- src/main/java/org/opensearch/knn/common/exception/DeleteModelWhenInTrainStateException.java
+++ src/main/java/org/opensearch/knn/common/exception/DeleteModelWhenInTrainStateException.java
@@ -9,7 +9,18 @@ import org.opensearch.OpenSearchException;
 import org.opensearch.common.logging.LoggerMessageFormat;
 import org.opensearch.rest.RestStatus;
 
+/**
+ * Exception thrown when a model is deleted while it is in the training state. The RestStatus associated with this
+ * exception should be a {@link RestStatus#CONFLICT} because the request cannot be deleted due to the model being in
+ * the training state.
+ */
 public class DeleteModelWhenInTrainStateException extends OpenSearchException {
+    /**
+     * Constructor
+     *
+     * @param msg detailed exception message
+     * @param args arguments of the message
+     */
     public DeleteModelWhenInTrainStateException(String msg, Object... args) {
         super(LoggerMessageFormat.format(msg, args));
     }
