0ab4b7c862006686bee83528def4683dc6a469cb
Martin Gaievski <gaievski@amazon.com>
2023-04-04 18:30:21 -0700

    Support .opensearch-knn-model index as system index with security enabled (#827)
    * Add support for integ tests on secured cluster
    
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>
    (cherry picked from commit b94b030afc74efc1fae5c5c7528edc25974a3fec)
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>
    (cherry picked from commit 99300b11a6f6a3cf4f05e570e23d59cca585b2d1)

--- src/testFixtures/java/org/opensearch/knn/ODFERestTestCase.java
+++ src/testFixtures/java/org/opensearch/knn/ODFERestTestCase.java
@@ -5,13 +5,6 @@
 
 package org.opensearch.knn;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -21,29 +14,62 @@ import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.After;
+import org.opensearch.action.search.SearchResponse;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestClientBuilder;
+import org.opensearch.common.Strings;
+import org.opensearch.common.io.PathUtils;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.unit.TimeValue;
 import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.core.xcontent.DeprecationHandler;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.core.xcontent.XContentParser;
 import org.opensearch.common.xcontent.XContentType;
+import org.opensearch.commons.rest.SecureRestClientBuilder;
+import org.opensearch.knn.plugin.KNNPlugin;
+import org.opensearch.rest.RestStatus;
+import org.opensearch.search.SearchHit;
 import org.opensearch.test.rest.OpenSearchRestTestCase;
-import org.junit.After;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.opensearch.commons.ConfigConstants.OPENSEARCH_SECURITY_SSL_HTTP_ENABLED;
+import static org.opensearch.commons.ConfigConstants.OPENSEARCH_SECURITY_SSL_HTTP_KEYSTORE_FILEPATH;
+import static org.opensearch.commons.ConfigConstants.OPENSEARCH_SECURITY_SSL_HTTP_KEYSTORE_KEYPASSWORD;
+import static org.opensearch.commons.ConfigConstants.OPENSEARCH_SECURITY_SSL_HTTP_KEYSTORE_PASSWORD;
+import static org.opensearch.commons.ConfigConstants.OPENSEARCH_SECURITY_SSL_HTTP_PEMCERT_FILEPATH;
 import static org.opensearch.knn.TestUtils.KNN_BWC_PREFIX;
 import static org.opensearch.knn.TestUtils.OPENDISTRO_SECURITY;
+import static org.opensearch.knn.TestUtils.OPENSEARCH_SYSTEM_INDEX_PREFIX;
+import static org.opensearch.knn.TestUtils.SECURITY_AUDITLOG_PREFIX;
 import static org.opensearch.knn.TestUtils.SKIP_DELETE_MODEL_INDEX;
+import static org.opensearch.knn.common.KNNConstants.MODELS;
 import static org.opensearch.knn.common.KNNConstants.MODEL_INDEX_NAME;
 
 /**
  * ODFE integration test base class to support both security disabled and enabled ODFE cluster.
  */
 public abstract class ODFERestTestCase extends OpenSearchRestTestCase {
+
+    private final Set<String> IMMUTABLE_INDEX_PREFIXES = Set.of(KNN_BWC_PREFIX, SECURITY_AUDITLOG_PREFIX, OPENSEARCH_SYSTEM_INDEX_PREFIX);
 
     protected boolean isHttps() {
         boolean isHttps = Optional.ofNullable(System.getProperty("https")).map("true"::equalsIgnoreCase).orElse(false);
@@ -66,7 +92,22 @@ public abstract class ODFERestTestCase extends OpenSearchRestTestCase {
     protected RestClient buildClient(Settings settings, HttpHost[] hosts) throws IOException {
         RestClientBuilder builder = RestClient.builder(hosts);
         if (isHttps()) {
-            configureHttpsClient(builder, settings);
+            String keystore = settings.get(OPENSEARCH_SECURITY_SSL_HTTP_KEYSTORE_FILEPATH);
+            if (Objects.nonNull(keystore)) {
+                URI uri;
+                try {
+                    uri = this.getClass().getClassLoader().getResource("security/sample.pem").toURI();
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+                Path configPath = PathUtils.get(uri).getParent().toAbsolutePath();
+                return new SecureRestClientBuilder(settings, configPath).build();
+            } else {
+                configureHttpsClient(builder, settings);
+                boolean strictDeprecationMode = settings.getAsBoolean("strictDeprecationMode", true);
+                builder.setStrictDeprecationMode(strictDeprecationMode);
+                return builder.build();
+            }
         } else {
             configureClient(builder, settings);
         }
@@ -120,8 +161,8 @@ public abstract class ODFERestTestCase extends OpenSearchRestTestCase {
 
     @SuppressWarnings("unchecked")
     @After
-    protected void wipeAllODFEIndices() throws IOException {
-        Response response = client().performRequest(new Request("GET", "/_cat/indices?format=json&expand_wildcards=all"));
+    protected void wipeAllODFEIndices() throws Exception {
+        Response response = adminClient().performRequest(new Request("GET", "/_cat/indices?format=json&expand_wildcards=all"));
         XContentType xContentType = XContentType.fromMediaType(response.getEntity().getContentType().getValue());
         try (
             XContentParser parser = xContentType.xContent()
@@ -140,12 +181,67 @@ public abstract class ODFERestTestCase extends OpenSearchRestTestCase {
             }
 
             for (Map<String, Object> index : parserList) {
-                String indexName = (String) index.get("index");
+                final String indexName = (String) index.get("index");
+                if (isIndexCleanupRequired(indexName)) {
+                    wipeIndexContent(indexName);
+                    continue;
+                }
                 if (!skipDeleteIndex(indexName)) {
                     adminClient().performRequest(new Request("DELETE", "/" + indexName));
                 }
             }
         }
+    }
+
+    private boolean isIndexCleanupRequired(final String index) {
+        return MODEL_INDEX_NAME.equals(index) && !getSkipDeleteModelIndexFlag();
+    }
+
+    private void wipeIndexContent(String indexName) throws IOException {
+        deleteModels(getModelIds());
+        deleteAllDocs(indexName);
+    }
+
+    private List<String> getModelIds() throws IOException {
+        final String restURIGetModels = String.join("/", KNNPlugin.KNN_BASE_URI, MODELS, "_search");
+        final Response response = adminClient().performRequest(new Request("GET", restURIGetModels));
+
+        assertEquals(RestStatus.OK, RestStatus.fromCode(response.getStatusLine().getStatusCode()));
+
+        final String responseBody = EntityUtils.toString(response.getEntity());
+        assertNotNull(responseBody);
+
+        final XContentParser parser = createParser(XContentType.JSON.xContent(), responseBody);
+        final SearchResponse searchResponse = SearchResponse.fromXContent(parser);
+
+        return Arrays.stream(searchResponse.getHits().getHits()).map(SearchHit::getId).collect(Collectors.toList());
+    }
+
+    private void deleteModels(final List<String> modelIds) throws IOException {
+        for (final String testModelID : modelIds) {
+            final String restURIGetModel = String.join("/", KNNPlugin.KNN_BASE_URI, MODELS, testModelID);
+            final Response getModelResponse = adminClient().performRequest(new Request("GET", restURIGetModel));
+            if (RestStatus.OK != RestStatus.fromCode(getModelResponse.getStatusLine().getStatusCode())) {
+                continue;
+            }
+            final String restURIDeleteModel = String.join("/", KNNPlugin.KNN_BASE_URI, MODELS, testModelID);
+            adminClient().performRequest(new Request("DELETE", restURIDeleteModel));
+        }
+    }
+
+    private void deleteAllDocs(final String indexName) throws IOException {
+        final String restURIDeleteByQuery = String.join("/", indexName, "_delete_by_query");
+        final Request request = new Request("POST", restURIDeleteByQuery);
+        final XContentBuilder matchAllDocsQuery = XContentFactory.jsonBuilder()
+            .startObject()
+            .startObject("query")
+            .startObject("match_all")
+            .endObject()
+            .endObject()
+            .endObject();
+
+        request.setJsonEntity(Strings.toString(matchAllDocsQuery));
+        adminClient().performRequest(request);
     }
 
     private boolean getSkipDeleteModelIndexFlag() {
@@ -159,11 +255,25 @@ public abstract class ODFERestTestCase extends OpenSearchRestTestCase {
     private boolean skipDeleteIndex(String indexName) {
         if (indexName != null
             && !OPENDISTRO_SECURITY.equals(indexName)
-            && !indexName.startsWith(KNN_BWC_PREFIX)
+            && IMMUTABLE_INDEX_PREFIXES.stream().noneMatch(indexName::startsWith)
             && !skipDeleteModelIndex(indexName)) {
             return false;
         }
 
         return true;
+    }
+
+    @Override
+    protected Settings restAdminSettings() {
+        return Settings.builder()
+            // disable the warning exception for admin client since it's only used for cleanup.
+            .put("strictDeprecationMode", false)
+            .put("http.port", 9200)
+            .put(OPENSEARCH_SECURITY_SSL_HTTP_ENABLED, isHttps())
+            .put(OPENSEARCH_SECURITY_SSL_HTTP_PEMCERT_FILEPATH, "sample.pem")
+            .put(OPENSEARCH_SECURITY_SSL_HTTP_KEYSTORE_FILEPATH, "test-kirk.jks")
+            .put(OPENSEARCH_SECURITY_SSL_HTTP_KEYSTORE_PASSWORD, "changeit")
+            .put(OPENSEARCH_SECURITY_SSL_HTTP_KEYSTORE_KEYPASSWORD, "changeit")
+            .build();
     }
 }
