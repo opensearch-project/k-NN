11c6616456e74d5c72ca5704e4a4ec867acf8d60
John Mazanec <jmazane@amazon.com>
2023-04-14 11:52:28 -0700

    Switch stashing thread context back to only around system index calls (#859)
    Revert "Add support of .opensearch-knn-model as system index to transport actions (#847)"
    
    Makes minor fixes to stashed thread context for calls to the model
    system index. In general, only direct calls to the system index should
    be wrapped in the stashed context.
    
    Signed-off-by: John Mazanec <jmazane@amazon.com>

--- src/main/java/org/opensearch/knn/plugin/transport/DeleteModelTransportAction.java
+++ src/main/java/org/opensearch/knn/plugin/transport/DeleteModelTransportAction.java
@@ -15,9 +15,7 @@ import lombok.extern.log4j.Log4j2;
 import org.opensearch.action.ActionListener;
 import org.opensearch.action.support.ActionFilters;
 import org.opensearch.action.support.HandledTransportAction;
-import org.opensearch.client.Client;
 import org.opensearch.common.inject.Inject;
-import org.opensearch.knn.common.ThreadContextHelper;
 import org.opensearch.knn.indices.ModelDao;
 import org.opensearch.tasks.Task;
 import org.opensearch.transport.TransportService;
@@ -26,23 +24,19 @@ import org.opensearch.transport.TransportService;
 public class DeleteModelTransportAction extends HandledTransportAction<DeleteModelRequest, DeleteModelResponse> {
 
     private final ModelDao modelDao;
-    private final Client client;
 
     @Inject
-    public DeleteModelTransportAction(TransportService transportService, ActionFilters filters, Client client) {
+    public DeleteModelTransportAction(TransportService transportService, ActionFilters filters) {
         super(DeleteModelAction.NAME, transportService, filters, DeleteModelRequest::new);
         this.modelDao = ModelDao.OpenSearchKNNModelDao.getInstance();
-        this.client = client;
     }
 
     @Override
     protected void doExecute(Task task, DeleteModelRequest request, ActionListener<DeleteModelResponse> listener) {
-        ThreadContextHelper.runWithStashedThreadContext(client, () -> {
-            String modelID = request.getModelID();
-            modelDao.delete(modelID, ActionListener.wrap(listener::onResponse, e -> {
-                log.error(e);
-                listener.onFailure(e);
-            }));
-        });
+        String modelID = request.getModelID();
+        modelDao.delete(modelID, ActionListener.wrap(listener::onResponse, e -> {
+            log.error(e);
+            listener.onFailure(e);
+        }));
     }
 }
