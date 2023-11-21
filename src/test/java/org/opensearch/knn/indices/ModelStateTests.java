c15dc9ab1ba0b8e2835626a1f6f703535acbbad8
Ryan Bogan <rbogan@amazon.com>
2023-11-21 10:26:40 -0800

    Modify unit test to increase coverage
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- src/test/java/org/opensearch/knn/indices/ModelStateTests.java
+++ src/test/java/org/opensearch/knn/indices/ModelStateTests.java
@@ -31,5 +31,9 @@ public class ModelStateTests extends KNNTestCase {
 
     public void testGetModelState() {
         assertEquals(ModelState.CREATED, ModelState.getModelState(ModelState.CREATED.getName()));
+        assertEquals(ModelState.TRAINING, ModelState.getModelState(ModelState.TRAINING.getName()));
+        assertEquals(ModelState.FAILED, ModelState.getModelState(ModelState.FAILED.getName()));
+        assertEquals(ModelState.ZOMBIE, ModelState.getModelState(ModelState.ZOMBIE.getName()));
+        expectThrows(IllegalArgumentException.class, () -> ModelState.getModelState("throw-exception"));
     }
 }
