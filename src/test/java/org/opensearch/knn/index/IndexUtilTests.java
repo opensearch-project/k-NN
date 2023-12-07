12a9ed862b77d979aa8241a6ca274d7e17df3674
Junqiu Lei <junqiu@amazon.com>
2023-12-07 14:12:49 -0800

    Allow nested knn field mapping when train model (#1318)
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>
    (cherry picked from commit 2e3ab953e3a9a3bdba83464f93d325e8eb12f567)
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- src/test/java/org/opensearch/knn/index/IndexUtilTests.java
+++ src/test/java/org/opensearch/knn/index/IndexUtilTests.java
@@ -14,13 +14,18 @@ package org.opensearch.knn.index;
 import com.google.common.collect.ImmutableMap;
 import org.opensearch.cluster.ClusterState;
 import org.opensearch.cluster.metadata.IndexMetadata;
+import org.opensearch.cluster.metadata.MappingMetadata;
 import org.opensearch.cluster.metadata.Metadata;
 import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.ValidationException;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.knn.KNNTestCase;
 import org.opensearch.knn.index.util.KNNEngine;
+import org.opensearch.knn.indices.ModelDao;
+import org.opensearch.knn.indices.ModelMetadata;
 
 import java.util.Map;
+import java.util.Objects;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -66,5 +71,137 @@ public class IndexUtilTests extends KNNTestCase {
         assertEquals(2, loadParameters.size());
         assertEquals(spaceType2.getValue(), loadParameters.get(SPACE_TYPE));
         assertEquals(efSearchValue, loadParameters.get(HNSW_ALGO_EF_SEARCH));
+    }
+
+    public void testValidateKnnField_NestedField() {
+        Map<String, Object> deepFieldValues = Map.of("type", "knn_vector", "dimension", 8);
+        Map<String, Object> deepField = Map.of("train-field", deepFieldValues);
+        Map<String, Object> deepFieldProperties = Map.of("properties", deepField);
+        Map<String, Object> nest_b = Map.of("b", deepFieldProperties);
+        Map<String, Object> nest_b_properties = Map.of("properties", nest_b);
+        Map<String, Object> nest_a = Map.of("a", nest_b_properties);
+        Map<String, Object> properties = Map.of("properties", nest_a);
+
+        String field = "a.b.train-field";
+        int dimension = 8;
+
+        MappingMetadata mappingMetadata = mock(MappingMetadata.class);
+        when(mappingMetadata.getSourceAsMap()).thenReturn(properties);
+        IndexMetadata indexMetadata = mock(IndexMetadata.class);
+        when(indexMetadata.mapping()).thenReturn(mappingMetadata);
+        ModelDao modelDao = mock(ModelDao.class);
+        ModelMetadata trainingFieldModelMetadata = mock(ModelMetadata.class);
+        when(trainingFieldModelMetadata.getDimension()).thenReturn(dimension);
+        when(modelDao.getMetadata(anyString())).thenReturn(trainingFieldModelMetadata);
+
+        ValidationException e = IndexUtil.validateKnnField(indexMetadata, field, dimension, modelDao);
+
+        assertNull(e);
+    }
+
+    public void testValidateKnnField_NonNestedField() {
+        Map<String, Object> fieldValues = Map.of("type", "knn_vector", "dimension", 8);
+        Map<String, Object> top_level_field = Map.of("top_level_field", fieldValues);
+        Map<String, Object> properties = Map.of("properties", top_level_field);
+        String field = "top_level_field";
+        int dimension = 8;
+
+        MappingMetadata mappingMetadata = mock(MappingMetadata.class);
+        when(mappingMetadata.getSourceAsMap()).thenReturn(properties);
+        IndexMetadata indexMetadata = mock(IndexMetadata.class);
+        when(indexMetadata.mapping()).thenReturn(mappingMetadata);
+        ModelDao modelDao = mock(ModelDao.class);
+        ModelMetadata trainingFieldModelMetadata = mock(ModelMetadata.class);
+        when(trainingFieldModelMetadata.getDimension()).thenReturn(dimension);
+        when(modelDao.getMetadata(anyString())).thenReturn(trainingFieldModelMetadata);
+
+        ValidationException e = IndexUtil.validateKnnField(indexMetadata, field, dimension, modelDao);
+
+        assertNull(e);
+    }
+
+    public void testValidateKnnField_NonKnnField() {
+        Map<String, Object> fieldValues = Map.of("type", "text");
+        Map<String, Object> top_level_field = Map.of("top_level_field", fieldValues);
+        Map<String, Object> properties = Map.of("properties", top_level_field);
+        String field = "top_level_field";
+        int dimension = 8;
+        MappingMetadata mappingMetadata = mock(MappingMetadata.class);
+        when(mappingMetadata.getSourceAsMap()).thenReturn(properties);
+        IndexMetadata indexMetadata = mock(IndexMetadata.class);
+        when(indexMetadata.mapping()).thenReturn(mappingMetadata);
+        ModelDao modelDao = mock(ModelDao.class);
+        ModelMetadata trainingFieldModelMetadata = mock(ModelMetadata.class);
+        when(trainingFieldModelMetadata.getDimension()).thenReturn(dimension);
+        when(modelDao.getMetadata(anyString())).thenReturn(trainingFieldModelMetadata);
+
+        ValidationException e = IndexUtil.validateKnnField(indexMetadata, field, dimension, modelDao);
+
+        assert Objects.requireNonNull(e).getMessage().matches("Validation Failed: 1: Field \"" + field + "\" is not of type knn_vector.;");
+    }
+
+    public void testValidateKnnField_WrongFieldPath() {
+        Map<String, Object> deepFieldValues = Map.of("type", "knn_vector", "dimension", 8);
+        Map<String, Object> deepField = Map.of("train-field", deepFieldValues);
+        Map<String, Object> deepFieldProperties = Map.of("properties", deepField);
+        Map<String, Object> nest_b = Map.of("b", deepFieldProperties);
+        Map<String, Object> nest_b_properties = Map.of("properties", nest_b);
+        Map<String, Object> nest_a = Map.of("a", nest_b_properties);
+        Map<String, Object> properties = Map.of("properties", nest_a);
+        String field = "a.train-field";
+        int dimension = 8;
+        MappingMetadata mappingMetadata = mock(MappingMetadata.class);
+        when(mappingMetadata.getSourceAsMap()).thenReturn(properties);
+        IndexMetadata indexMetadata = mock(IndexMetadata.class);
+        when(indexMetadata.mapping()).thenReturn(mappingMetadata);
+        ModelDao modelDao = mock(ModelDao.class);
+        ModelMetadata trainingFieldModelMetadata = mock(ModelMetadata.class);
+        when(trainingFieldModelMetadata.getDimension()).thenReturn(dimension);
+        when(modelDao.getMetadata(anyString())).thenReturn(trainingFieldModelMetadata);
+
+        ValidationException e = IndexUtil.validateKnnField(indexMetadata, field, dimension, modelDao);
+
+        assert (Objects.requireNonNull(e).getMessage().matches("Validation Failed: 1: Field \"" + field + "\" does not exist.;"));
+    }
+
+    public void testValidateKnnField_EmptyField() {
+        Map<String, Object> deepFieldValues = Map.of("type", "knn_vector", "dimension", 8);
+        Map<String, Object> deepField = Map.of("train-field", deepFieldValues);
+        Map<String, Object> deepFieldProperties = Map.of("properties", deepField);
+        Map<String, Object> nest_b = Map.of("b", deepFieldProperties);
+        Map<String, Object> nest_b_properties = Map.of("properties", nest_b);
+        Map<String, Object> nest_a = Map.of("a", nest_b_properties);
+        Map<String, Object> properties = Map.of("properties", nest_a);
+        String field = "";
+        int dimension = 8;
+        MappingMetadata mappingMetadata = mock(MappingMetadata.class);
+        when(mappingMetadata.getSourceAsMap()).thenReturn(properties);
+        IndexMetadata indexMetadata = mock(IndexMetadata.class);
+        when(indexMetadata.mapping()).thenReturn(mappingMetadata);
+        ModelDao modelDao = mock(ModelDao.class);
+        ModelMetadata trainingFieldModelMetadata = mock(ModelMetadata.class);
+        when(trainingFieldModelMetadata.getDimension()).thenReturn(dimension);
+        when(modelDao.getMetadata(anyString())).thenReturn(trainingFieldModelMetadata);
+
+        ValidationException e = IndexUtil.validateKnnField(indexMetadata, field, dimension, modelDao);
+
+        System.out.println(Objects.requireNonNull(e).getMessage());
+
+        assert (Objects.requireNonNull(e).getMessage().matches("Validation Failed: 1: Field path is empty.;"));
+    }
+
+    public void testValidateKnnField_EmptyIndexMetadata() {
+        String field = "a.b.train-field";
+        int dimension = 8;
+        IndexMetadata indexMetadata = mock(IndexMetadata.class);
+        when(indexMetadata.mapping()).thenReturn(null);
+        ModelDao modelDao = mock(ModelDao.class);
+        ModelMetadata trainingFieldModelMetadata = mock(ModelMetadata.class);
+        when(trainingFieldModelMetadata.getDimension()).thenReturn(dimension);
+        when(modelDao.getMetadata(anyString())).thenReturn(trainingFieldModelMetadata);
+
+        ValidationException e = IndexUtil.validateKnnField(indexMetadata, field, dimension, modelDao);
+
+        assert (Objects.requireNonNull(e).getMessage().matches("Validation Failed: 1: Invalid index. Index does not contain a mapping;"));
     }
 }
