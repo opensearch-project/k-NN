45e9e542aef60ef7073ee726e6ac14dec27bfa04
Naveen Tatikonda <navtat@amazon.com>
2024-01-11 17:26:51 -0600

    Bump lucene codec to 99 (#1383)
    * Add Lucene Codec 9.9
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Fix import statements for Lucene95 Codec
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Fix SegmentInfo Constructor in Test
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Temporarily Ignore Old Codec Tests
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Add CHANGELOG
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    * Delete Old Codec Tests
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>
    
    ---------
    
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>

--- src/test/java/org/opensearch/knn/index/codec/KNN990Codec/KNN990CodecTests.java
+++ src/test/java/org/opensearch/knn/index/codec/KNN990Codec/KNN990CodecTests.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.opensearch.knn.index.codec.KNN950Codec;
+package org.opensearch.knn.index.codec.KNN990Codec;
 
 import lombok.SneakyThrows;
 import org.apache.lucene.codecs.Codec;
@@ -14,24 +14,24 @@ import org.opensearch.knn.index.codec.KNNCodecTestCase;
 import java.util.Optional;
 import java.util.function.Function;
 
-import static org.opensearch.knn.index.codec.KNNCodecVersion.V_9_5_0;
+import static org.opensearch.knn.index.codec.KNNCodecVersion.V_9_9_0;
 
-public class KNN950CodecTests extends KNNCodecTestCase {
+public class KNN990CodecTests extends KNNCodecTestCase {
 
     @SneakyThrows
     public void testMultiFieldsKnnIndex() {
-        testMultiFieldsKnnIndex(KNN950Codec.builder().delegate(V_9_5_0.getDefaultCodecDelegate()).build());
+        testMultiFieldsKnnIndex(KNN990Codec.builder().delegate(V_9_9_0.getDefaultCodecDelegate()).build());
     }
 
     @SneakyThrows
     public void testBuildFromModelTemplate() {
-        testBuildFromModelTemplate((KNN950Codec.builder().delegate(V_9_5_0.getDefaultCodecDelegate()).build()));
+        testBuildFromModelTemplate((KNN990Codec.builder().delegate(V_9_9_0.getDefaultCodecDelegate()).build()));
     }
 
     // Ensure that the codec is able to return the correct per field knn vectors format for codec
     public void testCodecSetsCustomPerFieldKnnVectorsFormat() {
-        final Codec codec = new KNN950Codec();
-        assertTrue(codec.knnVectorsFormat() instanceof KNN950PerFieldKnnVectorsFormat);
+        final Codec codec = new KNN990Codec();
+        assertTrue(codec.knnVectorsFormat() instanceof KNN990PerFieldKnnVectorsFormat);
     }
 
     // IMPORTANT: When this Codec is moved to a backwards Codec, this test needs to be removed, because it attempts to
@@ -39,10 +39,10 @@ public class KNN950CodecTests extends KNNCodecTestCase {
     @SneakyThrows
     public void testKnnVectorIndex() {
         Function<MapperService, PerFieldKnnVectorsFormat> perFieldKnnVectorsFormatProvider = (
-            mapperService) -> new KNN950PerFieldKnnVectorsFormat(Optional.of(mapperService));
+            mapperService) -> new KNN990PerFieldKnnVectorsFormat(Optional.of(mapperService));
 
-        Function<PerFieldKnnVectorsFormat, Codec> knnCodecProvider = (knnVectorFormat) -> KNN950Codec.builder()
-            .delegate(V_9_5_0.getDefaultCodecDelegate())
+        Function<PerFieldKnnVectorsFormat, Codec> knnCodecProvider = (knnVectorFormat) -> KNN990Codec.builder()
+            .delegate(V_9_9_0.getDefaultCodecDelegate())
             .knnVectorsFormat(knnVectorFormat)
             .build();
 
