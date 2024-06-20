0c41dc58b1e4975ec525100c3d192abdd3f7801a
Junqiu Lei <junqiu@amazon.com>
2024-06-20 14:41:38 -0700

    Add IllegalArgumentException for Lucene radial search with ef_search (#1765)
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

---
+++ src/main/java/org/opensearch/knn/index/util/QueryContext.java
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.knn.index.util;
+
+import lombok.AllArgsConstructor;
+import org.opensearch.knn.index.VectorQueryType;
+
+/**
+ * Context class for query-specific information.
+ */
+@AllArgsConstructor
+public class QueryContext {
+    VectorQueryType queryType;
+}
