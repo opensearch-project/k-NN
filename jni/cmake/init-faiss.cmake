881364f73b5f06761a1c4beffea6fb182e9512d5
Heemin Kim <heemin@amazon.com>
2024-07-18 10:12:04 -0700

    Fix build failure due to patch conflict (#1851)
    Signed-off-by: Heemin Kim <heemin@amazon.com>

--- jni/cmake/init-faiss.cmake
+++ jni/cmake/init-faiss.cmake
@@ -12,44 +12,47 @@ if (NOT EXISTS ${FAISS_REPO_DIR})
     execute_process(COMMAND git submodule update --init -- external/faiss WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 endif ()
 
-# Define list of patch files
-set(PATCH_FILE_LIST)
-list(APPEND PATCH_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/patches/faiss/0001-Custom-patch-to-support-multi-vector.patch")
-list(APPEND PATCH_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/patches/faiss/0002-Enable-precomp-table-to-be-shared-ivfpq.patch")
-list(APPEND PATCH_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/patches/faiss/0003-Custom-patch-to-support-range-search-params.patch")
-list(APPEND PATCH_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/patches/faiss/0004-Custom-patch-to-support-binary-vector.patch")
+# Apply patches
+if(NOT DEFINED APPLY_LIB_PATCHES OR "${APPLY_LIB_PATCHES}" STREQUAL true)
+    # Define list of patch files
+    set(PATCH_FILE_LIST)
+    list(APPEND PATCH_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/patches/faiss/0001-Custom-patch-to-support-multi-vector.patch")
+    list(APPEND PATCH_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/patches/faiss/0002-Enable-precomp-table-to-be-shared-ivfpq.patch")
+    list(APPEND PATCH_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/patches/faiss/0003-Custom-patch-to-support-range-search-params.patch")
+    list(APPEND PATCH_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/patches/faiss/0004-Custom-patch-to-support-binary-vector.patch")
 
-# Get patch id of the last commit
-execute_process(COMMAND sh -c "git --no-pager show HEAD | git patch-id --stable" OUTPUT_VARIABLE PATCH_ID_OUTPUT_FROM_COMMIT WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/external/faiss)
-string(REPLACE " " ";" PATCH_ID_LIST_FROM_COMMIT ${PATCH_ID_OUTPUT_FROM_COMMIT})
-list(GET PATCH_ID_LIST_FROM_COMMIT 0 PATCH_ID_FROM_COMMIT)
+    # Get patch id of the last commit
+    execute_process(COMMAND sh -c "git --no-pager show HEAD | git patch-id --stable" OUTPUT_VARIABLE PATCH_ID_OUTPUT_FROM_COMMIT WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/external/faiss)
+    string(REPLACE " " ";" PATCH_ID_LIST_FROM_COMMIT ${PATCH_ID_OUTPUT_FROM_COMMIT})
+    list(GET PATCH_ID_LIST_FROM_COMMIT 0 PATCH_ID_FROM_COMMIT)
 
-# Find all patch files need to apply
-list(SORT PATCH_FILE_LIST ORDER DESCENDING)
-set(PATCH_FILES_TO_APPLY)
-foreach(PATCH_FILE IN LISTS PATCH_FILE_LIST)
-    # Get patch id of a patch file
-    execute_process(COMMAND sh -c "cat ${PATCH_FILE} | git patch-id --stable" OUTPUT_VARIABLE PATCH_ID_OUTPUT)
-    string(REPLACE " " ";" PATCH_ID_LIST ${PATCH_ID_OUTPUT})
-    list(GET PATCH_ID_LIST 0 PATCH_ID)
+    # Find all patch files need to apply
+    list(SORT PATCH_FILE_LIST ORDER DESCENDING)
+    set(PATCH_FILES_TO_APPLY)
+    foreach(PATCH_FILE IN LISTS PATCH_FILE_LIST)
+        # Get patch id of a patch file
+        execute_process(COMMAND sh -c "cat ${PATCH_FILE} | git patch-id --stable" OUTPUT_VARIABLE PATCH_ID_OUTPUT)
+        string(REPLACE " " ";" PATCH_ID_LIST ${PATCH_ID_OUTPUT})
+        list(GET PATCH_ID_LIST 0 PATCH_ID)
 
-    # Add the file to patch list if patch id does not match
-    if (${PATCH_ID} STREQUAL ${PATCH_ID_FROM_COMMIT})
-        break()
-    else()
-        list(APPEND PATCH_FILES_TO_APPLY ${PATCH_FILE})
-    endif()
-endforeach()
+        # Add the file to patch list if patch id does not match
+        if (${PATCH_ID} STREQUAL ${PATCH_ID_FROM_COMMIT})
+            break()
+        else()
+            list(APPEND PATCH_FILES_TO_APPLY ${PATCH_FILE})
+        endif()
+    endforeach()
 
-# Apply patch files
-list(SORT PATCH_FILES_TO_APPLY)
-foreach(PATCH_FILE IN LISTS PATCH_FILES_TO_APPLY)
-    message(STATUS "Applying patch of ${PATCH_FILE}")
-    execute_process(COMMAND git ${GIT_PATCH_COMMAND} --3way --ignore-space-change --ignore-whitespace ${PATCH_FILE} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/external/faiss ERROR_VARIABLE ERROR_MSG RESULT_VARIABLE RESULT_CODE)
-    if(RESULT_CODE)
-        message(FATAL_ERROR "Failed to apply patch:\n${ERROR_MSG}")
-    endif()
-endforeach()
+    # Apply patch files
+    list(SORT PATCH_FILES_TO_APPLY)
+    foreach(PATCH_FILE IN LISTS PATCH_FILES_TO_APPLY)
+        message(STATUS "Applying patch of ${PATCH_FILE}")
+        execute_process(COMMAND git ${GIT_PATCH_COMMAND} --3way --ignore-space-change --ignore-whitespace ${PATCH_FILE} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/external/faiss ERROR_VARIABLE ERROR_MSG RESULT_VARIABLE RESULT_CODE)
+        if(RESULT_CODE)
+            message(FATAL_ERROR "Failed to apply patch:\n${ERROR_MSG}")
+        endif()
+    endforeach()
+endif()
 
 if (${CMAKE_SYSTEM_NAME} STREQUAL Darwin)
     if(CMAKE_C_COMPILER_ID MATCHES "Clang\$")
