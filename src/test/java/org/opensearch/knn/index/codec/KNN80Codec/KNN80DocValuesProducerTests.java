c5c2b3a52c8af0caf905096f37f4fd2d161195ac
luyuncheng <luyuncheng@bytedance.com>
2024-08-13 21:07:32 +0800

    Fixed Comments for directory
    Signed-off-by: luyuncheng <luyuncheng@bytedance.com>

--- src/test/java/org/opensearch/knn/index/codec/KNN80Codec/KNN80DocValuesProducerTests.java
+++ src/test/java/org/opensearch/knn/index/codec/KNN80Codec/KNN80DocValuesProducerTests.java
@@ -12,7 +12,7 @@ import org.apache.lucene.codecs.DocValuesProducer;
 import org.apache.lucene.index.FieldInfo;
 import org.apache.lucene.index.FieldInfos;
 import org.apache.lucene.index.SegmentInfo;
-import org.apache.lucene.index.SegmentReadState;;
+import org.apache.lucene.index.SegmentReadState;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.IOContext;
 import org.apache.lucene.store.IndexOutput;
