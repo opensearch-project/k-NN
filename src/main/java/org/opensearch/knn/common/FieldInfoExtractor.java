b5a7c0f59222ca914dbfbb2b79855a5fb00b5aca
John Mazanec <jmazane@amazon.com>
2024-08-13 07:37:29 -0700

    Implementation of exact scoring
    Implements re-scoring. It uses the rescoring context in the query
    builder to execute a two-phased search. First, it oversamples the ANN
    index and then reduces the results down to the oversample factor, and
    then rescores and returns.
    
    Signed-off-by: John Mazanec <jmazane@amazon.com>

--- src/main/java/org/opensearch/knn/common/FieldInfoExtractor.java
+++ src/main/java/org/opensearch/knn/common/FieldInfoExtractor.java
@@ -8,6 +8,7 @@ package org.opensearch.knn.common;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang.StringUtils;
 import org.apache.lucene.index.FieldInfo;
+import org.opensearch.knn.index.SpaceType;
 import org.opensearch.knn.index.VectorDataType;
 import org.opensearch.knn.index.engine.KNNEngine;
 import org.opensearch.knn.indices.ModelMetadata;
@@ -19,6 +20,11 @@ import org.opensearch.knn.index.engine.qframe.QuantizationConfig;
 import org.opensearch.knn.index.engine.qframe.QuantizationConfigParser;
 
 import static org.opensearch.knn.common.KNNConstants.QFRAMEWORK_CONFIG;
+import org.opensearch.knn.indices.ModelDao;
+
+import java.util.Locale;
+
+import static org.opensearch.knn.common.KNNConstants.SPACE_TYPE;
 
 /**
  * A utility class to extract information from FieldInfo.
@@ -69,5 +75,32 @@ public class FieldInfoExtractor {
             return QuantizationConfig.EMPTY;
         }
         return QuantizationConfigParser.fromCsv(quantizationConfigString);
+    }
+
+    /**
+     * Get the space type for the given field info.
+     *
+     * @param modelDao ModelDao instance to retrieve model metadata
+     * @param fieldInfo FieldInfo instance to extract space type from
+     * @return SpaceType for the given field info
+     */
+    public static SpaceType getSpaceType(final ModelDao modelDao, final FieldInfo fieldInfo) {
+        final String spaceTypeString = fieldInfo.getAttribute(SPACE_TYPE);
+        if (StringUtils.isNotEmpty(spaceTypeString)) {
+            return SpaceType.getSpace(spaceTypeString);
+        }
+
+        final String modelId = fieldInfo.getAttribute(MODEL_ID);
+        if (StringUtils.isEmpty(modelId)) {
+            throw new IllegalArgumentException(
+                String.format(Locale.ROOT, "Unable to find the Space Type from Field Info attribute for field %s", fieldInfo.getName())
+            );
+        }
+
+        ModelMetadata modelMetadata = modelDao.getMetadata(modelId);
+        if (modelMetadata == null) {
+            throw new IllegalArgumentException(String.format(Locale.ROOT, "Unable to find the model metadata for model id %s", modelId));
+        }
+        return modelMetadata.getSpaceType();
     }
 }
---
+++ src/main/java/org/opensearch/knn/index/query/ResultUtil.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.knn.index.query;
+
+import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.TotalHits;
+import org.apache.lucene.util.BitSet;
+import org.apache.lucene.util.DocIdSetBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * Utility class used for processing results
+ */
+public final class ResultUtil {
+
+    /**
+     * Reduce the results to only include the top k results across all leaf results
+     *
+     * @param perLeafResults Results from the list
+     * @param k the number of results across all leaf results to return
+     */
+    public static void reduceToTopK(List<Map<Integer, Float>> perLeafResults, int k) {
+        // Iterate over all scores to get min competitive score
+        PriorityQueue<Float> topKMinQueue = new PriorityQueue<>(k);
+        for (int i = 0; i < k; i++) {
+            topKMinQueue.add(-Float.MAX_VALUE);
+        }
+
+        for (Map<Integer, Float> perLeafResult : perLeafResults) {
+            for (Float score : perLeafResult.values()) {
+                if (topKMinQueue.peek() != null && score > topKMinQueue.peek()) {
+                    topKMinQueue.poll();
+                    topKMinQueue.add(score);
+                }
+            }
+        }
+
+        // Reduce the results based on min competitive scor
+        float minScore = -Float.MAX_VALUE;
+        while (topKMinQueue.isEmpty() == false) {
+            float score = topKMinQueue.poll();
+            if (score > minScore) {
+                minScore = score;
+                break;
+            }
+        }
+
+        float finalMinScore = minScore;
+        perLeafResults.forEach(results -> results.entrySet().removeIf(entry -> entry.getValue() < finalMinScore));
+    }
+
+    /**
+     * Convert map to bit set
+     *
+     * @param resultMap Map of results
+     * @return BitSet of results
+     * @throws IOException If an error occurs during the search.
+     */
+    public static BitSet resultMapToMatchBitSet(Map<Integer, Float> resultMap) throws IOException {
+        if (resultMap.isEmpty()) {
+            return BitSet.of(DocIdSetIterator.empty(), 0);
+        }
+
+        final int maxDoc = Collections.max(resultMap.keySet()) + 1;
+        return BitSet.of(resultMapToDocIds(resultMap, maxDoc), maxDoc);
+    }
+
+    /**
+     * Convert map of docs to doc id set iterator
+     *
+     * @param resultMap Map of results
+     * @param maxDoc Max doc id
+     * @return Doc id set iterator
+     * @throws IOException If an error occurs during the search.
+     */
+    public static DocIdSetIterator resultMapToDocIds(Map<Integer, Float> resultMap, final int maxDoc) throws IOException {
+        if (resultMap.isEmpty()) {
+            return DocIdSetIterator.empty();
+        }
+        final DocIdSetBuilder docIdSetBuilder = new DocIdSetBuilder(maxDoc);
+        final DocIdSetBuilder.BulkAdder setAdder = docIdSetBuilder.grow(resultMap.size());
+        resultMap.keySet().forEach(setAdder::add);
+        return docIdSetBuilder.build().iterator();
+    }
+
+    /**
+     * COnvert map of results to top docs. Doc ids have proper offset
+     *
+     * @param resultMap map of scores for the leafs
+     * @param segmentOffset Offset to apply to ids to make them shard ids
+     * @return Top docs
+     */
+    public static TopDocs resultMapToTopDocs(Map<Integer, Float> resultMap, int segmentOffset) {
+        if (resultMap.isEmpty()) {
+            return new TopDocs(new TotalHits(0, TotalHits.Relation.EQUAL_TO), new ScoreDoc[0]);
+        }
+
+        int totalHits = 0;
+        final List<ScoreDoc> scoreDocs = new ArrayList<>();
+        final List<Map.Entry<Integer, Float>> topScores = new ArrayList<>(resultMap.entrySet());
+        topScores.sort(Map.Entry.<Integer, Float>comparingByValue().reversed());
+        for (Map.Entry<Integer, Float> entry : topScores) {
+            ScoreDoc scoreDoc = new ScoreDoc(entry.getKey() + segmentOffset, entry.getValue());
+            scoreDocs.add(scoreDoc);
+            totalHits++;
+        }
+
+        return new TopDocs(new TotalHits(totalHits, TotalHits.Relation.EQUAL_TO), scoreDocs.toArray(ScoreDoc[]::new));
+    }
+}
