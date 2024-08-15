b56675320cbc9786bb6d8d0f3066c32b071f5a14
Vikasht34 <viktari@amazon.com>
2024-08-15 09:09:51 -0700

    Quantization Framework Code Structure Improvement (#1967)
    * BackPort Java Doc Fix with Code Improvements
    
    Signed-off-by: VIKASH TIWARI <viktari@amazon.com>
    
    * Quantization Framework Code Structure Improvement
    
    Signed-off-by: VIKASH TIWARI <viktari@amazon.com>
    
    ---------
    
    Signed-off-by: VIKASH TIWARI <viktari@amazon.com>

--- src/main/java/org/opensearch/knn/quantization/models/quantizationOutput/BinaryQuantizationOutput.java
+++ src/main/java/org/opensearch/knn/quantization/models/quantizationOutput/BinaryQuantizationOutput.java
@@ -6,53 +6,52 @@
 package org.opensearch.knn.quantization.models.quantizationOutput;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
 
 /**
  * The BinaryQuantizationOutput class represents the output of a quantization process in binary format.
  * It implements the QuantizationOutput interface to handle byte arrays specifically.
  */
-@NoArgsConstructor
+@Getter
+@RequiredArgsConstructor
 public class BinaryQuantizationOutput implements QuantizationOutput<byte[]> {
-    @Getter
     private byte[] quantizedVector;
+    private final int bitsPerCoordinate;
+    private int currentVectorLength = -1; // Indicates uninitialized state
 
     /**
-     * Prepares the quantized vector array based on the provided parameters and returns it for direct modification.
-     * This method ensures that the internal byte array is appropriately sized and cleared before being used.
-     * The method accepts two parameters:
-     * <ul>
-     *     <li><b>bitsPerCoordinate:</b> The number of bits used per coordinate. This determines the granularity of the quantization.</li>
-     *     <li><b>vectorLength:</b> The length of the original vector that needs to be quantized. This helps in calculating the required byte array size.</li>
-     * </ul>
-     * If the existing quantized vector is either null or not the same size as the required byte array,
-     * a new byte array is allocated. Otherwise, the existing array is cleared (i.e., all bytes are set to zero).
-     * This method is designed to be used in conjunction with a bit-packing utility that writes quantized values directly
-     * into the returned byte array.
-     * @param params an array of parameters, where the first parameter is the number of bits per coordinate (int),
-     *               and the second parameter is the length of the vector (int).
-     * @return the prepared and writable quantized vector as a byte array.
-     * @throws IllegalArgumentException if the parameters are not as expected (e.g., missing or not integers).
+     * Prepares the quantized vector based on the vector length.
+     * This includes initializing or resetting the quantized vector.
+     *
+     * @param vectorLength The length of the vector to be quantized.
      */
     @Override
-    public byte[] prepareAndGetWritableQuantizedVector(Object... params) {
-        if (params.length != 2 || !(params[0] instanceof Integer) || !(params[1] instanceof Integer)) {
-            throw new IllegalArgumentException("Expected two integer parameters: bitsPerCoordinate and vectorLength");
+    public void prepareQuantizedVector(int vectorLength) {
+        if (vectorLength <= 0) {
+            throw new IllegalArgumentException("Vector length must be greater than zero.");
         }
-        int bitsPerCoordinate = (int) params[0];
-        int vectorLength = (int) params[1];
-        int totalBits = bitsPerCoordinate * vectorLength;
-        int byteLength = (totalBits + 7) >> 3;
 
-        if (this.quantizedVector == null || this.quantizedVector.length != byteLength) {
+        if (vectorLength != currentVectorLength) {
+            int totalBits = bitsPerCoordinate * vectorLength;
+            int byteLength = (totalBits + 7) >> 3;
             this.quantizedVector = new byte[byteLength];
+            this.currentVectorLength = vectorLength;
         } else {
             Arrays.fill(this.quantizedVector, (byte) 0);
         }
+    }
 
-        return this.quantizedVector;
+    /**
+     * Checks if the quantized vector has already been prepared for the given vector length.
+     *
+     * @param vectorLength The length of the vector to be quantized.
+     * @return true if the quantized vector is already prepared, false otherwise.
+     */
+    @Override
+    public boolean isPrepared(int vectorLength) {
+        return vectorLength == currentVectorLength && quantizedVector != null;
     }
 
     /**
