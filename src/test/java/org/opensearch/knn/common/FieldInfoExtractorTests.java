de10e5f64c0d88202345054c21b314254d35ebdc
Navneet Verma <navneev@amazon.com>
2024-08-15 22:48:23 -0700

    Fixing the dimension for the vector when using Lucene field in ModelFieldMapper
    Signed-off-by: Navneet Verma <navneev@amazon.com>

--- src/test/java/org/opensearch/knn/common/FieldInfoExtractorTests.java
+++ src/test/java/org/opensearch/knn/common/FieldInfoExtractorTests.java
@@ -20,25 +20,23 @@ public class FieldInfoExtractorTests extends KNNTestCase {
 
     public void testExtractVectorDataType_whenDifferentConditions_thenSuccess() {
         FieldInfo fieldInfo = Mockito.mock(FieldInfo.class);
-        MockedStatic<ModelUtil> modelUtilMockedStatic = Mockito.mockStatic(ModelUtil.class);
+        try (MockedStatic<ModelUtil> modelUtilMockedStatic = Mockito.mockStatic(ModelUtil.class)) {
+            // default case
+            Mockito.when(fieldInfo.getAttribute(KNNConstants.VECTOR_DATA_TYPE_FIELD)).thenReturn(null);
+            Mockito.when(fieldInfo.getAttribute(KNNConstants.MODEL_ID)).thenReturn(MODEL_ID);
+            modelUtilMockedStatic.when(() -> ModelUtil.getModelMetadata(MODEL_ID)).thenReturn(null);
+            Assert.assertEquals(VectorDataType.DEFAULT, FieldInfoExtractor.extractVectorDataType(fieldInfo));
 
-        // default case
-        Mockito.when(fieldInfo.getAttribute(KNNConstants.VECTOR_DATA_TYPE_FIELD)).thenReturn(null);
-        Mockito.when(fieldInfo.getAttribute(KNNConstants.MODEL_ID)).thenReturn(MODEL_ID);
-        modelUtilMockedStatic.when(() -> ModelUtil.getModelMetadata(MODEL_ID)).thenReturn(null);
-        Assert.assertEquals(VectorDataType.DEFAULT, FieldInfoExtractor.extractVectorDataType(fieldInfo));
+            // VectorDataType present in fieldInfo
+            Mockito.when(fieldInfo.getAttribute(KNNConstants.VECTOR_DATA_TYPE_FIELD)).thenReturn(VectorDataType.BINARY.getValue());
+            Assert.assertEquals(VectorDataType.BINARY, FieldInfoExtractor.extractVectorDataType(fieldInfo));
 
-        // VectorDataType present in fieldInfo
-        Mockito.when(fieldInfo.getAttribute(KNNConstants.VECTOR_DATA_TYPE_FIELD)).thenReturn(VectorDataType.BINARY.getValue());
-        Assert.assertEquals(VectorDataType.BINARY, FieldInfoExtractor.extractVectorDataType(fieldInfo));
-
-        // VectorDataType present in ModelMetadata
-        ModelMetadata modelMetadata = Mockito.mock(ModelMetadata.class);
-        Mockito.when(fieldInfo.getAttribute(KNNConstants.VECTOR_DATA_TYPE_FIELD)).thenReturn(null);
-        modelUtilMockedStatic.when(() -> ModelUtil.getModelMetadata(MODEL_ID)).thenReturn(modelMetadata);
-        Mockito.when(modelMetadata.getVectorDataType()).thenReturn(VectorDataType.BYTE);
-        Assert.assertEquals(VectorDataType.BYTE, FieldInfoExtractor.extractVectorDataType(fieldInfo));
-
-        modelUtilMockedStatic.close();
+            // VectorDataType present in ModelMetadata
+            ModelMetadata modelMetadata = Mockito.mock(ModelMetadata.class);
+            Mockito.when(fieldInfo.getAttribute(KNNConstants.VECTOR_DATA_TYPE_FIELD)).thenReturn(null);
+            modelUtilMockedStatic.when(() -> ModelUtil.getModelMetadata(MODEL_ID)).thenReturn(modelMetadata);
+            Mockito.when(modelMetadata.getVectorDataType()).thenReturn(VectorDataType.BYTE);
+            Assert.assertEquals(VectorDataType.BYTE, FieldInfoExtractor.extractVectorDataType(fieldInfo));
+        }
     }
 }
