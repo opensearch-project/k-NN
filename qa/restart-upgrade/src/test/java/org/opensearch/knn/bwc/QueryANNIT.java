78d07e23993f1e367969499537a1a8a3e398d206
Tejas Shah <shatejas@amazon.com>
2024-09-04 11:40:36 -0700

    Adds index and query BWC tests for missing engines
    This is done to make sure that KNN80DocsValueConsumer code path is hit
    
    Signed-off-by: Tejas Shah <shatejas@amazon.com>

--- qa/restart-upgrade/src/test/java/org/opensearch/knn/bwc/QueryANNIT.java
+++ qa/restart-upgrade/src/test/java/org/opensearch/knn/bwc/QueryANNIT.java
@@ -1,12 +1,6 @@
 /*
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- *
- * Modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
  */
 
 package org.opensearch.knn.bwc;
@@ -14,8 +8,13 @@ package org.opensearch.knn.bwc;
 import java.util.Map;
 
 import static org.opensearch.knn.common.KNNConstants.FAISS_NAME;
+import static org.opensearch.knn.common.KNNConstants.LUCENE_NAME;
 import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_EF_SEARCH;
+import static org.opensearch.knn.common.KNNConstants.NMSLIB_NAME;
 
+/**
+ * Use case: Test queries on indexes created on older versions
+ */
 public class QueryANNIT extends AbstractRestartUpgradeTestCase {
 
     private static final String TEST_FIELD = "test-field";
@@ -25,9 +24,33 @@ public class QueryANNIT extends AbstractRestartUpgradeTestCase {
     private static final int NUM_DOCS = 10;
     private static final String ALGORITHM = "hnsw";
 
-    public void testQueryANN() throws Exception {
+    public void testQueryOnFaissIndex() throws Exception {
         if (isRunningAgainstOldCluster()) {
             createKnnIndex(testIndex, getKNNDefaultIndexSettings(), createKnnIndexMapping(TEST_FIELD, DIMENSIONS, ALGORITHM, FAISS_NAME));
+            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 0, NUM_DOCS);
+            validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, NUM_DOCS, K);
+        } else {
+            validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, NUM_DOCS, K);
+            validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, NUM_DOCS, K, Map.of(METHOD_PARAMETER_EF_SEARCH, EF_SEARCH));
+            deleteKNNIndex(testIndex);
+        }
+    }
+
+    public void testQueryOnNmslibIndex() throws Exception {
+        if (isRunningAgainstOldCluster()) {
+            createKnnIndex(testIndex, getKNNDefaultIndexSettings(), createKnnIndexMapping(TEST_FIELD, DIMENSIONS, ALGORITHM, NMSLIB_NAME));
+            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 0, NUM_DOCS);
+            validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, NUM_DOCS, K);
+        } else {
+            validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, NUM_DOCS, K);
+            validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, NUM_DOCS, K, Map.of(METHOD_PARAMETER_EF_SEARCH, EF_SEARCH));
+            deleteKNNIndex(testIndex);
+        }
+    }
+
+    public void testQueryOnLuceneIndex() throws Exception {
+        if (isRunningAgainstOldCluster()) {
+            createKnnIndex(testIndex, getKNNDefaultIndexSettings(), createKnnIndexMapping(TEST_FIELD, DIMENSIONS, ALGORITHM, LUCENE_NAME));
             addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 0, NUM_DOCS);
             validateKNNSearch(testIndex, TEST_FIELD, DIMENSIONS, NUM_DOCS, K);
         } else {
