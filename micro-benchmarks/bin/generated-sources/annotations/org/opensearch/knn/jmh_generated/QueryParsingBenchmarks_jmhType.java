c659b4a46b53c1e13d0167308fb6bd7d3dfdc7d2
Akash Shankaran <akash.shankaran@intel.com>
2024-09-07 00:27:19 -0700

    few more fixes

---
+++ micro-benchmarks/bin/generated-sources/annotations/org/opensearch/knn/jmh_generated/QueryParsingBenchmarks_jmhType.java
@@ -0,0 +1,4 @@
+package org.opensearch.knn.jmh_generated;
+public class QueryParsingBenchmarks_jmhType extends QueryParsingBenchmarks_jmhType_B3 {
+}
+
---
+++ micro-benchmarks/bin/generated-sources/annotations/org/opensearch/knn/jmh_generated/TransferVectorsBenchmarks_jmhType.java
@@ -0,0 +1,4 @@
+package org.opensearch.knn.jmh_generated;
+public class TransferVectorsBenchmarks_jmhType extends TransferVectorsBenchmarks_jmhType_B3 {
+}
+
