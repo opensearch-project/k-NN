585a82aad6483c4c3e01cfa5352dc7d92e386a89
Vikasht34 <viktari@amazon.com>
2024-09-09 10:44:29 -0700

    Re-Call Issue Fix with Binary Quantized Vectors (#2071)
    * Re-Call Issue Fix with Binary Quantized Vectors
    
    Signed-off-by: VIKASH TIWARI <viktari@amazon.com>
    
    * Feedback Fix
    
    Signed-off-by: VIKASH TIWARI <viktari@amazon.com>
    
    ---------
    
    Signed-off-by: VIKASH TIWARI <viktari@amazon.com>
    Signed-off-by: Vikasht34 <viktari@amazon.com>
    (cherry picked from commit ce735c4cf42b0dbc3d601f576592dad3cc16a19d)

--- src/main/java/org/opensearch/knn/quantization/models/quantizationOutput/QuantizationOutput.java
+++ src/main/java/org/opensearch/knn/quantization/models/quantizationOutput/QuantizationOutput.java
@@ -14,7 +14,32 @@ public interface QuantizationOutput<T> {
     /**
      * Returns the quantized vector.
      *
-     * @return the quantized data.
+     * This method provides access to the quantized data in its current state.
+     * It returns the same reference to the internal quantized vector on each call, meaning any modifications
+     * to the returned array will directly affect the internal state of the object. This design is intentional
+     * to avoid unnecessary copying of data and improve performance, especially in scenarios where frequent
+     * access to the quantized vector is required.
+     *
+     * <p><b>Important:</b> As this method returns a direct reference to the internal array, care must be taken
+     * when modifying the returned array. If the returned vector is altered, the changes will reflect in the
+     * quantized vector managed by the object, which could lead to unintended side effects.</p>
+     *
+     * <p><b>Usage Example:</b></p>
+     * <pre>
+     * byte[] quantizedData = quantizationOutput.getQuantizedVector();
+     * // Use or modify quantizedData, but be cautious that changes affect the internal state.
+     * </pre>
+     *
+     * This method does not create a deep copy of the vector to avoid performance overhead in real-time
+     * or high-frequency operations. If a separate copy of the vector is needed, the caller should manually
+     * clone or copy the returned array.
+     *
+     * <p><b>Example to clone the array:</b></p>
+     * <pre>
+     * byte[] clonedData = Arrays.copyOf(quantizationOutput.getQuantizedVector(), quantizationOutput.getQuantizedVector().length);
+     * </pre>
+     *
+     * @return the quantized vector (same reference on each invocation).
      */
     T getQuantizedVector();
 
@@ -33,4 +58,11 @@ public interface QuantizationOutput<T> {
      * @return true if the quantized vector is already prepared, false otherwise.
      */
     boolean isPrepared(int vectorLength);
+
+    /**
+     * Returns a copy of the quantized vector.
+     *
+     * @return a copy of the quantized data.
+     */
+    T getQuantizedVectorCopy();
 }
