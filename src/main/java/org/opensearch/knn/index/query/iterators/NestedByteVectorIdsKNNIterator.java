4ecd2f682a0e5d4ebefac8b4e96a3890be7c0be6
Vijayan Balasubramanian <balasvij@amazon.com>
2024-09-27 23:15:43 -0700

    KNNIterators should support with and without filters (#2155)
    * Rename class names to represent both and filter and non filter use cases
    * Iterator should support with filters
    
    Update VectorIterator and NesterVector Iterator to
    iterate even if there is no filters provided to iterator.
    Currently this is used by exact search to score either topk
    docs or all docs when filter is provided by users.
    However, in future we will be allowing exact search
    even if there are no filters. Hence, decouple filter
    and make it option to support both cases.
    
    ---------
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>
    (cherry picked from commit 6f6dd566e4871ddb2d0b5829c08aab9e4af90774)

--- src/main/java/org/opensearch/knn/index/query/iterators/NestedByteVectorIdsKNNIterator.java
+++ src/main/java/org/opensearch/knn/index/query/iterators/NestedByteVectorIdsKNNIterator.java
@@ -3,30 +3,42 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.opensearch.knn.index.query.filtered;
+package org.opensearch.knn.index.query.iterators;
 
 import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.util.BitSet;
+import org.opensearch.common.Nullable;
 import org.opensearch.knn.index.SpaceType;
 import org.opensearch.knn.index.vectorvalues.KNNBinaryVectorValues;
 
 import java.io.IOException;
 
 /**
- * This iterator iterates filterIdsArray to score. However, it dedupe docs per each parent doc
+ * This iterator iterates filterIdsArray to score if filter is provided else it iterates over all docs.
+ * However, it dedupe docs per each parent doc
  * of which ID is set in parentBitSet and only return best child doc with the highest score.
  */
-public class NestedFilteredIdsKNNByteIterator extends FilteredIdsKNNByteIterator {
+public class NestedByteVectorIdsKNNIterator extends ByteVectorIdsKNNIterator {
     private final BitSet parentBitSet;
 
-    public NestedFilteredIdsKNNByteIterator(
-        final BitSet filterIdsArray,
+    public NestedByteVectorIdsKNNIterator(
+        @Nullable final BitSet filterIdsArray,
         final byte[] queryVector,
         final KNNBinaryVectorValues binaryVectorValues,
         final SpaceType spaceType,
         final BitSet parentBitSet
-    ) {
+    ) throws IOException {
         super(filterIdsArray, queryVector, binaryVectorValues, spaceType);
+        this.parentBitSet = parentBitSet;
+    }
+
+    public NestedByteVectorIdsKNNIterator(
+        final byte[] queryVector,
+        final KNNBinaryVectorValues binaryVectorValues,
+        final SpaceType spaceType,
+        final BitSet parentBitSet
+    ) throws IOException {
+        super(null, queryVector, binaryVectorValues, spaceType);
         this.parentBitSet = parentBitSet;
     }
 
@@ -46,14 +58,18 @@ public class NestedFilteredIdsKNNByteIterator extends FilteredIdsKNNByteIterator
         int currentParent = parentBitSet.nextSetBit(docId);
         int bestChild = -1;
 
+        // In order to traverse all children for given parent, we have to use docId < parentId, because,
+        // kNNVectorValues will not have parent id since DocId is unique per segment. For ex: let's say for doc id 1, there is one child
+        // and for doc id 5, there are three children. In that case knnVectorValues iterator will have [0, 2, 3, 4]
+        // and parentBitSet will have [1,5]
+        // Hence, we have to iterate till docId from knnVectorValues is less than parentId instead of till equal to parentId
         while (docId != DocIdSetIterator.NO_MORE_DOCS && docId < currentParent) {
-            binaryVectorValues.advance(docId);
             float score = computeScore();
             if (score > currentScore) {
                 bestChild = docId;
                 currentScore = score;
             }
-            docId = bitSetIterator.nextDoc();
+            docId = getNextDocId();
         }
 
         return bestChild;
