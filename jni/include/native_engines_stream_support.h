983c631a806c7bc5cc0b6ce1208e954a988a8b03
Dooyong Kim <kdooyong@amazon.com>
2024-10-03 16:38:53 -0700

    Introduce a loading layer in NMSLIB.
    Signed-off-by: Dooyong Kim <kdooyong@amazon.com>

--- jni/include/native_engines_stream_support.h
+++ jni/include/native_engines_stream_support.h
@@ -12,7 +12,6 @@
 #ifndef OPENSEARCH_KNN_JNI_STREAM_SUPPORT_H
 #define OPENSEARCH_KNN_JNI_STREAM_SUPPORT_H
 
-#include "faiss/impl/io.h"
 #include "jni_util.h"
 
 #include <jni.h>
@@ -23,10 +22,11 @@
 namespace knn_jni {
 namespace stream {
 
+
+
 /**
  * This class contains Java IndexInputWithBuffer reference and calls its API to copy required bytes into a read buffer.
  */
-
 class NativeEngineIndexInputMediator {
  public:
   // Expect IndexInputWithBuffer is given as `_indexInput`.
@@ -96,37 +96,6 @@ class NativeEngineIndexInputMediator {
   jbyteArray bufferArray;
   jmethodID copyBytesMethod;
 }; // class NativeEngineIndexInputMediator
-
-
-
-/**
- * A glue component inheriting IOReader to be passed down to Faiss library.
- * This will then indirectly call the mediator component and eventually read required bytes from Lucene's IndexInput.
- */
-class FaissOpenSearchIOReader final : public faiss::IOReader {
- public:
-  explicit FaissOpenSearchIOReader(NativeEngineIndexInputMediator *_mediator)
-      : faiss::IOReader(),
-        mediator(_mediator) {
-    name = "FaissOpenSearchIOReader";
-  }
-
-  size_t operator()(void *ptr, size_t size, size_t nitems) final {
-    const auto readBytes = size * nitems;
-    if (readBytes > 0) {
-      // Mediator calls IndexInput, then copy read bytes to `ptr`.
-      mediator->copyBytes(readBytes, (uint8_t *) ptr);
-    }
-    return nitems;
-  }
-
-  int filedescriptor() final {
-    throw std::runtime_error("filedescriptor() is not supported in FaissOpenSearchIOReader.");
-  }
-
- private:
-  NativeEngineIndexInputMediator *mediator;
-};  // class FaissOpenSearchIOReader
 
 
 
--- jni/src/org_opensearch_knn_jni_NmslibService.cpp
+++ jni/src/org_opensearch_knn_jni_NmslibService.cpp
@@ -12,79 +12,102 @@
 #include "org_opensearch_knn_jni_NmslibService.h"
 
 #include <jni.h>
-#include <string>
 
 #include "jni_util.h"
 #include "nmslib_wrapper.h"
+#include "nmslib_stream_support.h"
 
 static knn_jni::JNIUtil jniUtil;
 static const jint KNN_NMSLIB_JNI_VERSION = JNI_VERSION_1_1;
 
-jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-    JNIEnv* env;
-    if (vm->GetEnv((void**)&env, KNN_NMSLIB_JNI_VERSION) != JNI_OK) {
-        return JNI_ERR;
-    }
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+  JNIEnv *env;
+  if (vm->GetEnv((void **) &env, KNN_NMSLIB_JNI_VERSION) != JNI_OK) {
+    return JNI_ERR;
+  }
 
-    jniUtil.Initialize(env);
+  jniUtil.Initialize(env);
 
-    return KNN_NMSLIB_JNI_VERSION;
+  return KNN_NMSLIB_JNI_VERSION;
 }
 
 void JNI_OnUnload(JavaVM *vm, void *reserved) {
-    JNIEnv* env;
-    vm->GetEnv((void**)&env, KNN_NMSLIB_JNI_VERSION);
-    jniUtil.Uninitialize(env);
+  JNIEnv *env;
+  vm->GetEnv((void **) &env, KNN_NMSLIB_JNI_VERSION);
+  jniUtil.Uninitialize(env);
 }
 
-JNIEXPORT void JNICALL Java_org_opensearch_knn_jni_NmslibService_createIndex(JNIEnv * env, jclass cls, jintArray idsJ,
-                                                                             jlong vectorsAddressJ, jint dimJ, jstring indexPathJ,
-                                                                             jobject parametersJ)
-{
-    try {
-        knn_jni::nmslib_wrapper::CreateIndex(&jniUtil, env, idsJ, vectorsAddressJ, dimJ, indexPathJ, parametersJ);
-    } catch (...) {
-        jniUtil.CatchCppExceptionAndThrowJava(env);
-    }
+JNIEXPORT void JNICALL Java_org_opensearch_knn_jni_NmslibService_createIndex(JNIEnv *env,
+                                                                             jclass cls,
+                                                                             jintArray idsJ,
+                                                                             jlong vectorsAddressJ,
+                                                                             jint dimJ,
+                                                                             jstring indexPathJ,
+                                                                             jobject parametersJ) {
+  try {
+    knn_jni::nmslib_wrapper::CreateIndex(&jniUtil, env, idsJ, vectorsAddressJ, dimJ, indexPathJ, parametersJ);
+  } catch (...) {
+    jniUtil.CatchCppExceptionAndThrowJava(env);
+  }
 }
 
-JNIEXPORT jlong JNICALL Java_org_opensearch_knn_jni_NmslibService_loadIndex(JNIEnv * env, jclass cls,
-                                                                            jstring indexPathJ, jobject parametersJ)
-{
-    try {
-        return knn_jni::nmslib_wrapper::LoadIndex(&jniUtil, env, indexPathJ, parametersJ);
-    } catch (...) {
-        jniUtil.CatchCppExceptionAndThrowJava(env);
-    }
-    return NULL;
+JNIEXPORT jlong JNICALL Java_org_opensearch_knn_jni_NmslibService_loadIndex(JNIEnv *env, jclass cls,
+                                                                            jstring indexPathJ, jobject parametersJ) {
+  try {
+    return knn_jni::nmslib_wrapper::LoadIndex(&jniUtil, env, indexPathJ, parametersJ);
+  } catch (...) {
+    jniUtil.CatchCppExceptionAndThrowJava(env);
+  }
+  return NULL;
 }
 
-JNIEXPORT jobjectArray JNICALL Java_org_opensearch_knn_jni_NmslibService_queryIndex(JNIEnv * env, jclass cls,
+JNIEXPORT jlong JNICALL Java_org_opensearch_knn_jni_NmslibService_loadIndexWithStream(JNIEnv *env,
+                                                                                      jclass cls,
+                                                                                      jobject readStream,
+                                                                                      jobject parametersJ) {
+  try {
+    // Create a mediator locally.
+    // Note that `indexInput` is `IndexInputWithBuffer` type.
+    knn_jni::stream::NativeEngineIndexInputMediator mediator{&jniUtil, env, readStream};
+
+    // Create NMSLIB buffer.
+    knn_jni::stream::NmslibMediatorInputStreamBuffer nmslib_mediator_stream_buf{&mediator};
+
+    std::istream in{&nmslib_mediator_stream_buf};
+
+    return knn_jni::nmslib_wrapper::LoadIndexWithStream(&jniUtil, env, in, parametersJ);
+  } catch (...) {
+    jniUtil.CatchCppExceptionAndThrowJava(env);
+  }
+  return NULL;
+}
+
+JNIEXPORT jobjectArray JNICALL Java_org_opensearch_knn_jni_NmslibService_queryIndex(JNIEnv *env,
+                                                                                    jclass cls,
                                                                                     jlong indexPointerJ,
-                                                                                    jfloatArray queryVectorJ, jint kJ, jobject methodParamsJ)
-{
-    try {
-        return knn_jni::nmslib_wrapper::QueryIndex(&jniUtil, env, indexPointerJ, queryVectorJ, kJ, methodParamsJ);
-    } catch (...) {
-        jniUtil.CatchCppExceptionAndThrowJava(env);
-    }
-    return nullptr;
+                                                                                    jfloatArray queryVectorJ,
+                                                                                    jint kJ,
+                                                                                    jobject methodParamsJ) {
+  try {
+    return knn_jni::nmslib_wrapper::QueryIndex(&jniUtil, env, indexPointerJ, queryVectorJ, kJ, methodParamsJ);
+  } catch (...) {
+    jniUtil.CatchCppExceptionAndThrowJava(env);
+  }
+  return nullptr;
 }
 
-JNIEXPORT void JNICALL Java_org_opensearch_knn_jni_NmslibService_free(JNIEnv * env, jclass cls, jlong indexPointerJ)
-{
-    try {
-        return knn_jni::nmslib_wrapper::Free(indexPointerJ);
-    } catch (...) {
-        jniUtil.CatchCppExceptionAndThrowJava(env);
-    }
+JNIEXPORT void JNICALL Java_org_opensearch_knn_jni_NmslibService_free(JNIEnv *env, jclass cls, jlong indexPointerJ) {
+  try {
+    return knn_jni::nmslib_wrapper::Free(indexPointerJ);
+  } catch (...) {
+    jniUtil.CatchCppExceptionAndThrowJava(env);
+  }
 }
 
-JNIEXPORT void JNICALL Java_org_opensearch_knn_jni_NmslibService_initLibrary(JNIEnv * env, jclass cls)
-{
-    try {
-        knn_jni::nmslib_wrapper::InitLibrary();
-    } catch (...) {
-        jniUtil.CatchCppExceptionAndThrowJava(env);
-    }
+JNIEXPORT void JNICALL Java_org_opensearch_knn_jni_NmslibService_initLibrary(JNIEnv *env, jclass cls) {
+  try {
+    knn_jni::nmslib_wrapper::InitLibrary();
+  } catch (...) {
+    jniUtil.CatchCppExceptionAndThrowJava(env);
+  }
 }
