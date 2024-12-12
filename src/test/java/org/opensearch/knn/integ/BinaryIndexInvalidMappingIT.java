d31149c53ff623ab2044920eafd37b5256acbc6c
Heemin Kim <heemin@amazon.com>
2024-12-12 11:36:55 -0800

    Support expand_nested_docs parameter for nmslib engine
    Signed-off-by: Heemin Kim <heemin@amazon.com>

--- src/test/java/org/opensearch/knn/integ/BinaryIndexInvalidMappingIT.java
+++ src/test/java/org/opensearch/knn/integ/BinaryIndexInvalidMappingIT.java
@@ -8,7 +8,6 @@ package org.opensearch.knn.integ;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.junit.After;
 import org.opensearch.knn.KNNJsonIndexMappingsBuilder;
 import org.opensearch.knn.KNNRestTestCase;
 import org.opensearch.knn.index.VectorDataType;
@@ -29,15 +28,6 @@ import static org.opensearch.knn.common.KNNConstants.METHOD_HNSW;
 @Log4j2
 @AllArgsConstructor
 public class BinaryIndexInvalidMappingIT extends KNNRestTestCase {
-    @After
-    public void cleanUp() {
-        try {
-            deleteKNNIndex(INDEX_NAME);
-        } catch (Exception e) {
-            log.error(e);
-        }
-    }
-
     private String description;
     private String indexMapping;
     private String expectedExceptionMessage;
