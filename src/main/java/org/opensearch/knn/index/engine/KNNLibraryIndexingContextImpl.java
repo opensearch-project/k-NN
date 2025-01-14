b61e9529a8c3c8a3b91bb8f788160153021abdd0
AnnTian Shao <anntians@amazon.com>
2025-01-14 10:42:42 -0800

    Add validation check for training parameters in engine method abstraction
    Signed-off-by: AnnTian Shao <anntians@amazon.com>

--- src/main/java/org/opensearch/knn/index/engine/KNNLibraryIndexingContextImpl.java
+++ src/main/java/org/opensearch/knn/index/engine/KNNLibraryIndexingContextImpl.java
@@ -13,6 +13,11 @@ import org.opensearch.knn.index.mapper.VectorValidator;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.function.BiFunction;
+
+import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_NLIST;
+import static org.opensearch.knn.common.KNNConstants.ENCODER_PARAMETER_PQ_CODE_SIZE;
+import static org.opensearch.knn.common.KNNConstants.METHOD_ENCODER_PARAMETER;
 
 /**
  * Simple implementation of {@link KNNLibraryIndexingContext}
@@ -51,5 +56,34 @@ public class KNNLibraryIndexingContextImpl implements KNNLibraryIndexingContext 
     @Override
     public PerDimensionProcessor getPerDimensionProcessor() {
         return perDimensionProcessor;
+    }
+
+    @Override
+    public BiFunction<Long, KNNMethodContext, TrainingConfigValidationOutput> getTrainingConfigValidationSetup() {
+        return (trainingVectors, knnMethodContext) -> {
+
+            long minTrainingVectorCount = 1000;
+            TrainingConfigValidationOutput.TrainingConfigValidationOutputBuilder builder = TrainingConfigValidationOutput.builder();
+
+            MethodComponentContext encoderContext = (MethodComponentContext) knnMethodContext.getMethodComponentContext()
+                .getParameters()
+                .get(METHOD_ENCODER_PARAMETER);
+
+            if (knnMethodContext.getMethodComponentContext().getParameters().containsKey(METHOD_PARAMETER_NLIST)
+                && encoderContext.getParameters().containsKey(ENCODER_PARAMETER_PQ_CODE_SIZE)) {
+
+                int nlist = ((Integer) knnMethodContext.getMethodComponentContext().getParameters().get(METHOD_PARAMETER_NLIST));
+                int code_size = ((Integer) encoderContext.getParameters().get(ENCODER_PARAMETER_PQ_CODE_SIZE));
+                minTrainingVectorCount = (long) Math.max(nlist, Math.pow(2, code_size));
+            }
+
+            if (trainingVectors < minTrainingVectorCount) {
+                builder.valid(false).minTrainingVectorCount(minTrainingVectorCount);
+                return builder.build();
+            }
+
+            builder.valid(true);
+            return builder.build();
+        };
     }
 }
