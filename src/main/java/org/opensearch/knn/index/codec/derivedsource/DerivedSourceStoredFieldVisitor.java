59b8e6bb24124c4f624e4711cf11f89948ffc594
John Mazanec <jmazane@amazon.com>
2025-01-29 18:46:29 -0500

    Introduce derived vector source via stored fields (#2449)
    Generates the vector source in the source field from the
    KnnVectorsFormat or BVD. It does this by adding StoredFieldsFormat to
    our existing custom codec.
    
    Currently, feature is experimental and behind a feature flag via index 
    setting. In the future, we need to iterate to improve performance 
    and stability for nested/object portions.
    
    Signed-off-by: John Mazanec <jmazane@amazon.com>

---
+++ src/main/java/org/opensearch/knn/index/codec/derivedsource/DerivedSourceStoredFieldVisitor.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.knn.index.codec.derivedsource;
+
+import lombok.AllArgsConstructor;
+import org.apache.lucene.index.FieldInfo;
+import org.apache.lucene.index.StoredFieldVisitor;
+import org.opensearch.index.mapper.SourceFieldMapper;
+
+import java.io.IOException;
+
+/**
+ * Custom {@link StoredFieldVisitor} that wraps an upstream delegate visitor in order to transparently inject derived
+ * source vector fields into the document. After the source is modified, it is forwarded to the delegate.
+ */
+@AllArgsConstructor
+public class DerivedSourceStoredFieldVisitor extends StoredFieldVisitor {
+
+    private final StoredFieldVisitor delegate;
+    private final Integer documentId;
+    private final DerivedSourceVectorInjector derivedSourceVectorInjector;
+
+    @Override
+    public void binaryField(FieldInfo fieldInfo, byte[] value) throws IOException {
+        if (fieldInfo.name.equals(SourceFieldMapper.NAME)) {
+            delegate.binaryField(fieldInfo, derivedSourceVectorInjector.injectVectors(documentId, value));
+            return;
+        }
+        delegate.binaryField(fieldInfo, value);
+    }
+
+    @Override
+    public Status needsField(FieldInfo fieldInfo) throws IOException {
+        return delegate.needsField(fieldInfo);
+    }
+}
