697953f48b2cd3187f22f5361555ebe2dc6885e8
AnnTian Shao <anntians@amazon.com>
2025-01-29 17:40:16 -0800

    fixes to encoder and method classes
    Signed-off-by: AnnTian Shao <anntians@amazon.com>

--- src/main/java/org/opensearch/knn/index/engine/faiss/FaissSQEncoder.java
+++ src/main/java/org/opensearch/knn/index/engine/faiss/FaissSQEncoder.java
@@ -15,7 +15,6 @@ import org.opensearch.knn.index.engine.Parameter;
 import org.opensearch.knn.index.mapper.CompressionLevel;
 import org.opensearch.knn.index.engine.TrainingConfigValidationInput;
 import org.opensearch.knn.index.engine.TrainingConfigValidationOutput;
-import org.opensearch.knn.index.engine.KNNMethodContext;
 
 import java.util.Objects;
 import java.util.Set;
@@ -26,7 +25,6 @@ import static org.opensearch.knn.common.KNNConstants.FAISS_SQ_DESCRIPTION;
 import static org.opensearch.knn.common.KNNConstants.FAISS_SQ_ENCODER_FP16;
 import static org.opensearch.knn.common.KNNConstants.FAISS_SQ_ENCODER_TYPES;
 import static org.opensearch.knn.common.KNNConstants.FAISS_SQ_TYPE;
-import static org.opensearch.knn.common.KNNConstants.ENCODER_PARAMETER_PQ_M;
 
 /**
  * Faiss SQ encoder
@@ -68,23 +66,7 @@ public class FaissSQEncoder implements Encoder {
 
     @Override
     public TrainingConfigValidationOutput validateEncoderConfig(TrainingConfigValidationInput trainingConfigValidationInput) {
-        KNNMethodContext knnMethodContext = trainingConfigValidationInput.getKnnMethodContext();
-        KNNMethodConfigContext knnMethodConfigContext = trainingConfigValidationInput.getKnnMethodConfigContext();
-
         TrainingConfigValidationOutput.TrainingConfigValidationOutputBuilder builder = TrainingConfigValidationOutput.builder();
-
-        // validate ENCODER_PARAMETER_PQ_M is divisible by vector dimension
-        if (knnMethodContext != null && knnMethodConfigContext != null) {
-            if (knnMethodContext.getMethodComponentContext().getParameters().containsKey(ENCODER_PARAMETER_PQ_M)
-                && knnMethodConfigContext.getDimension() % (Integer) knnMethodContext.getMethodComponentContext()
-                    .getParameters()
-                    .get(ENCODER_PARAMETER_PQ_M) != 0) {
-                builder.valid(false);
-                return builder.build();
-            } else {
-                builder.valid(true);
-            }
-        }
         return builder.build();
     }
 }
