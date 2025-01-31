90f87ae8fd978d1502c8b9f8e3da077f07229da4
Kunal Kotwani <kkotwani@amazon.com>
2025-01-31 13:01:56 -0800

    Add version check for full field name validation (#2477)
    Signed-off-by: Kunal Kotwani <kkotwani@amazon.com>
    (cherry picked from commit 811ae2e6f1cc8ea7c60b7ed106a6da7b638aca27)

--- src/test/java/org/opensearch/knn/index/mapper/KNNVectorFieldMapperUtilTests.java
+++ src/test/java/org/opensearch/knn/index/mapper/KNNVectorFieldMapperUtilTests.java
@@ -77,4 +77,13 @@ public class KNNVectorFieldMapperUtilTests extends KNNTestCase {
         Assert.assertTrue(KNNVectorFieldMapperUtil.useLuceneKNNVectorsFormat(Version.V_2_17_0));
         Assert.assertTrue(KNNVectorFieldMapperUtil.useLuceneKNNVectorsFormat(Version.V_3_0_0));
     }
+
+    /**
+     * Test useFullFieldNameValidation method for different OpenSearch versions
+     */
+    public void testUseFullFieldNameValidation() {
+        Assert.assertFalse(KNNVectorFieldMapperUtil.useFullFieldNameValidation(Version.V_2_16_0));
+        Assert.assertTrue(KNNVectorFieldMapperUtil.useFullFieldNameValidation(Version.V_2_17_0));
+        Assert.assertTrue(KNNVectorFieldMapperUtil.useFullFieldNameValidation(Version.V_2_18_0));
+    }
 }
