a28e47796a38c6933b6ec9a61c1fb148a8f6bcfc
Mark Wu <wumr@amazon.com>
2025-02-04 14:18:49 -0800

    Introduce node level circuit breaker settings for k-NN
    Signed-off-by: Mark Wu <wumr@amazon.com>

--- src/test/java/org/opensearch/knn/index/memory/NativeMemoryCacheManagerTests.java
+++ src/test/java/org/opensearch/knn/index/memory/NativeMemoryCacheManagerTests.java
@@ -294,7 +294,7 @@ public class NativeMemoryCacheManagerTests extends OpenSearchSingleNodeTestCase 
 
     public void testGetMaxCacheSizeInKB() {
         NativeMemoryCacheManager nativeMemoryCacheManager = new NativeMemoryCacheManager();
-        assertEquals(KNNSettings.getCircuitBreakerLimit().getKb(), nativeMemoryCacheManager.getMaxCacheSizeInKilobytes());
+        assertEquals(KNNSettings.getClusterLevelCircuitBreakerLimit().getKb(), nativeMemoryCacheManager.getMaxCacheSizeInKilobytes());
         nativeMemoryCacheManager.close();
     }
 
