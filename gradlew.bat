07fe680eaa071979b9567634f52848c4642f05fc
John Mazanec <jmazane@amazon.com>
2025-03-03 14:39:50 -0800

    Update gradle to 8.13 to fix command exec on java 21 (#2571)
    Updates gradle version to fix command execution for java 21. With that,
    it simplifies overall gradle script and not having to locate
    dependencies. The environment is inherited from the path.
    
    Signed-off-by: John Mazanec <jmazane@amazon.com>

--- gradlew.bat
+++ gradlew.bat
@@ -1,6 +1,6 @@
-@rem
 @rem  Copyright OpenSearch Contributors
 @rem  SPDX-License-Identifier: Apache-2.0
+@rem
 @rem
 @rem
 @rem Copyright 2015 the original author or authors.
@@ -16,6 +16,8 @@
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
+@rem
+@rem SPDX-License-Identifier: Apache-2.0
 @rem
 
 @if "%DEBUG%"=="" @echo off
@@ -47,11 +49,11 @@ set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if %ERRORLEVEL% equ 0 goto execute
 
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+echo. 1>&2
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH. 1>&2
+echo. 1>&2
+echo Please set the JAVA_HOME variable in your environment to match the 1>&2
+echo location of your Java installation. 1>&2
 
 goto fail
 
@@ -61,11 +63,11 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto execute
 
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+echo. 1>&2
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
+echo. 1>&2
+echo Please set the JAVA_HOME variable in your environment to match the 1>&2
+echo location of your Java installation. 1>&2
 
 goto fail
 
