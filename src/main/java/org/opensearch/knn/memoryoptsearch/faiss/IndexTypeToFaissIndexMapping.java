f8f630415cb4419932a1c28196cbb5d2e8252b12
Dooyong Kim <kdooyong@amazon.com>
2025-04-24 14:15:56 -0700

    Make FaissIdMapIndex support binary format in Faiss index.
    Signed-off-by: Dooyong Kim <kdooyong@amazon.com>
    Co-authored-by: Dooyong Kim <kdooyong@amazon.com>

--- src/main/java/org/opensearch/knn/memoryoptsearch/faiss/IndexTypeToFaissIndexMapping.java
+++ src/main/java/org/opensearch/knn/memoryoptsearch/faiss/IndexTypeToFaissIndexMapping.java
@@ -6,6 +6,7 @@
 package org.opensearch.knn.memoryoptsearch.faiss;
 
 import lombok.experimental.UtilityClass;
+import org.opensearch.knn.memoryoptsearch.faiss.binary.FaissBinaryHnswIndex;
 import org.opensearch.knn.memoryoptsearch.faiss.cagra.FaissHNSWCagraIndex;
 
 import java.util.Collections;
@@ -26,13 +27,17 @@ public class IndexTypeToFaissIndexMapping {
     static {
         final Map<String, Function<String, FaissIndex>> mapping = new HashMap<>();
 
-        mapping.put(FaissIdMapIndex.IXMP, (indexType) -> new FaissIdMapIndex());
+        mapping.put(FaissIdMapIndex.IXMP, FaissIdMapIndex::new);
         mapping.put(FaissHNSWIndex.IHNF, FaissHNSWIndex::new);
         mapping.put(FaissHNSWIndex.IHNS, FaissHNSWIndex::new);
         mapping.put(FaissIndexFloatFlat.IXF2, FaissIndexFloatFlat::new);
         mapping.put(FaissIndexFloatFlat.IXFI, FaissIndexFloatFlat::new);
         mapping.put(FaissIndexScalarQuantizedFlat.IXSQ, (indexType) -> new FaissIndexScalarQuantizedFlat());
-        mapping.put(FaissHNSWCagraIndex.IHNC, (indexingType) -> new FaissHNSWCagraIndex());
+        mapping.put(FaissHNSWCagraIndex.IHNC, (indexType) -> new FaissHNSWCagraIndex());
+
+        // Binary index
+        mapping.put(FaissBinaryHnswIndex.IBHF, (indexType) -> new FaissBinaryHnswIndex());
+        mapping.put(FaissIdMapIndex.IBMP, FaissIdMapIndex::new);
 
         INDEX_TYPE_TO_FAISS_INDEX = Collections.unmodifiableMap(mapping);
     }
