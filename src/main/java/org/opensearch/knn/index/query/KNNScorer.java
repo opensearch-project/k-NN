bf555a9a7383caba228e3c762b881722b823558f
Finn Roblin <finnrobl@amazon.com>
2025-05-28 15:37:37 -0700

    Use DocIdSetIterator.empty() instead of statelessEmptyDocIdSetIterator
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- src/main/java/org/opensearch/knn/index/query/KNNScorer.java
+++ src/main/java/org/opensearch/knn/index/query/KNNScorer.java
@@ -58,72 +58,97 @@ public class KNNScorer extends Scorer {
     }
 
     /**
-     * Returns the Empty Scorer implementation. We use this scorer to short circuit the actual search when it is not
-     * required.
+     * Returns an Empty Scorer. We use this scorer to short circuit the actual search when it is not
+     * required. Since the underlying DocIdSetIterator.empty() is stateful and not thread-safe we must create a new
+     * scorer instance each time to avoid race conditions.
      * @return {@link KNNScorer}
      */
     public static Scorer emptyScorer() {
-        return EMPTY_SCORER_INSTANCE;
+        return new Scorer() {
+            private final DocIdSetIterator docIdsIter = DocIdSetIterator.empty();
+
+            @Override
+            public DocIdSetIterator iterator() {
+                return docIdsIter;
+            }
+
+            @Override
+            public float getMaxScore(int upTo) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public float score() throws IOException {
+                assert docID() != DocIdSetIterator.NO_MORE_DOCS;
+                return 0;
+            }
+
+            @Override
+            public int docID() {
+                return docIdsIter.docID();
+            }
+
+        };
     }
 
-    private static final Scorer EMPTY_SCORER_INSTANCE = new Scorer() {
-        /**
-         * stateless empty DocIdSetIterator. Used in testing as opposed to DocIdSetIterator.empty() since
-         * DocIdSetIterator.empty() contains a stateful exhausted variable. If we associate a particular
-         * DocIdSetIterator.empty() instance with our static EMPTY_SCORER_INSTANCE then we hit an assertion error
-         * when multiple threads race to call advance() as it is not thread-safe.
-         */
-        public static DocIdSetIterator statelessEmptyDocIdSetIterator() {
-            return new DocIdSetIterator() {
-                public int advance(int target) {
-                    return Integer.MAX_VALUE;
-                }
-
-                public int docID() {
-                    return DocIdSetIterator.NO_MORE_DOCS;
-                }
-
-                public int nextDoc() {
-                    return Integer.MAX_VALUE;
-                }
-
-                public long cost() {
-                    return 0L;
-                }
-            };
-        }
-
-        private static final DocIdSetIterator docIdsIter = statelessEmptyDocIdSetIterator();
-
-        @Override
-        public DocIdSetIterator iterator() {
-            return docIdsIter;
-        }
-
-        @Override
-        public float getMaxScore(int upTo) throws IOException {
-            return 0;
-        }
-
-        @Override
-        public float score() throws IOException {
-            assert docID() != DocIdSetIterator.NO_MORE_DOCS;
-            return 0;
-        }
-
-        @Override
-        public int docID() {
-            return docIdsIter.docID();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            return this == obj; // Singleton ensures only one instance exists
-        }
-
-        @Override
-        public int hashCode() {
-            return System.identityHashCode(this); // Consistent hash for singleton
-        }
-    };
+    // private static final Scorer EMPTY_SCORER_INSTANCE = new Scorer() {
+    // /**
+    // * stateless empty DocIdSetIterator. Used in testing as opposed to DocIdSetIterator.empty() since
+    // * DocIdSetIterator.empty() contains a stateful exhausted variable. If we associate a particular
+    // * DocIdSetIterator.empty() instance with our static EMPTY_SCORER_INSTANCE then we hit an assertion error
+    // * when multiple threads race to call advance() as it is not thread-safe.
+    // */
+    // public static DocIdSetIterator statelessEmptyDocIdSetIterator() {
+    // return new DocIdSetIterator() {
+    // public int advance(int target) {
+    // return Integer.MAX_VALUE;
+    // }
+    //
+    // public int docID() {
+    // return DocIdSetIterator.NO_MORE_DOCS;
+    // }
+    //
+    // public int nextDoc() {
+    // return Integer.MAX_VALUE;
+    // }
+    //
+    // public long cost() {
+    // return 0L;
+    // }
+    // };
+    // }
+    //
+    // private static final DocIdSetIterator docIdsIter = statelessEmptyDocIdSetIterator();
+    //
+    // @Override
+    // public DocIdSetIterator iterator() {
+    // return docIdsIter;
+    // }
+    //
+    // @Override
+    // public float getMaxScore(int upTo) throws IOException {
+    // return 0;
+    // }
+    //
+    // @Override
+    // public float score() throws IOException {
+    // assert docID() != DocIdSetIterator.NO_MORE_DOCS;
+    // return 0;
+    // }
+    //
+    // @Override
+    // public int docID() {
+    // return docIdsIter.docID();
+    // }
+    //
+    // @Override
+    // public boolean equals(Object obj) {
+    // return this == obj; // Singleton ensures only one instance exists
+    // }
+    //
+    // @Override
+    // public int hashCode() {
+    // return System.identityHashCode(this); // Consistent hash for singleton
+    // }
+    // };
 }
