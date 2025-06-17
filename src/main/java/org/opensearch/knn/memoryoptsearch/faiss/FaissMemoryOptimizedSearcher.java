504ce9576a6fdaaf0667e8892e168ab1e82a1a60
Dooyong Kim <kdooyong@amazon.com>
2025-06-16 17:46:25 -0700

    Fix Cagra HNSW graph populates random entry points at the beginning of search
    Signed-off-by: Dooyong Kim <kdooyong@amazon.com>

--- src/main/java/org/opensearch/knn/memoryoptsearch/faiss/FaissMemoryOptimizedSearcher.java
+++ src/main/java/org/opensearch/knn/memoryoptsearch/faiss/FaissMemoryOptimizedSearcher.java
@@ -8,7 +8,9 @@ package org.opensearch.knn.memoryoptsearch.faiss;
 import org.apache.lucene.codecs.hnsw.FlatVectorsScorer;
 import org.apache.lucene.index.VectorEncoding;
 import org.apache.lucene.index.VectorSimilarityFunction;
+import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.KnnCollector;
+import org.apache.lucene.search.knn.KnnSearchStrategy;
 import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.util.Bits;
 import org.apache.lucene.util.IOSupplier;
@@ -17,8 +19,10 @@ import org.apache.lucene.util.hnsw.OrdinalTranslatedKnnCollector;
 import org.apache.lucene.util.hnsw.RandomVectorScorer;
 import org.opensearch.knn.index.KNNVectorSimilarityFunction;
 import org.opensearch.knn.memoryoptsearch.VectorSearcher;
+import org.opensearch.knn.memoryoptsearch.faiss.cagra.FaissCagraHNSW;
 
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * This searcher directly reads FAISS index file via the provided {@link IndexInput} then perform vector search on it.
@@ -109,7 +113,7 @@ public class FaissMemoryOptimizedSearcher implements VectorSearcher {
 
         // Set up required components for vector search
         final RandomVectorScorer scorer = scorerSupplier.get();
-        final KnnCollector collector = new OrdinalTranslatedKnnCollector(knnCollector, scorer::ordToDoc);
+        final KnnCollector collector = createKnnCollector(knnCollector, scorer);
         final Bits acceptedOrds = scorer.getAcceptOrds(acceptDocs);
 
         if (knnCollector.k() < scorer.maxOrd()) {
@@ -133,5 +137,75 @@ public class FaissMemoryOptimizedSearcher implements VectorSearcher {
 
     private IndexInput getSlicedIndexInput() throws IOException {
         return indexInput.slice("FaissMemoryOptimizedSearcher", 0, fileSize);
+    }
+
+    private KnnCollector createKnnCollector(final KnnCollector knnCollector, final RandomVectorScorer scorer) {
+        final KnnCollector ordinalTranslatedKnnCollector = new OrdinalTranslatedKnnCollector(knnCollector, scorer::ordToDoc);
+
+        if (hnsw instanceof FaissCagraHNSW cagraHNSW) {
+            return new KnnCollector.Decorator(ordinalTranslatedKnnCollector) {
+                @Override
+                public KnnSearchStrategy getSearchStrategy() {
+                    return new RandomEntryPointsKnnSearchStrategy(
+                        cagraHNSW.getNumBaseLevelSearchEntryPoints(),
+                        cagraHNSW.getTotalNumberOfVectors(),
+                        knnCollector.getSearchStrategy()
+                    );
+                }
+            };
+        } else {
+            return ordinalTranslatedKnnCollector;
+        }
+    }
+
+    /**
+     * Knn search strategy having a doc-id-iterator returning random document ids.
+     * This is not designed for general purpose, it is particularly designed for populating random document ids for Cagra index.
+     * Note that doc-id-iterator returns a random ids in `nextDoc` method without sorting, and might return duplicated ids.
+     */
+    static class RandomEntryPointsKnnSearchStrategy extends KnnSearchStrategy.Seeded {
+        public RandomEntryPointsKnnSearchStrategy(
+            final int numberOfEntryPoints,
+            final long totalNumberOfVectors,
+            final KnnSearchStrategy originalStrategy
+        ) {
+            super(
+                generateRandomEntryPoints(numberOfEntryPoints, Math.toIntExact(totalNumberOfVectors)),
+                numberOfEntryPoints,
+                originalStrategy
+            );
+        }
+
+        private static DocIdSetIterator generateRandomEntryPoints(final int numberOfEntryPoints, int totalNumberOfVectors) {
+            return new DocIdSetIterator() {
+                int numPopulatedVectors = 0;
+
+                @Override
+                public int docID() {
+                    throw new UnsupportedOperationException("DISI in RandomEntryPointsKnnSearchStrategy does not support docID()");
+                }
+
+                @Override
+                public int nextDoc() {
+                    if (numPopulatedVectors < numberOfEntryPoints) {
+                        ++numPopulatedVectors;
+                        // It is fine to populate the same doc ids here, the same vectors will not be visited more than once with bitset.
+                        return ThreadLocalRandom.current().nextInt(totalNumberOfVectors);
+                    }
+
+                    return NO_MORE_DOCS;
+                }
+
+                @Override
+                public int advance(int targetDoc) {
+                    throw new UnsupportedOperationException("DISI in RandomEntryPointsKnnSearchStrategy does not support advance(int)");
+                }
+
+                @Override
+                public long cost() {
+                    throw new UnsupportedOperationException("DISI in RandomEntryPointsKnnSearchStrategy does not support cost()");
+                }
+            };
+        }
     }
 }
