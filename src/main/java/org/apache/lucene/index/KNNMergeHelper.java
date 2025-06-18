262782caf96b8a271adab324edc5d749e26291c9
luyuncheng <luyuncheng@bytedance.com>
2025-06-18 20:51:43 +0800

    ADD Merge Abort Caller
    Signed-off-by: luyuncheng <luyuncheng@bytedance.com>

---
+++ src/main/java/org/apache/lucene/index/KNNMergeHelper.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.apache.lucene.index;
+
+public class KNNMergeHelper {
+
+    private KNNMergeHelper() {}
+    public static boolean isMergeAborted() {
+        Thread mergeThread = Thread.currentThread();
+        if (mergeThread instanceof ConcurrentMergeScheduler.MergeThread) {
+            //return ((ConcurrentMergeScheduler.MergeThread) mergeThread).merge.isAborted();
+            Object mergeObject = LucenePackagePrivateCaller.callPrivateFieldWithMethod(
+                ConcurrentMergeScheduler.MergeThread.class,
+                    "merge",
+                    "isAborted",
+                    mergeThread
+            );
+            return ((Boolean)mergeObject).booleanValue();
+        }
+        return false;
+    }
+}
