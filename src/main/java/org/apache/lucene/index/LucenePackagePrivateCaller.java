29f1af048151e0d582f28ac2645372371d31f7a3
luyuncheng <luyuncheng@bytedance.com>
2025-06-19 19:54:56 +0800

    FXIED Tests
    Signed-off-by: luyuncheng <luyuncheng@bytedance.com>

--- src/main/java/org/apache/lucene/index/LucenePackagePrivateCaller.java
+++ src/main/java/org/apache/lucene/index/LucenePackagePrivateCaller.java
@@ -5,11 +5,13 @@
 
 package org.apache.lucene.index;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-
+@Log4j2
 public class LucenePackagePrivateCaller {
 
     public static Object callPrivateFieldWithMethod(Class<?> clz, String fieldName, String methodName, Object called) {
@@ -17,8 +19,9 @@ public class LucenePackagePrivateCaller {
             try {
                 Field field = clz.getDeclaredField(fieldName);
                 field.setAccessible(true);
-                return callMethod(field.getDeclaringClass(), methodName, null, called, null);
+                return callMethod(field.getType(), methodName, null, field.get(called), null);
             } catch (Exception e) {
+                log.error("callPrivateFieldWithMethod", e);
                 throw new RuntimeException(e);
             }
         });
@@ -30,6 +33,7 @@ public class LucenePackagePrivateCaller {
                 Method method = clz.getDeclaredMethod(methodName, argTypes);
                 return callMethod(method, called, args);
             } catch (Exception e) {
+                log.error("callMethod", e);
                 throw new RuntimeException(e);
             }
         });
@@ -41,6 +45,7 @@ public class LucenePackagePrivateCaller {
                 method.setAccessible(true);
                 return method.invoke(called, args);
             } catch (Exception e) {
+                log.error("callMethod", e);
                 throw new RuntimeException(e);
             }
         });
