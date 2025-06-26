57e713300397528824d613d49ad0d9dc1188561a
DNimmala5 <nimmala2015@gmail.com>
2025-06-26 00:40:39 +0000

    removed googletest make and build stuff

--- jni/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ec2-user/k-NN/jni/googletest-src/googlemock
-# Build directory: /home/ec2-user/k-NN/jni/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- jni/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ec2-user/k-NN/jni/googletest-src/googletest
-# Build directory: /home/ec2-user/k-NN/jni/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
