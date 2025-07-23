0acb6837d54ae589a9b8779a64967578921b2c33
TJ Neuenfeldt <56700214+neuenfeldttj@users.noreply.github.com>
2025-07-23 09:46:01 -0700

    Add KNN Profiling Info to Core Profiler - KNNQuery, Native, Rescore (#2797)
    * WIP, plugin interface will change
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * changelog
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * simplified timers, fix wildcard
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * used getProfileBreakdown and made each inner query be a new node of the tree
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * spotless, wildcard
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * added weight wrappers and utility functions
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * javadocs
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * removed lucene stuff and ran tests
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * added null check in rescore
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * nit fixes and more tests
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * revert KNNRestTestCase output
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * removed internalnested profiling
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * spotless
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * changed to IOSupplier
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * knnprofile unittests
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * fix tree structure
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * renamed to profileBreakdown, fixed tests
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    * javadoc
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    
    ---------
    
    Signed-off-by: TJ Neuenfeldt <tjneu@amazon.com>
    Signed-off-by: TJ Neuenfeldt <56700214+neuenfeldttj@users.noreply.github.com>

--- src/main/java/org/opensearch/knn/index/query/lucene/LuceneEngineKnnVectorQuery.java
+++ src/main/java/org/opensearch/knn/index/query/lucene/LuceneEngineKnnVectorQuery.java
@@ -13,6 +13,8 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.QueryVisitor;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.search.Weight;
+import org.opensearch.knn.profile.KNNProfileUtil;
+import org.opensearch.search.profile.query.QueryProfiler;
 
 import java.io.IOException;
 
@@ -40,8 +42,16 @@ public class LuceneEngineKnnVectorQuery extends Query {
      */
     @Override
     public Weight createWeight(IndexSearcher searcher, ScoreMode scoreMode, float boost) throws IOException {
+        QueryProfiler profiler = KNNProfileUtil.getProfiler(searcher);
+        if (profiler != null) {
+            profiler.getQueryBreakdown(luceneQuery);
+        }
         Query rewrittenQuery = luceneQuery.rewrite(searcher);
-        return rewrittenQuery.createWeight(searcher, scoreMode, boost);
+        final Weight weight = rewrittenQuery.createWeight(searcher, scoreMode, boost);
+        if (profiler != null) {
+            profiler.pollLastElement();
+        }
+        return weight;
     }
 
     @Override
