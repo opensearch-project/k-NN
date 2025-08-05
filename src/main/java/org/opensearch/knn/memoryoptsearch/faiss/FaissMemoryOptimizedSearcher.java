abc4d33e42991a5e7309dd09bf393350ce6eb594
Finn <67562851+finnroblin@users.noreply.github.com>
2025-08-05 21:51:41 +0000

    Support ADC in Lucene-on-faiss initial commit (#2781)
    * Support ADC in Lucene-on-faiss initial commit
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * Address Tejas and Dooyong comments
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * clean up some comments and rerun CI
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * address Dooyong comments; add LOF test suite; fix score translation; more efficient scoring
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * remove unnecessary test helper
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * Address Tejas comments
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * Have hamming distance fallthrough to unsupported space type in ADCFlatVectorsScorer
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * decrease matchRatio to 0.6 and retry CI
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    ---------
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- src/main/java/org/opensearch/knn/memoryoptsearch/faiss/FaissMemoryOptimizedSearcher.java
+++ src/main/java/org/opensearch/knn/memoryoptsearch/faiss/FaissMemoryOptimizedSearcher.java
@@ -6,6 +6,7 @@
 package org.opensearch.knn.memoryoptsearch.faiss;
 
 import org.apache.lucene.codecs.hnsw.FlatVectorsScorer;
+import org.apache.lucene.index.FieldInfo;
 import org.apache.lucene.index.VectorEncoding;
 import org.apache.lucene.index.VectorSimilarityFunction;
 import org.apache.lucene.search.DocIdSetIterator;
@@ -14,10 +15,15 @@ import org.apache.lucene.search.knn.KnnSearchStrategy;
 import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.util.Bits;
 import org.apache.lucene.util.IOSupplier;
+import org.apache.lucene.util.Version;
 import org.apache.lucene.util.hnsw.HnswGraphSearcher;
 import org.apache.lucene.util.hnsw.OrdinalTranslatedKnnCollector;
 import org.apache.lucene.util.hnsw.RandomVectorScorer;
+import org.opensearch.knn.common.FieldInfoExtractor;
+import org.opensearch.knn.common.KNNConstants;
 import org.opensearch.knn.index.KNNVectorSimilarityFunction;
+import org.opensearch.knn.index.SpaceType;
+import org.opensearch.knn.index.engine.qframe.QuantizationConfig;
 import org.opensearch.knn.memoryoptsearch.VectorSearcher;
 import org.opensearch.knn.memoryoptsearch.faiss.cagra.FaissCagraHNSW;
 
@@ -34,18 +40,31 @@ public class FaissMemoryOptimizedSearcher implements VectorSearcher {
     private final FaissHNSW hnsw;
     private final VectorSimilarityFunction vectorSimilarityFunction;
     private final long fileSize;
+    private boolean isAdc;
 
-    public FaissMemoryOptimizedSearcher(final IndexInput indexInput) throws IOException {
+    public FaissMemoryOptimizedSearcher(final IndexInput indexInput, final FieldInfo fieldInfo) throws IOException {
         this.indexInput = indexInput;
         this.fileSize = indexInput.length();
         this.faissIndex = FaissIndex.load(indexInput);
         final KNNVectorSimilarityFunction knnVectorSimilarityFunction = faissIndex.getVectorSimilarityFunction();
-        this.flatVectorsScorer = FlatVectorsScorerProvider.getFlatVectorsScorer(knnVectorSimilarityFunction);
+
         if (knnVectorSimilarityFunction != KNNVectorSimilarityFunction.HAMMING) {
             vectorSimilarityFunction = knnVectorSimilarityFunction.getVectorSimilarityFunction();
         } else {
             vectorSimilarityFunction = null;
         }
+
+        this.isAdc = false;
+        SpaceType spaceType = null;
+        if (fieldInfo != null) {
+            // Extract ADC info from fieldInfo to determine scorer.
+            final QuantizationConfig quantizationConfig = FieldInfoExtractor.extractQuantizationConfig(fieldInfo, Version.LATEST);
+            this.isAdc = quantizationConfig.isEnableADC();
+            spaceType = isAdc ? SpaceType.getSpace(fieldInfo.getAttribute(KNNConstants.SPACE_TYPE)) : null;
+        }
+
+        this.flatVectorsScorer = FlatVectorsScorerProvider.getFlatVectorsScorer(knnVectorSimilarityFunction, isAdc, spaceType);
+
         this.hnsw = extractFaissHnsw(faissIndex);
     }
 
@@ -63,7 +82,7 @@ public class FaissMemoryOptimizedSearcher implements VectorSearcher {
             VectorEncoding.FLOAT32,
             () -> flatVectorsScorer.getRandomVectorScorer(
                 vectorSimilarityFunction,
-                faissIndex.getFloatValues(getSlicedIndexInput()),
+                isAdc ? faissIndex.getByteValues(getSlicedIndexInput()) : faissIndex.getFloatValues(getSlicedIndexInput()),
                 target
             ),
             knnCollector,
@@ -100,7 +119,7 @@ public class FaissMemoryOptimizedSearcher implements VectorSearcher {
             return;
         }
 
-        if (faissIndex.getVectorEncoding() != vectorEncoding) {
+        if (!this.isAdc && faissIndex.getVectorEncoding() != vectorEncoding) {
             throw new IllegalArgumentException(
                 "Search for vector encoding ["
                     + vectorEncoding
