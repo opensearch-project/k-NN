95eff40660e032c56e04cc686e5c946d5bcbbf9b
Aditya Machiroutu <amachiroutu@gmail.com>
2025-08-08 16:30:45 -0700

    tweaking version in test case
    Signed-off-by: Aditya Machiroutu <amachiroutu@gmail.com>

--- src/testFixtures/java/org/opensearch/knn/KNNRestTestCase.java
+++ src/testFixtures/java/org/opensearch/knn/KNNRestTestCase.java
@@ -2516,7 +2516,7 @@ public class KNNRestTestCase extends ODFERestTestCase {
         return version.onOrAfter(Version.V_2_18_0);
     }
 
-    // BBQ encoder is only supported on or after V_3_2_0
+    // BBQ encoder is only supported on or after V_3_3_0
     protected boolean isBBQEncoderSupported(final Optional<String> bwcVersion) {
         if (bwcVersion.isEmpty()) {
             return false;
@@ -2526,7 +2526,7 @@ public class KNNRestTestCase extends ODFERestTestCase {
             versionString = versionString.substring(0, versionString.length() - 9);
         }
         final Version version = Version.fromString(versionString);
-        return version.onOrAfter(Version.V_3_2_0);
+        return version.onOrAfter(Version.V_3_3_0);
     }
 
     /**
