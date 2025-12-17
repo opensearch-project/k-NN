cfed056f95c32453a08e802f85240c6c618ec979
Sahil Buddharaju <sahilbud@amazon.com>
2025-12-17 10:42:22 -0800

    Derived source dynamic template fix
    Signed-off-by: Sahil Buddharaju <sahilbud@amazon.com>

--- src/main/java/org/opensearch/knn/index/codec/KNN10010Codec/KNN10010DerivedSourceStoredFieldsWriter.java
+++ src/main/java/org/opensearch/knn/index/codec/KNN10010Codec/KNN10010DerivedSourceStoredFieldsWriter.java
@@ -6,9 +6,11 @@
 package org.opensearch.knn.index.codec.KNN10010Codec;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.lucene.codecs.StoredFieldsReader;
 import org.apache.lucene.codecs.StoredFieldsWriter;
 import org.apache.lucene.index.FieldInfo;
 import org.apache.lucene.index.MergeState;
+import org.apache.lucene.index.SegmentInfo;
 import org.apache.lucene.index.StoredFieldDataInput;
 import org.apache.lucene.util.BytesRef;
 import org.opensearch.common.collect.Tuple;
@@ -20,14 +22,24 @@ import org.opensearch.core.compress.NotXContentException;
 import org.opensearch.core.xcontent.MediaType;
 import org.opensearch.core.xcontent.MediaTypeRegistry;
 import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.index.mapper.MappedFieldType;
+import org.opensearch.index.mapper.MapperService;
 import org.opensearch.index.mapper.SourceFieldMapper;
 import org.opensearch.knn.index.codec.backward_codecs.KNN9120Codec.KNN9120DerivedSourceStoredFieldsReader;
+import org.opensearch.knn.index.codec.derivedsource.DerivedFieldInfo;
+import org.opensearch.knn.index.codec.derivedsource.DerivedSourceSegmentAttributeParser;
+import org.opensearch.knn.index.mapper.KNNVectorFieldType;
+import org.opensearch.knn.index.util.IndexUtil;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -35,6 +47,8 @@ import java.util.stream.Collectors;
 public class KNN10010DerivedSourceStoredFieldsWriter extends StoredFieldsWriter {
 
     private final StoredFieldsWriter delegate;
+    private final SegmentInfo segmentInfo;
+    private final MapperService mapperService;
     private final Function<Map<String, Object>, Map<String, Object>> vectorMask;
 
     // Keeping the mask as small as possible.
@@ -43,12 +57,22 @@ public class KNN10010DerivedSourceStoredFieldsWriter extends StoredFieldsWriter 
     /**
      *
      * @param delegate StoredFieldsWriter to wrap
-     * @param vectorFieldTypesArg List of vector field types to mask. If empty, no masking will be done
      */
-    public KNN10010DerivedSourceStoredFieldsWriter(StoredFieldsWriter delegate, List<String> vectorFieldTypesArg) {
+    public KNN10010DerivedSourceStoredFieldsWriter(StoredFieldsWriter delegate, SegmentInfo segmentInfo, MapperService mapperService) {
         this.delegate = delegate;
-        List<String> vectorFieldTypes = vectorFieldTypesArg.stream().map(String::toLowerCase).toList();
-        if (vectorFieldTypes.isEmpty() == false) {
+        this.segmentInfo = segmentInfo;
+        this.mapperService = mapperService;
+
+        List<String> vectorFieldTypes = new ArrayList<>();
+        for (MappedFieldType fieldType : mapperService.fieldTypes()) {
+            if (fieldType instanceof KNNVectorFieldType knnVectorFieldType) {
+                if (IndexUtil.isDerivedEnabledForField(knnVectorFieldType, mapperService)) {
+                    vectorFieldTypes.add(fieldType.name().toLowerCase());
+                }
+            }
+        }
+
+        if (!vectorFieldTypes.isEmpty()) {
             this.vectorMask = XContentMapValues.transform(
                 vectorFieldTypes.stream().collect(Collectors.toMap(k -> k, k -> (Object o) -> o == null ? o : MASK)),
                 true
@@ -88,21 +112,67 @@ public class KNN10010DerivedSourceStoredFieldsWriter extends StoredFieldsWriter 
         delegate.writeField(info, value);
     }
 
+    /**
+     * During merge, multiple source segments are combined into a single new segment.
+     * The merged segment must know about ALL derived vector fields from ALL source segments.
+     *
+     * We have to write segment attributes here as well as in finish because during
+     * merge operations, {@code delegate.merge()} internally calls the delegate's finish(),
+     * not ours. Therefore, we must write attributes directly after the merge completes.
+     *
+     * @param mergeState contains readers for source segments and info for the target segment
+     * @return the number of documents merged
+     * @throws IOException if an I/O error occurs during merge
+     */
     @Override
     public int merge(MergeState mergeState) throws IOException {
-        // In case of backwards compatibility, with old segments, we need to perform a non-optimal merge. Basically, it
-        // will repopulate each source and then inject the vector and then remove it. This allows us to migrate
-        // segments from filter approach to mask approach
         if (KNN9120DerivedSourceStoredFieldsReader.doesMergeContainLegacySegments(mergeState)) {
             return super.merge(mergeState);
         }
 
-        // We wrap the segments to avoid injecting back vectors and then removing. If this is not done, then we will
-        // inject and then just write to disk potentially.
+        Set<String> vectorFields = new HashSet<>();
+        Set<String> nestedVectorFields = new HashSet<>();
+
+        for (int i = 0; i < mergeState.storedFieldsReaders.length; i++) {
+            StoredFieldsReader reader = mergeState.storedFieldsReaders[i];
+
+            if (reader instanceof KNN10010DerivedSourceStoredFieldsReader knnReader) {
+                for (DerivedFieldInfo fieldInfo : knnReader.getDerivedVectorFields()) {
+                    if (fieldInfo.isNested()) {
+                        nestedVectorFields.add(fieldInfo.name());
+                    } else {
+                        vectorFields.add(fieldInfo.name());
+                    }
+                }
+            }
+        }
+
         for (int i = 0; i < mergeState.storedFieldsReaders.length; i++) {
             mergeState.storedFieldsReaders[i] = KNN10010DerivedSourceStoredFieldsReader.wrapForMerge(mergeState.storedFieldsReaders[i]);
         }
-        return delegate.merge(mergeState);
+
+        int result = delegate.merge(mergeState);
+
+        if (!vectorFields.isEmpty()) {
+            List<String> sortedVectorFields = new ArrayList<>(vectorFields);
+            Collections.sort(sortedVectorFields);
+            DerivedSourceSegmentAttributeParser.addDerivedVectorFieldsSegmentInfoAttribute(
+                segmentInfo,
+                new ArrayList<>(vectorFields),
+                false
+            );
+        }
+        if (!nestedVectorFields.isEmpty()) {
+            List<String> sortedNestedVectorFields = new ArrayList<>(nestedVectorFields);
+            Collections.sort(sortedNestedVectorFields);
+            DerivedSourceSegmentAttributeParser.addDerivedVectorFieldsSegmentInfoAttribute(
+                segmentInfo,
+                new ArrayList<>(nestedVectorFields),
+                true
+            );
+        }
+
+        return result;
     }
 
     @Override
@@ -157,8 +227,55 @@ public class KNN10010DerivedSourceStoredFieldsWriter extends StoredFieldsWriter 
         delegate.finishDocument();
     }
 
+    /**
+     * Writes derived vector field attributes after the segment writing is finished
+     * Previously, segment attributes were written in {@code fieldsWriter()}
+     * at segment creation time. When bulk indexing documents with dynamic templates that create
+     * new field mappings, only the
+     * first document's mapping existed at that point. Subsequent documents' dynamic mappings
+     * were created as they were parsed, AFTER the segment attributes were already written.
+     *
+     * This caused the segment attribute to be incomplete:
+     * By moving segment attribute writing to {@code finish()}, we ensure all
+     * documents have been parsed and all dynamic mappings exist in {@code mapperService}
+     * before we query for the complete list of derived vector fields.
+     *
+     * @param i the number of documents in the segment
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     public void finish(int i) throws IOException {
+        List<String> vectorFieldTypes;
+        List<String> nestedVectorFieldTypes;
+
+        vectorFieldTypes = new ArrayList<>();
+        nestedVectorFieldTypes = new ArrayList<>();
+
+        for (MappedFieldType fieldType : mapperService.fieldTypes()) {
+            if (fieldType instanceof KNNVectorFieldType knnVectorFieldType) {
+                if (IndexUtil.isDerivedEnabledForField(knnVectorFieldType, mapperService) == false) {
+                    continue;
+                }
+
+                boolean isNested = mapperService.documentMapper().mappers().getNestedScope(fieldType.name()) != null;
+                if (isNested) {
+                    nestedVectorFieldTypes.add(fieldType.name());
+                } else {
+                    vectorFieldTypes.add(fieldType.name());
+                }
+            }
+        }
+
+        // Write segment attributes
+        if (!vectorFieldTypes.isEmpty()) {
+            Collections.sort(vectorFieldTypes);
+            DerivedSourceSegmentAttributeParser.addDerivedVectorFieldsSegmentInfoAttribute(segmentInfo, vectorFieldTypes, false);
+        }
+        if (!nestedVectorFieldTypes.isEmpty()) {
+            Collections.sort(nestedVectorFieldTypes);
+            DerivedSourceSegmentAttributeParser.addDerivedVectorFieldsSegmentInfoAttribute(segmentInfo, nestedVectorFieldTypes, true);
+        }
+
         delegate.finish(i);
     }
 
