982759408de74d882f5c5ec5e24a4f4149fae1eb
Dooyong Kim <kdooyong@amazon.com>
2026-01-21 02:16:41 -0800

    Initial commit for ARM Neon
    Signed-off-by: Dooyong Kim <kdooyong@amazon.com>

---
+++ jni/src/simd/similarity_function/arm_neon_simd_similarity_function.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <stdint.h>
+#include <cmath>
+
+#include "simd_similarity_function_common.cpp"
+#include "faiss_score_to_lucene_transform.cpp"
+
+//
+// FP16
+//
+
+template <BulkScoreTransform BulkScoreTransformFunc, ScoreTransform ScoreTransformFunc>
+struct ArmNeonFP16MaxIP final : BaseSimilarityFunction<BulkScoreTransformFunc, ScoreTransformFunc> {
+    void calculateSimilarityInBulk(SimdVectorSearchContext* srchContext,
+                                   int32_t* internalVectorIds,
+                                   float* scores,
+                                   const int32_t numVectors) final {
+
+    }
+};
+
+template <BulkScoreTransform BulkScoreTransformFunc, ScoreTransform ScoreTransformFunc>
+struct ArmNeonFP16L2 final : BaseSimilarityFunction<BulkScoreTransformFunc, ScoreTransformFunc> {
+    void calculateSimilarityInBulk(SimdVectorSearchContext* srchContext,
+                                   int32_t* internalVectorIds,
+                                   float* scores,
+                                   const int32_t numVectors) final {
+
+    }
+};
+
+
+
+//
+// FP16
+//
+// 1. Max IP
+ArmNeonFP16MaxIP<FaissScoreToLuceneScoreTransform::ipToMaxIpTransformBulk, FaissScoreToLuceneScoreTransform::ipToMaxIpTransform> FP16_MAX_INNER_PRODUCT_SIMIL_FUNC;
+// 2. L2
+ArmNeonFP16L2<FaissScoreToLuceneScoreTransform::l2TransformBulk, FaissScoreToLuceneScoreTransform::l2Transform> FP16_L2_SIMIL_FUNC;
+
+#ifndef __NO_SELECT_FUNCTION
+SimilarityFunction* SimilarityFunction::selectSimilarityFunction(const NativeSimilarityFunctionType nativeFunctionType) {
+    if (nativeFunctionType == NativeSimilarityFunctionType::FP16_MAXIMUM_INNER_PRODUCT) {
+        return &FP16_MAX_INNER_PRODUCT_SIMIL_FUNC;
+    } else if (nativeFunctionType == NativeSimilarityFunctionType::FP16_L2) {
+        return &FP16_L2_SIMIL_FUNC;
+    }
+
+    throw std::runtime_error("Invalid native similarity function type was given, nativeFunctionType="
+                             + std::to_string(static_cast<int32_t>(nativeFunctionType)));
+}
+#endif
