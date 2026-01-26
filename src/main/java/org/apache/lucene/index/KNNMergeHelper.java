19a276a5ed4c736e634c6cc8963b4d7316b066d2
luyuncheng <luyuncheng@bytedance.com>
2026-01-26 22:18:32 +0800

    remove LucenePackagePrivateCaller for simply way.
    Signed-off-by: luyuncheng <luyuncheng@bytedance.com>

--- src/main/java/org/apache/lucene/index/KNNMergeHelper.java
+++ src/main/java/org/apache/lucene/index/KNNMergeHelper.java
@@ -7,24 +7,34 @@ package org.apache.lucene.index;
 
 import lombok.extern.log4j.Log4j2;
 
+import java.lang.reflect.Field;
+
 @Log4j2
 public class KNNMergeHelper {
+
+    private static final Field MERGE_FIELD;
+    static {
+        try {
+            MERGE_FIELD = ConcurrentMergeScheduler.MergeThread.class.getDeclaredField("merge");
+            MERGE_FIELD.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException("Failed to initialize", e);
+        }
+    }
 
     private KNNMergeHelper() {}
 
     public static boolean isMergeAborted() {
         Thread mergeThread = Thread.currentThread();
         if (mergeThread instanceof ConcurrentMergeScheduler.MergeThread) {
-            // return ((ConcurrentMergeScheduler.MergeThread) mergeThread).merge.isAborted();
             try {
-                Object mergeObject = LucenePackagePrivateCaller.callPrivateFieldWithMethod(
-                    ConcurrentMergeScheduler.MergeThread.class,
-                    "merge",
-                    "isAborted",
-                    mergeThread
-                );
-                return ((Boolean) mergeObject).booleanValue();
+                MergePolicy.OneMerge merge = (MergePolicy.OneMerge) MERGE_FIELD.get(mergeThread);
+                return merge.isAborted();
             } catch (RuntimeException e) {
+                log.error("isMergeAborted get exception", e);
+                return false;
+            } catch (IllegalAccessException e) {
+                log.error("IllegalAccessException for reflection merge field", e);
                 return false;
             }
         }
