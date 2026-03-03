b4bb43c3fffc3b02d3e2a2c049eb2585e4848a1a
luyuncheng <luyuncheng@bytedance.com>
2026-03-03 16:00:34 +0800

    Merge Should Throw Runtime/IO Exception
    Signed-off-by: luyuncheng <luyuncheng@bytedance.com>

--- src/main/java/org/opensearch/knn/index/codec/nativeindex/NativeIndexWriter.java
+++ src/main/java/org/opensearch/knn/index/codec/nativeindex/NativeIndexWriter.java
@@ -132,6 +132,7 @@ public class NativeIndexWriter {
             throw new MergePolicy.MergeAbortedException("KNN Merge aborted.");
         } catch (Exception ex) {
             log.error("Merge exception happened for field {}", fieldInfo.name, ex);
+            throw new IOException("Merge exception happened for field: " + fieldInfo.name, ex);
         }
     }
 
@@ -165,6 +166,7 @@ public class NativeIndexWriter {
                 knnVectorValuesSupplier.get(),
                 nativeIndexParams
             );
+            System.out.println(indexBuilder.getClass());
             indexBuilder.buildAndWriteIndex(nativeIndexParams);
             CodecUtil.writeFooter(output);
         }
