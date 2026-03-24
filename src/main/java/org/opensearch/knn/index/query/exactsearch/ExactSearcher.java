9b5d41083aa79e432e639400cea5e92a34fd4d2a
Vijayan Balasubramanian <balasvij@amazon.com>
2026-03-24 11:40:32 -0700

    Use VectorScorer bulk scoring in ExactSearcher
    Replace doc-by-doc iterator scoring with Lucene's VectorScorer.Bulk API
    and DocAndFloatFeatureBuffer for batch processing. This enables batch
    skipping when the max score of a batch is below the current threshold,
    matching the pattern used in Lucene's AbstractKnnVectorQuery.exactSearch.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- src/main/java/org/opensearch/knn/index/query/exactsearch/ExactSearcher.java
+++ src/main/java/org/opensearch/knn/index/query/exactsearch/ExactSearcher.java
@@ -5,16 +5,15 @@
 
 package org.opensearch.knn.index.query.exactsearch;
 
-import com.google.common.base.Predicates;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NonNull;
 import lombok.Value;
 import lombok.extern.log4j.Log4j2;
 import org.apache.lucene.index.FieldInfo;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.SegmentReader;
 import org.apache.lucene.index.VectorSimilarityFunction;
+import org.apache.lucene.search.DocAndFloatFeatureBuffer;
 import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.HitQueue;
 import org.apache.lucene.search.ScoreDoc;
@@ -44,7 +43,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 @Log4j2
 @AllArgsConstructor
@@ -65,13 +63,17 @@ public class ExactSearcher {
         if (vectorScorer == null) {
             return TopDocsCollector.EMPTY_TOPDOCS;
         }
+        // When nested, matchedDocsIterator is already consumed inside NestedBestChildVectorScorer,
+        // so pass null to avoid double consumption of the same iterator.
+        final boolean isNested = context.getParentsFilter() != null;
+        final DocIdSetIterator matchedDocs = isNested ? null : context.getMatchedDocsIterator();
         if (context.getRadius() != null) {
-            return doRadialSearch(leafReaderContext, context, vectorScorer);
+            return doRadialSearch(leafReaderContext, context, vectorScorer, matchedDocs);
         }
-        if (context.getMatchedDocsIterator() != null && context.numberOfMatchedDocs <= context.getK()) {
-            return scoreAllDocs(vectorScorer);
+        if (matchedDocs != null && context.numberOfMatchedDocs <= context.getK()) {
+            return scoreAllDocs(vectorScorer, matchedDocs);
         }
-        return searchTopCandidates(vectorScorer, context.getK(), Predicates.alwaysTrue());
+        return searchTopK(vectorScorer, matchedDocs, context.getK());
     }
 
     /**
@@ -84,9 +86,12 @@ public class ExactSearcher {
      * @return TopDocs containing the results of the search
      * @throws IOException exception raised by scorer during traversal
      */
-    private TopDocs doRadialSearch(LeafReaderContext leafReaderContext, ExactSearcherContext context, VectorScorer vectorScorer)
-        throws IOException {
-        // Ensure `isMemoryOptimizedSearchEnabled` is set. This is necessary to determine whether distance to score conversion is required.
+    private TopDocs doRadialSearch(
+        final LeafReaderContext leafReaderContext,
+        final ExactSearcherContext context,
+        final VectorScorer vectorScorer,
+        final DocIdSetIterator matchedDocs
+    ) throws IOException {
         assert (context.isMemoryOptimizedSearchEnabled != null);
 
         final SegmentReader reader = Lucene.segmentReader(leafReaderContext.reader());
@@ -99,64 +104,99 @@ public class ExactSearcher {
             throw new IllegalArgumentException(String.format(Locale.ROOT, "Engine [%s] does not support radial search", engine));
         }
         final SpaceType spaceType = FieldInfoExtractor.getSpaceType(modelDao, fieldInfo);
-        // We need to use the given radius when memory optimized search is enabled. Since it relies on Lucene's scoring framework, the given
-        // max distance is already converted min score then saved in `radius`. Thus, we don't need a score translation which does not make
-        // sense as it is treating min score as a max distance otherwise.
-        // For FAISS exact search, radius is in FAISS distance space (e.g., inner product for cosine).
-        // We need to convert it to OpenSearch score space using the reverse translation.
         final float minScore = context.isMemoryOptimizedSearchEnabled ? context.getRadius() : engine.score(context.getRadius(), spaceType);
 
-        return filterDocsByMinScore(context, vectorScorer, minScore);
+        return searchWithMinScore(vectorScorer, matchedDocs, context.getMaxResultWindow(), minScore);
     }
 
-    private TopDocs scoreAllDocs(VectorScorer vectorScorer) throws IOException {
-        final DocIdSetIterator iterator = vectorScorer.iterator();
+    private TopDocs scoreAllDocs(final VectorScorer vectorScorer, final DocIdSetIterator matchedDocs) throws IOException {
+        final VectorScorer.Bulk bulkScorer = vectorScorer.bulk(matchedDocs);
+        final DocAndFloatFeatureBuffer buffer = new DocAndFloatFeatureBuffer();
         final List<ScoreDoc> scoreDocList = new ArrayList<>();
-        int docId;
-        while ((docId = iterator.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
-            scoreDocList.add(new ScoreDoc(docId, vectorScorer.score()));
+
+        while (true) {
+            bulkScorer.nextDocsAndScores(DocIdSetIterator.NO_MORE_DOCS, null, buffer);
+            if (buffer.size == 0) {
+                break;
+            }
+            for (int i = 0; i < buffer.size; i++) {
+                scoreDocList.add(new ScoreDoc(buffer.docs[i], buffer.features[i]));
+            }
         }
+
         scoreDocList.sort(Comparator.comparing(scoreDoc -> scoreDoc.score, Comparator.reverseOrder()));
         return new TopDocs(new TotalHits(scoreDocList.size(), TotalHits.Relation.EQUAL_TO), scoreDocList.toArray(ScoreDoc[]::new));
     }
 
-    private TopDocs searchTopCandidates(VectorScorer vectorScorer, int limit, @NonNull Predicate<Float> filterScore) throws IOException {
-        final DocIdSetIterator iterator = vectorScorer.iterator();
-        // Creating min heap and init with MAX DocID and Score as -INF.
-        final HitQueue queue = new HitQueue(limit, true);
+    private TopDocs searchTopK(final VectorScorer vectorScorer, final DocIdSetIterator matchedDocs, final int k) throws IOException {
+        final VectorScorer.Bulk bulkScorer = vectorScorer.bulk(matchedDocs);
+        final DocAndFloatFeatureBuffer buffer = new DocAndFloatFeatureBuffer();
+        final HitQueue queue = new HitQueue(k, true);
         ScoreDoc topDoc = queue.top();
-        int docId;
-        while ((docId = iterator.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
-            final float currentScore = vectorScorer.score();
-            if (filterScore.test(currentScore) && currentScore > topDoc.score) {
-                topDoc.score = currentScore;
-                topDoc.doc = docId;
-                // As the HitQueue is min heap, updating top will bring the doc with -INF score or worst score we
-                // have seen till now on top.
-                topDoc = queue.updateTop();
+
+        for (float maxScore = bulkScorer.nextDocsAndScores(DocIdSetIterator.NO_MORE_DOCS, null, buffer); buffer.size > 0; maxScore =
+            bulkScorer.nextDocsAndScores(DocIdSetIterator.NO_MORE_DOCS, null, buffer)) {
+            if (maxScore < topDoc.score) {
+                continue;
+            }
+            for (int i = 0; i < buffer.size; i++) {
+                if (buffer.features[i] > topDoc.score) {
+                    topDoc.score = buffer.features[i];
+                    topDoc.doc = buffer.docs[i];
+                    topDoc = queue.updateTop();
+                }
             }
         }
 
-        // If scores are negative we will remove them.
-        // This is done, because there can be negative values in the Heap as we init the heap with Score as -INF.
-        // If filterIds < k, some values in heap can have a negative score.
         while (queue.size() > 0 && queue.top().score < 0) {
             queue.pop();
         }
 
-        ScoreDoc[] topScoreDocs = new ScoreDoc[queue.size()];
+        final ScoreDoc[] topScoreDocs = new ScoreDoc[queue.size()];
         for (int i = topScoreDocs.length - 1; i >= 0; i--) {
             topScoreDocs[i] = queue.pop();
         }
-
-        TotalHits totalHits = new TotalHits(topScoreDocs.length, TotalHits.Relation.EQUAL_TO);
-        return new TopDocs(totalHits, topScoreDocs);
+        return new TopDocs(new TotalHits(topScoreDocs.length, TotalHits.Relation.EQUAL_TO), topScoreDocs);
     }
 
-    private TopDocs filterDocsByMinScore(ExactSearcherContext context, VectorScorer vectorScorer, float minScore) throws IOException {
-        int maxResultWindow = context.getMaxResultWindow();
-        Predicate<Float> scoreGreaterThanOrEqualToMinScore = score -> score >= minScore;
-        return searchTopCandidates(vectorScorer, maxResultWindow, scoreGreaterThanOrEqualToMinScore);
+    private TopDocs searchWithMinScore(
+        final VectorScorer vectorScorer,
+        final DocIdSetIterator matchedDocs,
+        final int maxResultWindow,
+        final float minScore
+    ) throws IOException {
+        final VectorScorer.Bulk bulkScorer = vectorScorer.bulk(matchedDocs);
+        final DocAndFloatFeatureBuffer buffer = new DocAndFloatFeatureBuffer();
+        final HitQueue queue = new HitQueue(maxResultWindow, true);
+        ScoreDoc topDoc = queue.top();
+
+        for (float maxBatchScore = bulkScorer.nextDocsAndScores(
+            DocIdSetIterator.NO_MORE_DOCS,
+            null,
+            buffer
+        ); buffer.size > 0; maxBatchScore = bulkScorer.nextDocsAndScores(DocIdSetIterator.NO_MORE_DOCS, null, buffer)) {
+            if (maxBatchScore < minScore) {
+                continue;
+            }
+            for (int i = 0; i < buffer.size; i++) {
+                final float score = buffer.features[i];
+                if (score >= minScore && score > topDoc.score) {
+                    topDoc.score = score;
+                    topDoc.doc = buffer.docs[i];
+                    topDoc = queue.updateTop();
+                }
+            }
+        }
+
+        while (queue.size() > 0 && queue.top().score < 0) {
+            queue.pop();
+        }
+
+        final ScoreDoc[] topScoreDocs = new ScoreDoc[queue.size()];
+        for (int i = topScoreDocs.length - 1; i >= 0; i--) {
+            topScoreDocs[i] = queue.pop();
+        }
+        return new TopDocs(new TotalHits(topScoreDocs.length, TotalHits.Relation.EQUAL_TO), topScoreDocs);
     }
 
     private VectorScorer createVectorScorer(final LeafReaderContext leafReaderContext, final ExactSearcherContext context)
