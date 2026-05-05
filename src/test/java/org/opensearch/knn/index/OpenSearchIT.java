c4d4357830d09d8c7e05450a581c890d5a4c4357
Tejas Shah <shatejas@amazon.com>
2026-05-05 11:57:17 -0700

    Attempt to fix integ test
    Signed-off-by: Tejas Shah <shatejas@amazon.com>

--- src/test/java/org/opensearch/knn/index/OpenSearchIT.java
+++ src/test/java/org/opensearch/knn/index/OpenSearchIT.java
@@ -1431,8 +1431,13 @@ public class OpenSearchIT extends KNNRestTestCase {
         float[] query = new float[dim];
         Arrays.fill(query, 2);
 
+        // Force merging to one segment since each doc gets added with refresh=true which can  create
+        // multiple segments. This fails the assertion since its expecting each metric to have 7 values intead
+        // of a total 7 values
+        forceMergeKnnIndex(INDEX_NAME);
+
         int k = 7;
-        // Create knn search, P <= k
+        // Filtered k-NN search without profiling
         XContentBuilder builder = XContentFactory.jsonBuilder()
             .startObject()
             .field("profile", false)
