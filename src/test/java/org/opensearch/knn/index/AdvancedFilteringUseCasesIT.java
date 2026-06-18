2a6754309b1655e51131398eb6eea0fab55a1a24
Venkateshwaran Shanmugham <venkateshwaracholan@gmail.com>
2026-06-18 15:25:55 +0530

    Add regression test for mixed parent and nested filters (#2771)
    Signed-off-by: Venkateshwaran Shanmugham <venkateshwaracholan@gmail.com>

--- src/test/java/org/opensearch/knn/index/AdvancedFilteringUseCasesIT.java
+++ src/test/java/org/opensearch/knn/index/AdvancedFilteringUseCasesIT.java
@@ -48,10 +48,29 @@ import static org.hamcrest.Matchers.containsString;
 /**
  * This class contains the IT for some advanced and tricky use-case of filters.
  * <a href="https://github.com/opensearch-project/k-NN/issues/1356">Github issue</a>
+ * <a href="https://github.com/opensearch-project/k-NN/issues/2771">Github issue #2771</a>
  */
 public class AdvancedFilteringUseCasesIT extends KNNRestTestCase {
 
     private static final String INDEX_NAME = "advanced_filtering_test_index";
+
+    private static final String ISSUE_2771_INDEX_NAME = "issue-2771-mixed-parent-nested-filters";
+
+    private static final String ISSUE_2771_NESTED_FIELD = "nested_chunks";
+
+    private static final String ISSUE_2771_VECTOR_FIELD = "chunk_embedding";
+
+    private static final String ISSUE_2771_CHUNK_TYPE_FIELD = "chunk_type";
+
+    private static final String ISSUE_2771_PARENT_TYPE_FIELD = "parent_type";
+
+    private static final int ISSUE_2771_DIMENSION = 2;
+
+    private static final int ISSUE_2771_K = 5;
+
+    private static final Float[] ISSUE_2771_QUERY_VECTOR = { 5.1f, 5.4f };
+
+    private static final int ISSUE_2771_EXPECTED_HITS = 1;
 
     private static final String FIELD_NAME_NESTED = "test_nested";
 
@@ -453,6 +472,136 @@ public class AdvancedFilteringUseCasesIT extends KNNRestTestCase {
         }
     }
 
+    /**
+     * Reproduces <a href="https://github.com/opensearch-project/k-NN/issues/2771">issue #2771</a>: efficient
+     * pre-filtering in nested k-NN should support a single {@code knn.filter} bool that combines a root-level
+     * clause with an unwrapped nested-field clause.
+     * <pre>
+     * GET /vector-test/_search
+     * {
+     *   "query": {
+     *     "nested": {
+     *       "path": "nested_chunks",
+     *       "query": {
+     *         "knn": {
+     *           "nested_chunks.chunk_embedding": {
+     *             "k": 5,
+     *             "vector": [5.1, 5.4],
+     *             "filter": {
+     *               "bool": {
+     *                 "must": [
+     *                   { "term": { "parent_type": "foo" } },
+     *                   { "term": { "nested_chunks.chunk_type": "bar" } }
+     *                 ]
+     *               }
+     *             }
+     *           }
+     *         }
+     *       }
+     *     }
+     *   }
+     * }
+     * </pre>
+     */
+    @SneakyThrows
+    @ExpectRemoteBuildValidation
+    public void testFiltering_whenNestedKNNAndMixedParentAndNestedFilter_thenSuccess() {
+        for (final String engine : enginesToTest) {
+            createKnnIndex(ISSUE_2771_INDEX_NAME, createIssue2771Mappings(engine));
+
+            String doc1 = NestedKnnDocBuilder.create(ISSUE_2771_NESTED_FIELD)
+                .addVectorWithMetadata(
+                    ISSUE_2771_VECTOR_FIELD,
+                    new Float[] { 5.2f, 4.4f },
+                    ISSUE_2771_CHUNK_TYPE_FIELD,
+                    "bar"
+                )
+                .addTopLevelField(ISSUE_2771_PARENT_TYPE_FIELD, "foo")
+                .build();
+            addKnnDoc(ISSUE_2771_INDEX_NAME, "1", doc1);
+
+            String doc2 = NestedKnnDocBuilder.create(ISSUE_2771_NESTED_FIELD)
+                .addVectorWithMetadata(
+                    ISSUE_2771_VECTOR_FIELD,
+                    new Float[] { 0.9f, 0.6f },
+                    ISSUE_2771_CHUNK_TYPE_FIELD,
+                    "bar"
+                )
+                .addVectorWithMetadata(
+                    ISSUE_2771_VECTOR_FIELD,
+                    new Float[] { 0.3f, 0.1f },
+                    ISSUE_2771_CHUNK_TYPE_FIELD,
+                    "qux"
+                )
+                .addTopLevelField(ISSUE_2771_PARENT_TYPE_FIELD, "baz")
+                .build();
+            addKnnDoc(ISSUE_2771_INDEX_NAME, "2", doc2);
+
+            refreshIndex(ISSUE_2771_INDEX_NAME);
+            forceMergeKnnIndex(ISSUE_2771_INDEX_NAME);
+
+            final XContentBuilder builder = XContentFactory.jsonBuilder().startObject().startObject(QUERY);
+            builder.startObject(TYPE_NESTED);
+            builder.field(PATH, ISSUE_2771_NESTED_FIELD);
+            builder.startObject(QUERY)
+                .startObject(KNN)
+                .startObject(ISSUE_2771_NESTED_FIELD + "." + ISSUE_2771_VECTOR_FIELD);
+            builder.field(VECTOR, ISSUE_2771_QUERY_VECTOR);
+            builder.field(K, ISSUE_2771_K);
+            builder.startObject(FILTER_FIELD);
+            builder.startObject("bool");
+            builder.startArray("must");
+            builder.startObject();
+            builder.startObject(TERM_FIELD);
+            builder.field(ISSUE_2771_PARENT_TYPE_FIELD, "foo");
+            builder.endObject();
+            builder.endObject();
+            builder.startObject();
+            builder.startObject(TERM_FIELD);
+            builder.field(ISSUE_2771_NESTED_FIELD + "." + ISSUE_2771_CHUNK_TYPE_FIELD, "bar");
+            builder.endObject();
+            builder.endObject();
+            builder.endArray();
+            builder.endObject();
+            builder.endObject();
+            builder.endObject()
+                .endObject()
+                .endObject()
+                .endObject()
+                .endObject()
+                .endObject();
+
+            validateIssue2771FilterSearch(builder.toString(), engine);
+            deleteKNNIndex(ISSUE_2771_INDEX_NAME);
+        }
+    }
+
+    private void validateIssue2771FilterSearch(final String query, final String engine) throws IOException, ParseException {
+        String response = EntityUtils.toString(performSearch(ISSUE_2771_INDEX_NAME, query).getEntity());
+        Assert.assertEquals("For engine " + engine + ", hits: ", ISSUE_2771_EXPECTED_HITS, parseHits(response));
+        Assert.assertEquals("For engine " + engine + ", totalSearchHits: ", ISSUE_2771_EXPECTED_HITS, parseTotalSearchHits(response));
+        Assert.assertEquals("For engine " + engine + ", doc id: ", "1", parseIds(response).get(0));
+
+        if (KNNEngine.getEngine(engine) == KNNEngine.FAISS) {
+            updateIndexSettings(
+                ISSUE_2771_INDEX_NAME,
+                Settings.builder().put(KNNSettings.ADVANCED_FILTERED_EXACT_SEARCH_THRESHOLD, 0)
+            );
+            response = EntityUtils.toString(performSearch(ISSUE_2771_INDEX_NAME, query).getEntity());
+            Assert.assertEquals(
+                "For engine " + engine + ", hits with ANN search: ",
+                ISSUE_2771_EXPECTED_HITS,
+                parseHits(response)
+            );
+            Assert.assertEquals(
+                "For engine " + engine + ", totalSearchHits with ANN search: ",
+                ISSUE_2771_EXPECTED_HITS,
+                parseTotalSearchHits(response)
+            );
+            Assert.assertEquals("For engine " + engine + ", doc id with ANN search: ", "1", parseIds(response).get(0));
+        }
+    }
+
     private void validateFilterSearch(final String query, final String engine) throws IOException, ParseException {
         String response = EntityUtils.toString(performSearch(INDEX_NAME, query).getEntity());
         // Validate number of documents returned as the expected number of documents
@@ -512,6 +661,37 @@ public class AdvancedFilteringUseCasesIT extends KNNRestTestCase {
             .endObject()
             .endObject()
             .endObject()
+            .endObject()
+            .endObject()
+            .endObject();
+
+        return builder.toString();
+    }
+
+    @SneakyThrows
+    private String createIssue2771Mappings(final String engine) {
+        XContentBuilder builder = XContentFactory.jsonBuilder()
+            .startObject()
+            .startObject(PROPERTIES_FIELD)
+            .startObject(ISSUE_2771_NESTED_FIELD)
+            .field(TYPE, TYPE_NESTED)
+            .startObject(PROPERTIES_FIELD)
+            .startObject(ISSUE_2771_VECTOR_FIELD)
+            .field(TYPE, TYPE_KNN_VECTOR)
+            .field(DIMENSION, ISSUE_2771_DIMENSION)
+            .startObject(KNN_METHOD)
+            .field(NAME, METHOD_HNSW)
+            .field(METHOD_PARAMETER_SPACE_TYPE, SpaceType.L2.getValue())
+            .field(KNN_ENGINE, engine)
+            .endObject()
+            .endObject()
+            .startObject(ISSUE_2771_CHUNK_TYPE_FIELD)
+            .field(TYPE, "keyword")
+            .endObject()
+            .endObject()
+            .endObject()
+            .startObject(ISSUE_2771_PARENT_TYPE_FIELD)
+            .field(TYPE, "keyword")
             .endObject()
             .endObject()
             .endObject();
