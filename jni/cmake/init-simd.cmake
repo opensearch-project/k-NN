ecbf588aff5e13c3c163cf6b210981edcd578ced
Juraj Lutter <juraj@lutter.sk>
2026-07-11 23:50:19 +0200

    Fix macOS AVX detection path
    Signed-off-by: Juraj Lutter <juraj@lutter.sk>

--- jni/cmake/init-simd.cmake
+++ jni/cmake/init-simd.cmake
@@ -85,7 +85,7 @@ elseif(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD" OR ${CMAKE_SYSTEM_NAME} STREQUAL 
                 return 0;
             }" HAVE_AVX512_SPR_COMPILER)
         unset(CMAKE_REQUIRED_FLAGS)
-    
+
         if(HAVE_AVX512_SPR_COMPILER)
             set(KNN_HAVE_AVX512_SPR ON)
             set(SIMD_OPT_LEVEL "avx512_spr")
@@ -109,37 +109,37 @@ elseif(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD" OR ${CMAKE_SYSTEM_NAME} STREQUAL 
                 return 0;
             }" HAVE_AVX512_COMPILER)
         unset(CMAKE_REQUIRED_FLAGS)
-    
+
         if(HAVE_AVX512_COMPILER)
             set(KNN_HAVE_AVX512 ON)
-    	set(SIMD_OPT_LEVEL "avx512") # Keep optimization level as avx512 to improve performance on Linux or FreeBSD. This is not present on mac systems, and presently not supported on Windows OS.
+            set(SIMD_OPT_LEVEL "avx512") # Keep optimization level as avx512 to improve performance on Linux or FreeBSD. This is not present on mac systems, and presently not supported on Windows OS.
             set(SIMD_FLAGS -mavx512f -mavx512vl -mavx512bw)
             add_definitions(-DKNN_HAVE_AVX512)
             message(STATUS "[SIMD] AVX512 + F16C supported by compiler.")
         else()
             message(FATAL_ERROR "[SIMD] AVX512 + FP16 was explicitly enabled, but compiler does not support it.")
         endif()
+    endif()
+else()
+    set(CMAKE_REQUIRED_FLAGS "-mavx2 -mf16c -mfma")
+    check_cxx_source_compiles("
+        #include <immintrin.h>
+        int main() {
+            __m256 v = _mm256_setzero_ps();
+            __m128i h = _mm256_cvtps_ph(v, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);
+            (void)h;
+            return 0;
+        }" HAVE_AVX2_COMPILER)
+    unset(CMAKE_REQUIRED_FLAGS)
+ 
+    if(HAVE_AVX2_COMPILER)
+        set(KNN_HAVE_AVX2_F16C ON)
+        set(SIMD_OPT_LEVEL "avx2") # Keep optimization level as avx2 to improve performance on Linux, FreeBSD and Mac.
+        set(SIMD_FLAGS -mavx2 -mf16c -mfma)
+        add_definitions(-DKNN_HAVE_AVX2_F16C)
+        message(STATUS "[SIMD] AVX2 + F16C supported by compiler.")
     else()
-        set(CMAKE_REQUIRED_FLAGS "-mavx2 -mf16c -mfma")
-        check_cxx_source_compiles("
-            #include <immintrin.h>
-            int main() {
-                __m256 v = _mm256_setzero_ps();
-                __m128i h = _mm256_cvtps_ph(v, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);
-                (void)h;
-                return 0;
-            }" HAVE_AVX2_COMPILER)
-        unset(CMAKE_REQUIRED_FLAGS)
-    
-        if(HAVE_AVX2_COMPILER)
-            set(KNN_HAVE_AVX2_F16C ON)
-    	set(SIMD_OPT_LEVEL "avx2") # Keep optimization level as avx2 to improve performance on Linux, FreeBSD and Mac.
-            set(SIMD_FLAGS -mavx2 -mf16c -mfma)
-            add_definitions(-DKNN_HAVE_AVX2_F16C)
-            message(STATUS "[SIMD] AVX2 + F16C supported by compiler.")
-        else()
-            message(FATAL_ERROR "[SIMD] AVX2 + F16C was explicitly enabled, but compiler does not support it.")
-        endif()
+        message(FATAL_ERROR "[SIMD] AVX2 + F16C was explicitly enabled, but compiler does not support it.")
     endif()
 endif()
 
