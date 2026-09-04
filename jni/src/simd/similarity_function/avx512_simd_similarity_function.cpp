49d36aef07ab287ef362bac186684579c5275797
Shreya Bhatta <shreyab963@gmail.com>
2026-09-04 08:47:06 -0700

    Add SPDX license header checker and missing headers (#3532)
    * Add SPDX license header checker and missing headers
    
    Signed-off-by: shreyah963 <shreyab963@gmail.com>
    
    * Pin @kt3k/license-checker to version 3.2.2
    
    Signed-off-by: shreyah963 <shreyab963@gmail.com>
    
    * Narrow jni ignore to vendored dirs and add SPDX headers to first-party native source
    
    Signed-off-by: shreyah963 <shreyab963@gmail.com>
    
    ---------
    
    Signed-off-by: shreyah963 <shreyab963@gmail.com>
    Co-authored-by: Navneet Verma <navneev@amazon.com>

--- jni/src/simd/similarity_function/avx512_simd_similarity_function.cpp
+++ jni/src/simd/similarity_function/avx512_simd_similarity_function.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <immintrin.h>
 #include <algorithm>
 #include <array>
--- jni/src/simd/similarity_function/simd_similarity_function_common.cpp
+++ jni/src/simd/similarity_function/simd_similarity_function_common.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <cstdlib>
 #include <cstring>
 #include <stdexcept>
