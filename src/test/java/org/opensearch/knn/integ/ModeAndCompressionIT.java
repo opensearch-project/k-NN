27513f567a2621e49d754dd4945566d0db791a6c
Navneet Verma <navneev@amazon.com>
2026-09-09 16:26:41 -0700

    Removed the random vectors generation and moved the images to latest rather than snapshot
    Signed-off-by: Navneet Verma <navneev@amazon.com>

--- src/test/java/org/opensearch/knn/integ/ModeAndCompressionIT.java
+++ src/test/java/org/opensearch/knn/integ/ModeAndCompressionIT.java
@@ -29,7 +29,6 @@ import org.opensearch.knn.index.SpaceType;
 import org.opensearch.knn.index.mapper.CompressionLevel;
 import org.opensearch.knn.index.mapper.Mode;
 import org.opensearch.knn.index.query.parser.RescoreParser;
-import org.opensearch.knn.common.annotation.ExpectRemoteBuildValidation;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -144,7 +143,6 @@ public class ModeAndCompressionIT extends KNNRestTestCase {
     }
 
     @SneakyThrows
-    @ExpectRemoteBuildValidation
     public void testIndexCreation_whenValid_ThenSucceed() {
         XContentBuilder builder;
         for (String compressionLevel : COMPRESSION_LEVELS) {
